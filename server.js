6dc90a6e6dfef4810f2840c2aa098c38a138df3f
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-06-10 17:22:15 +0500

    http server

--- server.js
+++ server.js
@@ -46,20 +46,20 @@ mongoose
 // readFileSync function must use __dirname get current directory
 // require use ./ refer to current directory.
 
-const option = {
-  key: fs.readFileSync(__dirname + '/private.pem'),
-  cert: fs.readFileSync(__dirname + '/certificate.pem'),
-  chain1: fs.readFileSync(__dirname + '/chain1.pem'),
-  fullchain1: fs.readFileSync(__dirname + '/fullchain1.pem'),
-};
 // const option = {
-//   key: fs.readFileSync('/etc/letsencrypt/archive/1obet.com/privkey1.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/archive/1obet.com/cert1.pem'),
-//   chain1: fs.readFileSync('/etc/letsencrypt/archive/1obet.com/chain1.pem'),
-//   fullchain1: fs.readFileSync(
-//     '/etc/letsencrypt/archive/1obet.com/fullchain1.pem'
-//   ),
+//   key: fs.readFileSync(__dirname + '/private.pem'),
+//   cert: fs.readFileSync(__dirname + '/certificate.pem'),
+//   chain1: fs.readFileSync(__dirname + '/chain1.pem'),
+//   fullchain1: fs.readFileSync(__dirname + '/fullchain1.pem'),
 // };
+const option = {
+  key: fs.readFileSync('/etc/letsencrypt/archive/1obet.com/privkey1.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/archive/1obet.com/cert1.pem'),
+  chain1: fs.readFileSync('/etc/letsencrypt/archive/1obet.com/chain1.pem'),
+  fullchain1: fs.readFileSync(
+    '/etc/letsencrypt/archive/1obet.com/fullchain1.pem'
+  ),
+};
 // console.log('dirname',__dirname);
 // JSON
 app.use(express.json());
