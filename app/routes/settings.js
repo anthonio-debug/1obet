1ab7ef4a3e7560edc9d2007a1cce7cef88ab7917
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-06-10 17:10:55 +0500

    minor fixes

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -312,8 +312,10 @@ function getSideBarMenu(req, res) {
 
 function getSideBarMenu(req, res) {
   let type = req.decoded.role == 5 ? 0 : 1;
-  
-  
+  let onlyCompany = 0;
+  if(req.decoded.role == 0){
+    onlyCompany =  1;
+  }
   SideBarMenu.find({ 
       $or: [
         { type: type }, 
