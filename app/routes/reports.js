aeb9eae1effdc1012dec12ead03836b294d2a0d6
MUmarRasheed <sfsumarr@gmail.com>
2023-06-13 19:06:27 +0500

    fix in final sheet

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -176,7 +176,7 @@ function getFinalReport(req, res) {
   let query = {};
 
   if (req.decoded.login.role !== '5') {
-    query.userId = parseInt(req.decoded.createdBy);
+    query.userId = req.decoded.userId
   }
 
   User.aggregate(
