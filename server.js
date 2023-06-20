f30a01192e2b60a53a1f14b59e96ac2e7f29a307
MUmarRasheed <sfsumarr@gmail.com>
2023-06-20 19:06:55 +0500

    added bookDetails2Reports

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.use('/api', require('./app/routes/casinoGames').loginRouter);
 app.use('/api', require('./app/routes/dailyPLReports').loginRouter);
 app.use('/api', require('./app/routes/dailyReports').loginRouter);
 app.use('/api', require('./app/routes/commissionReports').loginRouter);
+app.use('/api', require('./app/routes/bookDetail2Reports').loginRouter);
 
 // // Allowed Apis for this role
 // app.use(function (req, res, next) {
