d601186494e58f7e0c4a7f0e585bff408e31e84d
MUmarRasheed <sfsumarr@gmail.com>
2023-06-24 17:50:58 +0500

    revert startdate,endDate in getusersbets api

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -257,8 +257,8 @@ function getUserBets(req, res) {
     page = Number(req.query.page);
   }
   if (req.body.startDate && req.body.endDate) {
-    const startTimestamp = new Date(req.body.startDate).getTime() ;
-    const endTimestamp = new Date(req.body.endDate).getTime() ;
+    const startTimestamp = new Date(req.body.startDate).getTime() / 1000;
+    const endTimestamp = new Date(req.body.endDate).getTime() / 1000;
     query.createdAt = {
       $gte: startTimestamp,
       $lte: endTimestamp,
