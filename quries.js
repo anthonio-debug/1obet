70861ecc6843ad0a5ee7396ccbb2b78177e85f2e
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-06-26 17:49:49 +0500

    Quries change

--- quries.js
+++ quries.js
@@ -80,7 +80,7 @@ db.sidebarmenus.insertMany([
     link: "/bet-fair-games",
   },
   {
-    name: "All World Casino",
+    name: "120 British Casino",
     lightIcon: "https://1obet.com/assets/light-icons/royal-casino.png",
     darkIcon: "https://1obet.com/assets/dark-icons/royal-casino.png",
     marketId: "tyv234543-432453343423-53423d332",
@@ -224,7 +224,7 @@ db.sidebarmenus.insertMany([
     link: "/bet-fair-games",
   },
   {
-    name: "All World Casino",
+    name: "120 British Casino",
     lightIcon: "https://1obet.com/assets/light-icons/royal-casino.png",
     darkIcon: "https://1obet.com/assets/dark-icons/royal-casino.png",
     marketId: "tyv234543-432453343423-53423d332",
