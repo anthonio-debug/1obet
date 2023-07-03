e3e5c140e931e1df0824b09f7ddedb779357b923
MUmarRasheed <sfsumarr@gmail.com>
2023-07-03 13:57:03 +0500

    user can change its password

--- app/routes/user.js
+++ app/routes/user.js
@@ -365,10 +365,6 @@ function updateUser(req, res) {
     return res.status(400).send({ errors: errors.errors });
   }
 
-  if (req.decoded.role == '5') {
-    return res.status(404).send({message:'you are not allowed to do this'})
-  }
-
   User.findOne({ userId: req.body.id }, (err, user) => {
     if (err || !user) {
       return res.status(404).send({ message: 'User not found' });
