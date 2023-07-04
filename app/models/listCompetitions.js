6331d9eca00afe458181df6a1bd62d1fc25a10b9
MUmarRasheed <sfsumarr@gmail.com>
2023-07-04 17:36:01 +0500

    added the status field in competition model

--- app/models/listCompetitions.js
+++ app/models/listCompetitions.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const competitionSchema = new mongoose.Schema({
   Id: { type: String, required: true },
   Name: { type: String, required: true },
+  Status: { type: Number },
 });
 
 const Competition = mongoose.model('Competition', competitionSchema);
--- app/routes/sportsAPI.js
+++ app/routes/sportsAPI.js
@@ -9,7 +9,7 @@ const Odds = require('../models/odds');
 const loginRouter = express.Router();
 
 async function listCompetitions(req, res) {
-  const sportId = req.params.sportId; // Use req.query to retrieve the sportId parameter
+  const sportId = req.params.sportId;
   const url = `${config.sportsAPIUrl}/listCompetitions/${sportId}`;
   console.log('URL:', url);
   try {
@@ -17,20 +17,35 @@ async function listCompetitions(req, res) {
     console.log('Response:', response.data);
     const competitionData = response.data;
 
-    // Create an array to store the created Competition documents
     const competitions = [];
 
-    // Iterate over the competitionData array and create a new Competition document for each competition
     for (const data of competitionData) {
-      const competition = new ListCompetitions({
+      let status;
+
+      // Set the status based on the sportId
+      if (sportId == '1') {
+        status = 1;
+      } else if (sportId == '2') {
+        status = 2;
+      } else if (sportId == '4') {
+        status = 3;
+      }
+      // Check if the competition already exists in the database
+      const existingCompetition = await ListCompetitions.find({
         Id: data.Id,
-        Name: data.Name,
       });
 
-      // Save the document to the database
-      await competition.save();
+      if (!existingCompetition) {
+        const competition = new ListCompetitions({
+          Id: data.Id,
+          Name: data.Name,
+          Status: status,
+        });
 
-      competitions.push(competition);
+        await competition.save();
+
+        competitions.push(competition);
+      }
     }
 
     res.status(200).json({
