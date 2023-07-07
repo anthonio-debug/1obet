444d31e17cbcbd2e2e1dedf13f3f1308d3eab78b
MUmarRasheed <sfsumarr@gmail.com>
2023-07-07 21:57:46 +0500

    fix the fancydata url

--- app/routes/fancyGames.js
+++ app/routes/fancyGames.js
@@ -6,7 +6,7 @@ const loginRouter = express.Router();
 
 async function getFancyData(req, res) {
   const eventId = req.params.eventId;
-  const url = `${config.fancyUrl}/${eventId}`;
+  const url = `${config.fancyUrl}/bm_fancy/${eventId}`;
   console.log('url', url);
   try {
     const response = await axios.get(url);
