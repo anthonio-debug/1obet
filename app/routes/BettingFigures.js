6695899b6266f16a9ec2984ef394ca9b2d1345de
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-16 15:46:28 +0500

    Score card changes Added

--- app/routes/BettingFigures.js
+++ app/routes/BettingFigures.js
@@ -63,44 +63,55 @@ async function UpdateBettingFigures(req, res) {
 
 async function livesportscore(req, res) {
     try {
-        const id = req.params.id;
-        const response =  await   axios.get(`https://livesportscore.xyz:3440/api/bf_scores/${id}`);
-        const data = response.data;
+        const id = req.params.id
+        const apiResponse =  await   axios.get(`https://livesportscore.xyz:3440/api/bf_scores/${id}`);
+        const data = apiResponse.data;
+        const response = {};
         if(typeof(data[0]) == "string"){
 
-            const type = await Event.findOne({Id: id}, {_id: 0,matchType:1}).matchType;
-            const scoreInfo = JSON.parse(data).score
-            const response = {
-                score: 0,
-                wickets: 0,
-                overs: 0,
-                team : scoreInfo.spnnation1,
-                crr   : scoreInfo.spnrunrate1.substring(scoreInfo.spnrunrate1.indexOf(' ') + 1).trim(),
-                balls: scoreInfo.balls,
-                type: type,
-                rrr: "0" 
+            const type          = await Event.findOne({Id: id}, {_id: 0,matchType:1}).matchType;
+            const scoreInfo     = JSON.parse(data).score
+            let score           = scoreInfo.score1;
+            let played          = scoreInfo.score2;
+
+            if(scoreInfo.activenation1 == 1){
+                response.team   =  scoreInfo.spnnation1
+                response.crr    = scoreInfo.spnrunrate1.substring(scoreInfo.spnrunrate1.indexOf(' ') + 1).trim()
+
             }
-            let score = scoreInfo.score1;
-            if( scoreInfo.activenation2 == 1){
+            else if(scoreInfo.activenation2 == 1){
                 response.team    = scoreInfo.spnnation2;
                 response.crr     = scoreInfo.spnrunrate2.substring(scoreInfo.spnrunrate2.indexOf(' ') + 1).trim()
                 score            = scoreInfo.score2;
-                response.target  = scoreInfo.score1.replaceAll(/[\s-]/g, ',').replaceAll(/[())]/g, '').split(',')[0];
-
+                played           = scoreInfo.score1;
             }
-            if(scoreInfo.spnreqrate1 != null && scoreInfo.spnreqrate1 != "" ){
-                response.rrr = scoreInfo.spnreqrate;
-            }
-            else if(scoreInfo.spnreqrate2 != null && scoreInfo.spnreqrate2 != ""){
+            
+            response.type   =  type
+            response.balls  = scoreInfo.balls
 
-                scoreInfo.spnreqrate2 != null && scoreInfo.spnreqrate2 != ""
+            if(type == "TEST"){
+                score = score.split('&');
+                score = score[score.length - 1].trim()
+                played = played.split('&');
+                played = played[played.length - 1].trim();
             }
-
+            played = played.replaceAll(/[\s-]/g, ',').replaceAll(/[())]/g, '').split(',');
+            played = played.filter(element => element != 0).length;
+            if(played > 0){
+                response.secondInnings  = 1;
+                response.spnmessage =  scoreInfo.spnmessage
+                response.target  = (parseInt(scoreInfo.score1.replaceAll(/[\s-]/g, ',').replaceAll(/[())]/g, '').split(',')[0]) + 1).toString();
+                if(scoreInfo.spnreqrate1 != null && scoreInfo.spnreqrate1 != "" ){
+                    response.rrr = scoreInfo.spnreqrate;
+                }
+                else if(scoreInfo.spnreqrate2 != null && scoreInfo.spnreqrate2 != ""){
+                    response.rrr = scoreInfo.spnreqrate2;
+                }
+            }
             [response.score, response.wickets, response.overs] = score.replaceAll(/[\s-]/g, ',').replaceAll(/[())]/g, '').split(',');
-
             res.json({
                 status: true,
-                msg: "current score information",
+                msg: "current score info",
                 data: response,
                 testing: scoreInfo
             })
