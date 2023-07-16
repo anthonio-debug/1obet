62dd43cfdbc81bfef543153978efb8892b601d87
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-17 01:45:05 +0500

    Event to inPlay Event

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -552,7 +552,7 @@ async function livesportscore(id) {
     const data = apiResponse.data;
     const response = {};
     if(typeof(data[0]) == "string"){
-      const event = await Event.findOne({ Id: id }, { _id: 0, matchType: 1, sportsId: 1 });
+      const event = await inPlayEvents.findOne({ Id: id }, { _id: 0, matchType: 1, sportsId: 1 });
       const type = event ? event.matchType : null;
       const scoreInfo     = JSON.parse(data).score
       let score           = scoreInfo.score1;
