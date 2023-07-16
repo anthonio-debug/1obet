88f5fa34e716d9b60e1d64dde90d3d0f7da38590
MUmarRasheed <sfsumarr@gmail.com>
2023-07-16 05:25:50 -0700

    fix issue

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -553,7 +553,7 @@ async function livesportscore(id) {
     const response = {};
     if(typeof(data[0]) == "string"){
 
-        const type          = await Event.findOne({Id: id}, {_id: 0,matchType:1}).matchType;
+        const type          = await Event.findOne({Id: id}, {_id: 0,matchType:1});
         const scoreInfo     = JSON.parse(data).score
         let score           = scoreInfo.score1;
         let played          = scoreInfo.score2;
