3a65570e245be4df67de0ee2596cc4e0251454fd
MUmarRasheed <sfsumarr@gmail.com>
2023-07-17 06:42:54 -0700

    add helper function in rollback for sha1 key issue

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -2,6 +2,77 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const CasinoDebits = require('../models/casinoCalls');
+const crypto = require('crypto');
+const config = require('config')
+
+function validateKey(key, payload, salt) {
+  const queryString = Object.keys(payload)
+    .sort()
+    .map(key => `${key}=${payload[key]}`)
+    .join('&');
+
+    console.log('queryString',queryString);
+  const hash = crypto
+    .createHash('sha1')
+    .update(salt + queryString)
+    .digest('hex');
+  console.log('Hash',hash);
+
+  return hash === key;
+}
+
+function rollback(req, res) {
+  const payload = req.query;
+  const remoteId = payload.remote_id;
+  const salt = config.saltKey;
+
+  const key = payload.key;
+  delete payload.key;
+
+  console.log('Received key:', key);
+
+  if (!validateKey(key, payload, salt)) {
+    console.log('Key validation failed');
+    return res.json({
+      status: 403,
+      msg: 'INCORRECT_KEY_VALIDATION'
+    });
+  }
+
+  console.log('Key validation successful');
+
+  const casinoDebits = new CasinoDebits(payload);
+  casinoDebits.save((err, savedPayload) => {
+    if (err) {
+      console.error(err);
+      return res.json({
+        status: 500,
+        msg: 'Internal error'
+      });
+    }
+
+    User.findOne({ remoteId }, (err, user) => {
+      if (err || !user) {
+        return res.json({
+          status: 500,
+          msg: 'Internal error'
+        });
+      }
+
+      if (payload.action === 'rollback') {
+        user.availableBalance += payload.amount * 307;
+        user.save();
+      }
+
+      const updatedBalance = (user.availableBalance / 307).toFixed(2);
+
+      return res.json({
+        status: 200,
+        balance: updatedBalance
+      });
+    });
+  });
+}
 
 function balance(req, res) {
   const payload = req.query;
@@ -80,30 +151,30 @@ function credit(req, res) {
   });
 }
 
-function rollback(req, res) {
-  const payload = req.query
-  const remoteId = req.query.remote_id;
-  const casinoDebits = new CasinoDebits(payload);
-  casinoDebits.save((err, savedPayload) => {
-    if (err) {
-      console.error(err);
-      return res.send({ status: '500', msg: 'internal error' });
-    }
-  User.findOne({ remoteId: remoteId }, (err, user) => {
-    if (err || !user) {
-      return res.send({ status: '500', msg: 'internal error' });
-    }
-    if(req.query.action == 'rollback'){
-      user.availableBalance += req.query.amount * 307;
-      user.save();
-    }
-    return res.send({
-      status: 200,
-      balance: (user.availableBalance / 307).toFixed(2),
-    });
-  });
-})
-}
+// function rollback(req, res) {
+//   const payload = req.query
+//   const remoteId = req.query.remote_id;
+//   const casinoDebits = new CasinoDebits(payload);
+//   casinoDebits.save((err, savedPayload) => {
+//     if (err) {
+//       console.error(err);
+//       return res.send({ status: '500', msg: 'internal error' });
+//     }
+//   User.findOne({ remoteId: remoteId }, (err, user) => {
+//     if (err || !user) {
+//       return res.send({ status: '500', msg: 'internal error' });
+//     }
+//     if(req.query.action == 'rollback'){
+//       user.availableBalance += req.query.amount * 307;
+//       user.save();
+//     }
+//     return res.send({
+//       status: 200,
+//       balance: (user.availableBalance / 307).toFixed(2),
+//     });
+//   });
+// })
+// }
 
 function casino(req, res) {
   const { action, remote_id } = req.query;
