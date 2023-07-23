5385f56a7795e1e332d6ceb3a17dfa04a825ed3d
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-23 18:23:38 +0500

    Core Issue  Server

--- socketServer.js
+++ socketServer.js
@@ -24,7 +24,7 @@ const io = new Server(server,  {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    allowedHeaders: '*/*',
+    // allowedHeaders: '*/*',
     // credentials: true
   },
   // transports: ['polling'] // Enable WebSocket transport
