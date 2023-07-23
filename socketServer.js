2c08d470e33ae4da6c9c0d5b6877a75629de1864
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-23 19:16:14 +0500

    Port Allwed

--- socketServer.js
+++ socketServer.js
@@ -1,46 +1,70 @@
+// Import required modules
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-let config = require('config');
-let cors = require('cors');
-const https = require('https');
-const option = require('./option');
-const PORT = 4001
-// CONNECT THE DATABASE
-let options = {
+const { Server } = require('socket.io');
+const { listOdds , racesMarketOdds } = require('./app/routes/socketHelper')
+
+// Create Express app
+const app = express();
+const server = app.listen(4001, () => {
+  console.log('Server listening on port 4001');
+});
+
+// Connect to MongoDB using Mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/Bet99', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  connectTimeoutMS: 30000, // 30 seconds
-};
+});
 
-mongoose
-  .connect(config.DBHost, options)
-  .then(() => {
-    console.log('Database connected');
-  })
-  .catch((err) => {
-    console.log(` Database did not connect because ${err}`);
+
+// Create Socket.io instance
+const io = new Server(server,  {
+  pingInterval: 5000,
+  pingTimeout: 60000,
+  cookie: false,
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: '*/*',
+    credentials: true
+  },
+  transports: ['polling'] // Enable WebSocket transport
+
+});
+
+
+
+// Socket.io event handlers
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // LISTEN FOR EVENT 1
+  socket.on('listOdds', async (data) => {
+    console.log('Received event1:', data);
+
+    // Perform some function with data
+    const result = await listOdds(data);
+    
+    // Emit 'event1_response' with the result
+    socket.emit('listOdds_response', result);
   });
 
 
-// READ FORM DATA
 
-var corsOptions = {
-  origin: true,
-  credentials: true,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+  // LISTEN FOR EVENT 2
+  socket.on('racesMarketOdds', async (data) => {
+    console.log('Received event2:', data);
 
-app.use(cors(corsOptions));
+    // Perform some function with data
+    const result = await racesMarketOdds(data);
 
-app.get('/', (req, res) => {
-  res.send(
-    '<body style="background: #000; color: #fff"><h2> This is the homepage of 1obet.com </h2></body>'
-  );
-});
+    // Emit 'event2_response' with the result
+    socket.emit('racesMarketOdds_response', result);
+  });
 
-var server = https.createServer(option, app);
-server.listen(PORT, (err) => {
-  if (err) throw new Error(err);
-  console.log(`Server is listening on port ${PORT}`);
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
 });
