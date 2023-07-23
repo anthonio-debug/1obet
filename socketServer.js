962d56d5762fec1638883c8c1f594203675e443f
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-23 19:22:02 +0500

    Tet server

--- socketServer.js
+++ socketServer.js
@@ -1,70 +1,55 @@
-// Import required modules
 const express = require('express');
-const mongoose = require('mongoose');
-const { Server } = require('socket.io');
-const { listOdds , racesMarketOdds } = require('./app/routes/socketHelper')
-
-// Create Express app
 const app = express();
-const server = app.listen(4001, () => {
-  console.log('Server listening on port 4001');
-});
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+let config = require('config');
+let fs = require('fs');
+let cors = require('cors');
+var morgan = require('morgan');
+const https = require('https');
+const option = require('./option');
+const apisMiddleware = require('./app/middlewares/apisMiddleware');
+const loginMiddleWare = require('./app/middlewares/loginMiddleware');
+const aclMiddleware = require('./app/middlewares/aclMiddleware');
+const accessMiddleware = require('./app/middlewares/accessMiddleware');
+const checkRoleMiddleware = require('./app/middlewares/checkRoleMiddleware');
 
-// Connect to MongoDB using Mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/Bet99', {
+// CONNECT THE DATABASE
+let options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  connectTimeoutMS: 30000, // 30 seconds
+};
+
+mongoose.set('strictQuery', false);
+mongoose.set({ debug: true });
+mongoose
+  .connect(config.DBHost, options)
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((err) => {
+    console.log(` Database did not connect because ${err}`);
+  });
+
+
+// READ FORM DATA
+
+var corsOptions = {
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+app.get('/', (req, res) => {
+  res.send(
+    '<body style="background: #000; color: #fff"><h2> This is the homepage of 1obet.com </h2></body>'
+  );
 });
 
-
-// Create Socket.io instance
-const io = new Server(server,  {
-  pingInterval: 5000,
-  pingTimeout: 60000,
-  cookie: false,
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: '*/*',
-    credentials: true
-  },
-  transports: ['polling'] // Enable WebSocket transport
-
-});
-
-
-
-// Socket.io event handlers
-io.on('connection', (socket) => {
-  console.log('New client connected');
-
-  // LISTEN FOR EVENT 1
-  socket.on('listOdds', async (data) => {
-    console.log('Received event1:', data);
-
-    // Perform some function with data
-    const result = await listOdds(data);
-    
-    // Emit 'event1_response' with the result
-    socket.emit('listOdds_response', result);
-  });
-
-
-
-  // LISTEN FOR EVENT 2
-  socket.on('racesMarketOdds', async (data) => {
-    console.log('Received event2:', data);
-
-    // Perform some function with data
-    const result = await racesMarketOdds(data);
-
-    // Emit 'event2_response' with the result
-    socket.emit('racesMarketOdds_response', result);
-  });
-
-
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
+var server = https.createServer(option, app);
+server.listen(config.PORT, (err) => {
+  if (err) throw new Error(err);
+  console.log(`Server is listening on port ${config.PORT}`);
 });
--- testSocket.js
+++ testSocket.js
@@ -1,12 +1,11 @@
 // Import required modules
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Import cors module
 const { Server } = require('socket.io');
 const { listOdds , racesMarketOdds } = require('./app/routes/socketHelper')
+
 // Create Express app
 const app = express();
-app.use(cors());
 const server = app.listen(4001, () => {
   console.log('Server listening on port 4001');
 });
@@ -20,9 +19,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/Bet99', {
 
 // Create Socket.io instance
 const io = new Server(server,  {
+  pingInterval: 5000,
+  pingTimeout: 60000,
+  cookie: false,
   cors: {
-    origin: "*",
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: '*/*',
+    credentials: true
+  },
+  transports: ['polling'] // Enable WebSocket transport
 
 });
 
@@ -35,13 +41,12 @@ io.on('connection', (socket) => {
   // LISTEN FOR EVENT 1
   socket.on('listOdds', async (data) => {
     console.log('Received event1:', data);
-    // Perform some function with data
 
+    // Perform some function with data
     const result = await listOdds(data);
+    
     // Emit 'event1_response' with the result
-    setInterval(() => {
-      socket.emit('listOdds_response', result);
-    }, 2000);
+    socket.emit('listOdds_response', result);
   });
 
 
