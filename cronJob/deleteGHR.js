1e3ce8fa8b062d10860ee06f8df24ff803e3c2f3
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-26 15:42:19 +0500

    EU time zone

--- cronJob/deleteGHR.js
+++ cronJob/deleteGHR.js
@@ -1,52 +1,24 @@
 const cron = require("node-cron");
-const axios = require("axios");
-const Bets = require("../app/models/bets");
-const CricketMatch = require("../app/models/cricketMatches");
-const User = require("../app/models/user");
-const Settings = require("../app/models/settings");
-const Cash = require("../app/models/deposits");
-const { getParents } = require("../app/routes/bets");
-const ListMarkets = require('../app/models/listMarkets')
 const Events = require('../app/models/events');
-const Odds = require('../app/models/odds');
-const raceOdds = require('../app/models/raceOdds');
-const FancyGames = require("../app/models/fancyGames");
+const raceOdds = require('../app/models/raceOdds'); 
 const raceMarkets = require("../app/models/raceMarkets");
 const moment = require('moment');
 require('./db');
 
-
 const deleteGHR = () => {
   //run after 5 minutes
-cron.schedule('* * * * * *', async () => {
+  cron.schedule('*/5 * * * *', async () => {
     try {
-
       const GHR           = moment(new Date(Date.now() - 10 * 60 * 1000)).format("YYYY-MM-DDTH:mm:ss+00:00");
       const eventIds      = await Events.distinct('meetingId',{ sportsId: { $in: ['7', '4339'] }, openDate : {$lt: GHR}})
       let marketIds       = await raceMarkets.distinct('marketId',{ eventIds : { $in: eventIds } })
       await Events.remove({ meetingId: {  $in: eventIds  } })
       await raceOdds.remove({ marketId: {  $in: marketIds  } })
       await raceMarkets.remove({ marketId: {  $in: marketIds  } })
-    } catch (error) {
+    } 
+    catch (error) {
       console.error('Error running listMarket cron job:', error);
     }
   });
 }
 deleteGHR()
-
-
-const deleteClosedOddsData = () => {
-cron.schedule('*/5 * * * *', async () => {
-    try {
-        let raceMarketIds = await raceMarkets.distinct('marketId',{ status : 'CLOSED' })
-
-        await Events.remove({ marketIds: {  $in: raceMarketIds  } })
-        await raceOdds.remove({ marketId: {  $in: raceMarketIds  } })
-        await raceMarkets.remove({ marketId: {  $in: raceMarketIds  } })
-        
-    } catch (error) {
-      console.error('Error running listMarket cron job:', error);
-    }
-  });
-}
-deleteClosedOddsData()
--- cronJob/deleteSocerOldEvent.js
+++ cronJob/deleteSocerOldEvent.js
@@ -9,11 +9,13 @@ require('./db');
 const deleteSoccerOldEvent = () => {
   cron.schedule('*/5 * * * *', async () => {
     try {
-      // Suppose 
-      const soccer     = moment(new Date(Date.now() - 110 * 60 * 1000)).format("MM/DD/YYYY hh:mm:ss A Z");
-      const eventIds   = await Events.distinct('Id',{ sportsId: '1', openDate : {$lt: soccer}})
+
+      const eventIds_1  = await Events.distinct('Id',{ sportsId: '1', openDate : {$lt: moment(new Date(Date.now() - 110 * 60 * 1000)).format("MM/DD/YYYY h:mm:ss A Z")}})
+      const eventIds_2  = await Events.distinct('Id',{ sportsId: '1', openDate : {$lt: moment(new Date(Date.now() - ((110-180) * 60 * 1000))).format("ddd MMM DD YYYY HH:mm:ss ") + "GMT+0200 (Central European Summer Time)"}})
+      const eventIds    = eventIds_1.concat(eventIds_2)
+
       await ListMarkets.remove({ eventId: { $in: eventIds } })
-       await Events.remove({ Id: { $in: eventIds } })
+      await Events.remove({ Id: { $in: eventIds } })
       await Odds.remove({eventId: { $in: eventIds } } )
       await FancyGames.remove({ eventId: { $in: eventIds }})
         
