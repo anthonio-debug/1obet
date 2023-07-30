793f5d640abff813b34d8061769f313e09bd40e9
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-30 13:45:47 +0500

    Socket interval issue

--- testSocket.js
+++ testSocket.js
@@ -44,7 +44,7 @@ const io = new Server(server,  {
   pingTimeout: 60000,
   cookie: false,
   cors: {
-    origin: "https://1obet.com",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: '*/*',
     credentials: true
