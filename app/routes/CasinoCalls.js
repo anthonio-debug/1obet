e45aebedfef4d1f121aab4ca16b021fb027b3688
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-07-31 20:51:26 +0500

    check

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -99,7 +99,7 @@ async function debit(req, res) {
       },
       { session, readPreference: "primary" }
     );   
-    const user = await User.findOne({ remoteId: payload.remote_id }, { session, readPreference: 'primary' }).exec();  
+    const user = await users.findOne({ remoteId: payload.remote_id }, { session, readPreference: 'primary' }).exec();  
 
     if (!user) {
       await session.abortTransaction();
