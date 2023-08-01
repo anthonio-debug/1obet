5d66bd170b1d27d7fa37b3ce146462b75645a9cf
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-01 05:51:37 +0500

    Minor Test

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -97,7 +97,7 @@ async function debit(req, res) {
 
 
     // 
-    await session.withTransaction(async () => {
+    // await session.withTransaction(async () => {
 
       console.log(`>>>>>>>>>>>>>>>>>>>>>>>>>>> remote_id ${payload.remote_id}`)
       const sameTransId = await casinoCalls.countDocuments(
@@ -157,7 +157,7 @@ async function debit(req, res) {
       await casinoDebits.save();
 
       // 
-    }, transactionOptions);
+    // }, transactionOptions);
 
     await session.commitTransaction();
     return res.json({
