ef70d05fb87a90e45eec546930f3884204ffcc74
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-02 19:04:36 +0500

    moment not defined

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2,6 +2,7 @@ const express = require('express');
 var jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
 const Settings = require('../models/settings');
+const moment = require('moment');
 const User = require('../models/user');
 const settingsValidation = require('../validators/settings');
 const termsAndConditions = require('../models/termsAndConditions');
@@ -21,6 +22,7 @@ const RaceMarkets = require('../models/raceMarkets');
 const RaceOdds = require('../models/raceOdds');
 const loginRouter = express.Router();
 const router = express.Router();
+
 const SelectedCasino = require('../models/selectedCasino');
 
 function updateDefaultTheme(req, res) {
@@ -392,7 +394,7 @@ async function listCompetitions(req, res) {
 async function listEventsBySport(req, res) {
   const sportId = req.query.id;
   try {
-    const date = moment(new Date(Date.now() + 24 *    60 * 60 * 1000)).format("MM/DD/YYYY h:mm:ss +00:00");
+    const date = moment(new Date(Date.now() + 24 *  60 * 60 * 1000)).format("MM/DD/YYYY h:mm:ss +00:00");
 
     let events; 
     if(sportId == "4"){
