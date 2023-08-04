a3b8c30936d03cd83793a008bd0bfe7ac2dd3d10
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-04 19:55:05 +0500

    Test of Odds for Inplay False

--- app/routes/sportsAPI.js
+++ app/routes/sportsAPI.js
@@ -423,26 +423,6 @@ async function listMarketsByCronJob(eventId,sport) {
   try {
     const response = await axios.get(url);
     const marketsData = response.data;
-    // console.log('marketData', marketsData)
-    let sportIds = { "soccer" : "1", "cricket" : "4", "tennis" : "2" }
-
-    var listMarketData = await marketsData.map((element) => ({
-      updateOne: {
-        filter: { marketId: element.marketId },
-        update: {
-          $set: {
-            status: element.status,
-            eventId: eventId,
-            marketName: element.marketName,
-            totalMatched: element.totalMatched,
-            sportsId: sportIds[sport],
-          },
-        },
-        upsert: true,
-      },
-    }));
-
-    const savedEvents = await ListMarket.bulkWrite(listMarketData);
      
     let marketIds = []
     marketsData.forEach(element => {
--- cronJob/GetEventsAndMarkets.js
+++ cronJob/GetEventsAndMarkets.js
@@ -19,19 +19,19 @@ require('../db');
 
 const findOddsForOneTime = async (ids) => {
     try{   
-        console.log("=============== ids ", ids);
-        const marketIds = await Events.distinct("marketIds", { '_id': { $in: ids} });
-        console.log('========= MarketID ', marketIds);
-        let batchArray = [];
-        for (let i = 0; i < marketIds.length; i += 20) {
-        batchArray.push(marketIds.slice(i, i + 20));
-        }
+        // console.log("=============== ids ", ids);
+        const marketId = await Events.distinct("marketIds", { '_id': { $in: ids}});
+        console.log('========= MarketID ', marketId);
+        // let batchArray = [];
+        // for (let i = 0; i < marketIds.length; i += 20) {
+        // batchArray.push(marketIds.slice(i, i + 20));
+        // }
 
-        console.log('========= batchArray', batchArray);
+        // console.log('========= batchArray', batchArray);
 
-        for (let i = 0; i < batchArray.length; i++) {
-        await getnewOdds(batchArray[i]);
-        }
+        // for (let i = 0; i < batchArray.length; i++) {
+        //     await getnewOdds(batchArray[i]);
+        // }
     } catch (error) {
         console.error('Error running odds cron job:', error);
     }
@@ -47,15 +47,15 @@ const GetEventsAndMarkets = async () => {
             const listEventsResponse = await eventsBySupportJobs(sportsId);
             const listEventsData = listEventsResponse.events;
             const  newInsertedIds = listEventsResponse?.newInsertedIds.map((id)=> id._id)
-            const dummydata = listEventsData.map( async(item)=>{
-                let eventId = item.Id
-                let sport = item.sport
-                // console.log("Sports Name", sport)
-                let listMarketsResponse = await listMarketsByCronJob(eventId,sport);
-            })
-
+            for (let i = 0; i < listEventsData.length; i++) {
+                let eventId = listEventsData[i].Id
+                let sport = listEventsData[i].sport
+                await listMarketsByCronJob(eventId,sport);
+            }
             if(newInsertedIds.length > 0){
-                await findOddsForOneTime(newInsertedIds)
+                // setTimeout( async () => {
+                    await findOddsForOneTime(newInsertedIds)
+                // }, 1500);
             }
         }
         } catch (error) {
