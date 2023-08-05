d9db5b84bf59ef3a09d38103b0942070fd10aefa
MUmarRasheed <sfsumarr@gmail.com>
2023-08-04 23:58:58 -0700

    return all selected games for testing

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1044,7 +1044,7 @@ async function bettorDashboardGames(req, res) {
       },
     ]).exec();
 
-    const selectedCasinoData = await SelectedCasino.find({ 'games.isDashboard': true }, { 'games.$': 1 });
+    const selectedCasinoData = await SelectedCasino.find({});
 
     const organizedEvents = {
       soccer: events[0].soccer,
