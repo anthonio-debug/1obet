5a0d174842ac5e6efbe0c2d34c63fabb793ab252
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-09 21:35:12 +0500

    AvailableToLay

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -191,7 +191,7 @@ async function placeBet(req, res) {
           console.log(`No availableToBack odds matched with the bet rate ${betRate}`);
           return res.status(404).send({ message: `No availableToBack odds matched with the bet rate ${req.body.betRate}` });
         }
-        selectedOddsRate = availableToBack[matchedIndex].price;
+        selectedOddsRate = AvailableToLay[matchedIndex].price;
 
       } else {
         console.log('Invalid type value. Type should be 0 or 1.');
