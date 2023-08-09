7d21373174414143a79f0a67180e933e7b9ca063
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-09 20:45:13 +0500

    index finding test

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -167,9 +167,13 @@ async function placeBet(req, res) {
 
         const availableToBack   = OddDetailsTeam.ExchangePrices.AvailableToBack;
         console.log('availableToBack', availableToBack);
-        const matchedBack = availableToBack.filter((back, index) => {
-          if(back.price == betRate)
-            return index 
+        // const matchedBack = availableToBack.filter((back, index) => {
+        //   if(back.price == betRate){
+        //     return index 
+        //   }
+        // });
+        const matchedBack = availableToBack.filter((back) => {
+          return back.price === betRate;
         });
 
         console.log('matchedBack', matchedBack);
