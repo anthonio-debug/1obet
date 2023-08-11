c763a9bcd10874f658e4edb273a6b9195e30a9aa
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-11 20:37:51 +0500

    oddsData[0]

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -245,7 +245,8 @@ async function placeBet(req, res) {
       const url = `${config.horseRaceUrl}/odds/?ids=${market}`;
       const response = await axios.get(url);
       const oddsData = response.data;
-      console.log("oddsData Runners ====== ", oddsData[0].runners);
+      
+      console.log("oddsData Runners ====== ", oddsData[0]);
       if (!oddsData) {
         console.log(`Match odds not found for sports ID ${sportsId}`);
         return res.status(404).send({ message: `Bet mis match` });
