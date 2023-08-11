075364d29286528ba2700709a114a531ef6e15c2
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-11 19:03:18 +0500

    current position server

--- app/routes/currentPosition.js
+++ app/routes/currentPosition.js
@@ -118,16 +118,16 @@ function getCurrentPosition_old(req, res) {
 //   prev = current;
 // });
 
-for (const user of parentUser) {
-  let CurrentPosition = await new CurrentPosition({
-    userId: user.userId,
-    description: "some transection name",
-    amount: -(user.commission / 100) * remainingAmount,
-    betId: bet._id,
-    matchId: matchId,
-  });
-  await CurrentPosition.save();
-}
+// for (const user of parentUser) {
+//   let CurrentPosition = await new CurrentPosition({
+//     userId: user.userId,
+//     description: "some transection name",
+//     amount: -(user.commission / 100) * remainingAmount,
+//     betId: bet._id,
+//     matchId: matchId,
+//   });
+//   await CurrentPosition.save();
+// }
 
   
 
