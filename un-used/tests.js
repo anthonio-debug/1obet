a1787d2c86f5ff0ac7ed16d02df8b44e96a2d877
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-12 16:40:47 +0500

    Testing Script

--- un-used/tests.js
+++ un-used/tests.js
@@ -1,46 +1,12 @@
+const axios = require('axios');
+
 
 async function eventsBySupportJobs() {
   const url = `https://streamingtv.fun:3440/api/listEventsBySport/4`;
   try {
     const response = await axios.get(url);
-    const events = response.data; 
-    if(events.length > 0){
-      var sportsEventData = events.map((element) => ({
-        updateOne: {
-          filter: { Id: element.Id },
-          update: {
-            $set: {
-              sportsId: sportsId,
-              sport: element.sport,
-              competitionId: element.competitionId,
-              competitionName: element.competitionName,
-              Id: element.Id,
-              name: element.name,
-              countryCode: element.countryCode,
-              timezone: element.timezone,
-              openDate: element.openDate,
-              inplay: element.inplay,
-              hasFancy: element.hasFancy,
-              status: element.status,
-              isPremium: element.isPremium,
-              type: element.type,
-              matchType: getMatchType(element.competitionName, element.name, sportsId)
-            },
-          },
-          upsert: true,
-        },
-      }));
-    }
+    console.log("=========", response);
 
-    // 1"obet.com/*"
-    const savedEvents = await inPlayEvents.bulkWrite(sportsEventData);
-    // console.log('===== Saved Events bulkWrite logs ', savedEvents?.result?.upserted)
-    return({
-      success: true,
-      message: 'Events retrieved and saved successfully',
-      events: events,
-      newInsertedIds: savedEvents?.result?.upserted
-    });
   } catch (error) {
     console.error(error);
     return({
