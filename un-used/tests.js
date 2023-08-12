0afb7259dd0721987777c3ef0402c3d29991bccb
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-12 16:38:29 +0500

    test script

--- un-used/tests.js
+++ un-used/tests.js
@@ -1,26 +1,54 @@
-const axios   = require('axios');
-const config  = require('config');
-// require('./db');
-async function name() {
-  const selectionId = 46636793;
-  const market = 1.216943899
-  const url = `http://136.244.77.249:33333/odds/?ids=${market}`;
-  const response = await axios.get(url);
-  const oddsData = response.data;
-  
-  
-  // console.log("oddsData Runners ====== ", oddsData);
-  if (oddsData.length == 0) {
-    console.log(`Match odds not found for sports ID`);
-    // return res.status(404).send({ message: `Bet mis match` });
+
+async function eventsBySupportJobs() {
+  const url = `https://streamingtv.fun:3440/api/listEventsBySport/4`;
+  try {
+    const response = await axios.get(url);
+    const events = response.data; 
+    if(events.length > 0){
+      var sportsEventData = events.map((element) => ({
+        updateOne: {
+          filter: { Id: element.Id },
+          update: {
+            $set: {
+              sportsId: sportsId,
+              sport: element.sport,
+              competitionId: element.competitionId,
+              competitionName: element.competitionName,
+              Id: element.Id,
+              name: element.name,
+              countryCode: element.countryCode,
+              timezone: element.timezone,
+              openDate: element.openDate,
+              inplay: element.inplay,
+              hasFancy: element.hasFancy,
+              status: element.status,
+              isPremium: element.isPremium,
+              type: element.type,
+              matchType: getMatchType(element.competitionName, element.name, sportsId)
+            },
+          },
+          upsert: true,
+        },
+      }));
+    }
+
+    // 1"obet.com/*"
+    const savedEvents = await inPlayEvents.bulkWrite(sportsEventData);
+    // console.log('===== Saved Events bulkWrite logs ', savedEvents?.result?.upserted)
+    return({
+      success: true,
+      message: 'Events retrieved and saved successfully',
+      events: events,
+      newInsertedIds: savedEvents?.result?.upserted
+    });
+  } catch (error) {
+    console.error(error);
+    return({
+      success: false,
+      message: 'Failed to get or save events',
+      error: error.message,
+    });
   }
-  
-  console.log('oddsData runners =========== ', oddsData[0]?.runners);
-  // console.log('selectionId ========= ', selectionId);
-  
-  const runnerFromAPI = oddsData[0].runners.find((runner) => {
-    return runner.selectionId == selectionId
-  });
-  console.log('matchOdds runners ====== ', runnerFromAPI);
 }
-name()
+
+eventsBySupportJobs()
