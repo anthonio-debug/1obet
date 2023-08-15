9d6a7b559a1e6baeb03f7dfb3806d1daaada6e56
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-16 02:02:28 +0500

    Projection errror

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1071,20 +1071,20 @@ async function bettorDashboardGames(req, res) {
           as: 'odds'
         }
       },
-      {
-        $project: {
-          _id: 1,
-          Id: 1,
-          openDate: 1,
-          name: 1,
-          competitionName: 1,
-          inplay: 1,
-          oddsData: {
-            $slice: ["$odds", 1]
-          }
-        }
-      },
-    ]).exec();
+      // {
+      //   $project: {
+      //     _id: 1,
+      //     Id: 1,
+      //     openDate: 1,
+      //     name: 1,
+      //     competitionName: 1,
+      //     inplay: 1,
+      //     oddsData: {
+      //       $slice: ["$odds", 1]
+      //     }
+      //   }
+      // },
+    ])
 
 
     const organizedEvents = {
