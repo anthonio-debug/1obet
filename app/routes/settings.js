3b618a7d3a53ea43273cdf209a4497cf8e459d92
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-15 23:33:18 +0500

    oddsData: {                   $slice: ["$odds", 1]                 }

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -980,7 +980,7 @@ async function bettorDashboardGames(req, res) {
           marketIds: 1,   
           sportsId: 1,
           iconStatus: 1,
-          oddData: { $slice: ["$odds.runners", 1] }   
+          oddsData: { $slice: ["$odds", 1]} 
         }
       },
       {
