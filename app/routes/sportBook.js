84f6ad1b83a044a449188f7de675492aab468be4
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-18 13:39:28 +0500

    Sports Book

--- app/routes/sportBook.js
+++ app/routes/sportBook.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const loginRouter = express.Router();
+const axios = require('axios')
 
 async function sportsBook(req, res) {
   try {
-    // https://em-api.thegameprovider.com/api/seamless/provider
+    // 
+    const response = await axios.post("https://em-api.thegameprovider.com/api/seamless/provider", {
+        api_password: config.api_password,
+        api_login: config.api_username,
+        method: 'getGameList',
+        show_additional: true,
+        show_systems: 1,
+        currency: 'PKR',
+      });
+
+      return res.send({
+        success: true,
+        message: 'Markets updated successfully',
+        data: response
+      });
+
   } catch (err) {
     console.error(err);
     res.status(404).send({ message: 'betlock not saved' });
