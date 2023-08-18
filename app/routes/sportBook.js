2adf3424f742188d458911273fddfa5a99292159
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-18 17:23:29 +0500

    get All Sports Book Games

--- app/routes/sportBook.js
+++ app/routes/sportBook.js
@@ -26,13 +26,45 @@ async function sportsBook(req, res) {
 
   } catch (err) {
     console.error(err);
-    res.status(404).send({ message: 'betlock not saved' });
+    res.status(404).send({ message: 'Something went wrong' });
   }
 }
+
+async function getAllSportsBookGames(req, res) {
+    try {
+        const response = await axios.post("https://em-api.thegameprovider.com/api/seamless/provider",{
+            "api_password": "b16gWs7e0QAASLTne0",
+            "api_login": "1obet_mc_s",
+            "method": "getGameDirect",
+            "show_systems": 1,
+            "show_additional": true,
+            "currency": "PKR"
+        });
+  
+        return res.send({
+        success: true,
+        message: 'Markets updated successfully',
+        data: response.data
+        });
+  
+    } catch (err) {
+        console.error(err);
+        res.status(404).send({ message: 'betlock not saved' });
+    }
+}
+
+
+
 
 loginRouter.get(
   '/sportsBook',
   sportsBook
 );
+loginRouter.get(
+    '/getAllSportsBookGames',
+    getAllSportsBookGames
+  );
+
+
 
 module.exports = { loginRouter };
