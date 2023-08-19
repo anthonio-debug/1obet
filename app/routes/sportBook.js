ec0111dba06ee3c649dd4d293ffdae280790dc8a
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-19 12:21:53 +0500

    change in order of API

--- app/routes/sportBook.js
+++ app/routes/sportBook.js
@@ -4,6 +4,46 @@ const axios         = require('axios');
 const User          = require('../models/user');
 const SportsBooks   = require('../models/sportsBook');
 
+async function getAllSportsBookGamesFromThirdParty(req, res) {
+  try {
+    const response = await axios.post("https://em-api.thegameprovider.com/api/seamless/provider",{
+        "api_login": "1obet_mc_s",
+        "api_password": "b16gWs7e0QAASLTne0",
+        "method": "getGameList",
+        "show_systems": 1,
+        "show_additional": true,
+        "currency": "PKR"
+    });
+
+    const games = response.data.response.map((element) => ({
+      updateOne: {
+        filter: { id: element.id },
+        update: {
+          $set: {
+            id_hash: element.id_hash,
+            name: element.name,
+            type: element.type,
+            subcategory: element.subcategory,
+            category: element.category,
+            gameName: element.gamename,
+            image_preview: element.image_preview,
+            provider_name: element.provider_name
+          },
+        },
+        upsert: true,
+      },
+    }));
+
+    const SportsBookRes = await SportsBooks.bulkWrite(games);
+    return res.send({
+      success: true,
+      message: 'Markets updated successfully',
+    });
+  } catch (err) {
+      console.error(err);
+      res.status(404).send({ message: 'Something went wrong ' });
+  }
+}
 
 async function sportGamesList(req, res) {
   try {
@@ -47,47 +87,7 @@ async function sportsBook(req, res) {
   }
 }
 
-async function getAllSportsBookGamesFromThirdParty(req, res) {
-  try {
-    const response = await axios.post("https://em-api.thegameprovider.com/api/seamless/provider",{
-        "api_login": "1obet_mc_s",
-        "api_password": "b16gWs7e0QAASLTne0",
-        "method": "getGameList",
-        "show_systems": 1,
-        "show_additional": true,
-        "currency": "PKR"
-    });
-
-    const games = response.data.response.map((element) => ({
-      updateOne: {
-        filter: { id: element.id },
-        update: {
-          $set: {
-            id_hash: element.id_hash,
-            name: element.name,
-            type: element.type,
-            subcategory: element.subcategory,
-            category: element.category,
-            gameName: element.gamename,
-            image_preview: element.image_preview,
-            provider_name: element.provider_name
-          },
-        },
-        upsert: true,
-      },
-    }));
-
-    const SportsBookRes = await SportsBooks.bulkWrite(games);
-    return res.send({
-      success: true,
-      message: 'Markets updated successfully',
-    });
-  } catch (err) {
-      console.error(err);
-      res.status(404).send({ message: 'Something went wrong ' });
-  }
-}
-loginRouter.get( '/sportGamesList', sportGamesList );
 loginRouter.get( '/getAllSportsBookGamesFromThirdParty', getAllSportsBookGamesFromThirdParty );
+loginRouter.get( '/sportGamesList', sportGamesList );
 loginRouter.get( '/sportsBook', sportsBook );
 module.exports = { loginRouter };
