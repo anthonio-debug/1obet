ec68a5989cfe24f49eb19f9847846a9f7e32b198
ysfm <ysfm@jsistem.com>
2023-08-20 14:59:50 -0400

    Models
    New Models

--- app/models/events.js
+++ app/models/events.js
@@ -17,6 +17,10 @@ const inPlayEventsSchema = new mongoose.Schema({
   timezone: { type: String },
   openDate: { type: Number },
   inplay: { type: Boolean, index: true },
+  inplayFromServer: { type: Boolean, index: true },
+  lastCheckMarket: { type: Number, default: 0 },
+  isShowed:{ type: Boolean, index: true, default:false },
+
   hasFancy: { type: Boolean },
   status: { type: String },
   isPremium:{type: Boolean },
