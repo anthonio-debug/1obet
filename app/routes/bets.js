dc22df0fc683131f060b8dce4415cf1dea36570a
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-26 16:44:11 +0500

    event data excluded in Figure battig

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -148,7 +148,8 @@ const placeBet = async (req, res) => {
       if (!subMarketDetail) {
         return res.status(404).send({ message: 'Bet not allowed' });
       }
-    } else {
+    } else if (config.FigureEvenOddSmallBig.includes(subMarketDetail.Id)){
+    }else {
       const currentMarket =  eventDetail.marketIds.find((market) => market.marketName ==  subMarketName );
       id = currentMarket.id;
       subMarketDetail = await SubMarketType.findOne({ name: subMarketName, marketId: marketId }).exec();
@@ -459,7 +460,7 @@ const placeBet = async (req, res) => {
       }
     }
 
-    if(FigureEvenOddSmallBig.includes(subMarketDetail.Id)){
+    if(config.FigureEvenOddSmallBig.includes(subMarketDetail.Id)){
       winningAmount = betAmount;
       loosingAmount = betAmount;
     }
