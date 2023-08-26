2e75aba48ffc56f7f974aebc6917784d1c46ce76
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-27 02:56:07 +0500

    static id

--- app/routes/sportsHighlights.js
+++ app/routes/sportsHighlights.js
@@ -10,7 +10,7 @@ async function getAllSportsHighlight(req, res) {
     const sportId = req.query.sport;
     const sportsHighlights = await inPlayEvents.aggregate([
       { $match: { 
-        sportsId: sportId,
+        sportsId: "4",
         openDate: {
           $gte: new Date().getTime() -12*60*60*1000
         }
