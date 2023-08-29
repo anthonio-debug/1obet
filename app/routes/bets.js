7c208a9babed1b2faccf092cb568cad5103f8435
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-29 20:56:25 +0500

    DbBackScores

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -359,20 +359,24 @@ const placeBet = async (req, res) => {
         runnerName = dbSelectedOdds.nat; // Get the runner name from the 'nat' field
         
         if (req.body.type == 0) {
-          const apiBackOdds   = [apiSelectedOdds.bs1, apiSelectedOdds.bs2, apiSelectedOdds.bs3];
-          const DbBackOdds    = [dbSelectedOdds.bs1, dbSelectedOdds.bs2, dbSelectedOdds.bs3];
-          const DbBackScores  = [dbSelectedOdds.b1, dbSelectedOdds.b2, dbSelectedOdds.b3];
-          const index         = DbBackOdds.indexOf(betRate)
-          TargetScore         = DbBackScores[index]
+          const apiBackOdds2   = [apiSelectedOdds.bs1, apiSelectedOdds.bs2, apiSelectedOdds.bs3];
+          const apiBackOdds = apiBackOdds.map( item => Number(item) )
 
-          // if (index == -1) {
-          //   console.log(`No availableToBack odds matched with the bet rate ${betRate}`);
-          //   return res.status(404).send({ message: `Bet miss matched` });
-          // }
-          // if(apiBackOdds[index] < betRate ){
-          //   console.log(`No availableToBack odds matched with the bet rate ${betRate}`);
-          //   return res.status(404).send({ message: `Bet miss matched` });
-          // }
+          const DbBackOdds    = [dbSelectedOdds.bs1, dbSelectedOdds.bs2, dbSelectedOdds.bs3];
+
+          const DbBackScores2  = [dbSelectedOdds.b1, dbSelectedOdds.b2, dbSelectedOdds.b3];
+          const DbBackScores   = DbBackScores2.map( item => Number(item) )
+          const index          = DbBackOdds.indexOf(betRate)
+          TargetScore          = DbBackScores[index]
+
+          if (index == -1) {
+            console.log(`No availableToBack odds matched with the bet rate ${betRate}`);
+            return res.status(404).send({ message: `Bet miss matched` });
+          }
+          if(apiBackOdds[index] < betRate ){
+            console.log(`No availableToBack odds matched with the bet rate ${betRate}`);
+            return res.status(404).send({ message: `Bet miss matched` });
+          }
         } 
         else if (req.body.type == 1) {
           const apiBackOdds   = [apiSelectedOdds.ls1, apiSelectedOdds.ls2, apiSelectedOdds.ls3];
@@ -409,9 +413,6 @@ const placeBet = async (req, res) => {
       console.log(" url ===== ", url);
       const response      = await axios.get(url);
       console.log("response ========", response.data);
-      // debugger;
-
-
         
       if(! response?.data?.data?.t2?.length){
         console.log(`Odds not available for the selected team ||||||  ${req.body.selectionId}`);
@@ -457,7 +458,6 @@ const placeBet = async (req, res) => {
           //   return res.status(404).send({ message: `Bet miss matched` });
           // }
         } 
-        // 3.5 "3.5" to float 
         else if (req.body.type == 1) {
           const apiBackOdds   = [apiSelectedOdds.l1, apiSelectedOdds.l2, apiSelectedOdds.l3];
           const DbBackOdds    = [dbSelectedOdds.l1, dbSelectedOdds.l2, dbSelectedOdds.l3];
