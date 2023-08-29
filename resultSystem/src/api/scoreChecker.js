3cff0174dc33b73902d5edbf0cce6b50611ddc93
ysfm <ysfm@jsistem.com>
2023-08-29 12:03:58 -0400

    Fancy Checker

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -92,7 +92,7 @@ function scoreChecker() {
                 const bets = await Bets.find({ marketId: betData.marketId, sportsId: betData.sportsId, status: 1 });
 
                 await newRecord.save();
-                await Bets.findOneAndUpdate({ _id: betData._id }, { betData: newRecord._id });
+                await Bets.findOneAndUpdate({ _id: betData._id }, { resultId: newRecord._id });
 
                 if (result.winnerSelectionId == -1) {
                     console.log("This event Canceled ");
@@ -147,7 +147,7 @@ function scoreChecker() {
 
 
                 await newRecord.save();
-                await Bets.findOneAndUpdate({ _id: betData._id }, { betData: newRecord._id });
+                await Bets.findOneAndUpdate({ _id: betData._id }, { resultId: newRecord._id });
 
                 const bets = await Bets.find({ matchId: event._id.toString(), isfancyOrbookmaker: true, fancyData: null, status: 1 });
 
@@ -214,7 +214,7 @@ function scoreChecker() {
                 });
 
                 await newRecord.save();
-                await Bets.findOneAndUpdate({ _id: betData._id }, { betData: newRecord._id });
+                await Bets.findOneAndUpdate({ _id: betData._id }, { resultId: newRecord._id });
 
 
                 const bets = await Bets.find({ matchId: event._id.toString(), isfancyOrbookmaker: true, fancyData: { $ne: null }, status: 1 });
