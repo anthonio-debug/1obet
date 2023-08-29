77037f6a9ca966ef5b903851761caf6f02f0c850
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-08-29 19:39:43 +0500

    dbFancyOdds

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -421,12 +421,14 @@ const placeBet = async (req, res) => {
       const DBOddDetails  = await FancyOdds.findById(oddsId);
       const dbFancyOdds   = DBOddDetails?.data?.data?.t2[0]?.bm1
 
+      console.log(" DBOddDetails ==== ", DBOddDetails)
+        console.log(" dbFancyOdds ==== ", dbFancyOdds)
+
       if (apiFancyOdds.length && dbFancyOdds.length){
         const apiSelectedOdds = apiFancyOdds.find(runner => runner.sid == req.body.selectionId);
         const dbSelectedOdds  = dbFancyOdds.find(runner  => runner.sid == req.body.selectionId);
 
-        console.log(" apiSelectedOdds ==== ", apiSelectedOdds)
-        console.log(" dbSelectedOdds ==== ", dbSelectedOdds)
+        
 
 
 
