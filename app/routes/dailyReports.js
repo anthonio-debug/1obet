25b26873b16fd51bb4cb95b48ec2bd16a775ea94
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-09-01 05:43:44 +0500

    dailySportsWiseReport

--- app/routes/dailyReports.js
+++ app/routes/dailyReports.js
@@ -199,11 +199,204 @@ const dailyMarketsReports = async (req, res) => {
 */ 
 
 
+async function getAllChildrens(createdByIDs) {
+  const userIDs = [];
+  const queriedUserIDs = new Set(); // Keep track of queried user IDs
+
+  if (createdByIDs.length === 0) {
+    return userIDs;
+  }
+
+  const uniqueIDs = Array.from(new Set(createdByIDs)); // Remove duplicate IDs
+
+  const users = await User.find(
+    { createdBy: { $in: uniqueIDs } },
+    { userId: 1, userName: 1, createdBy: 1 }
+  ).lean();
+
+  for (const user of users) {
+    if (!queriedUserIDs.has(user.userId)) {
+      userIDs.push(user.userId);
+      queriedUserIDs.add(user.userId);
+    }
+  }
+
+  const subUserIDs = await getAllChildrens(userIDs);
+  userIDs.push(...subUserIDs);
+  return Array.from(new Set(userIDs)); // Ensure unique user IDs in the final array
+}
+
+const getDailyReport = async(req, res) => {
+  const errors = validationResult(req);
+  if (errors.errors.length !== 0) {
+    return res.status(400).send({ errors: errors.errors });
+  }
+
+  const userId      = parseInt(req.decoded.userId)
+  const currentUser = await User.findOne({ userId: userId});
+  const users       = [currentUser.createdBy, userId];
+  let parents       = [userId]
+  do{
+    childUsers      = await User.distinct("userId", {
+      createdBy: {
+        $in: parents
+      }
+    });
+    console.log(" childUsers ======= ", childUsers);
+    if(childUsers.length)
+      users.push(...childUsers)
+    parents = childUsers
+  }while (childUsers.length > 0)
+
+  const response = await CashDeposit.aggregate([
+    {  
+      $match: {
+        userId: {
+          $in: users
+        },
+        cashOrCredit: { $in: ["Bet", "Commission", "loosing"] },
+        $and: [
+          {
+            createdAt: {$gte: req.query.startDate}
+          },
+          {
+            createdAt: {$lte: req.query.endDate}
+          }
+        ]
+      }
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'userId',
+        foreignField: 'userId',
+        as: 'userInfo'
+      }
+    }, 
+    {
+      $group:{
+        _id: "$userId",
+        amount: { $sum: "$amount"},
+        name: { $first: { $arrayElemAt: ["$userInfo.userName", 0] } }
+      }
+    }
+  ]);
+
+  return res.send({
+    success: true,
+    message: 'Commission reports',
+    results: response,
+  });
+
+}
+
+const dailySportsWiseReport = async (req, res) => {
+  const errors = validationResult(req);
+  if (errors.errors.length !== 0) {
+    return res.status(400).send({ errors: errors.errors });
+  }
+
+    const Id        =  parseInt(req.query.userId)
+    console.log(" Id ========== ", Id);
+    const response = await CashDeposit.aggregate([
+      {  
+        $match: {
+          userId: Id,
+          cashOrCredit: { $in: ["Bet", "Commission", "loosing"] },
+          $and: [
+            {
+              createdAt: {$gte: req.query.startDate}
+            },
+            {
+              createdAt: {$lte: req.query.endDate}
+            }
+          ]
+        }
+      },
+      {
+        $lookup: {
+          from: 'markettypes',
+          localField: 'sportsId',
+          foreignField: 'Id',
+          as: 'marketInfo'
+        }
+      }, 
+      {
+        $group:{
+          _id: "$sportsId",
+          amount: { $sum: "$amount"},
+          userId: { $first: "$userId" },
+          name: { $first: { $arrayElemAt: ["$marketInfo.name", 0] } }
+        }
+      }
+    ]);
+
+    return res.send({
+      success: true,
+      message: 'Market wise Reports !',
+      results: response,
+    });
+}
+
+const dailyMatchWiseReports = async (req, res) => {
+  const errors = validationResult(req);
+  if (errors.errors.length !== 0) {
+    return res.status(400).send({ errors: errors.errors });
+  }
+  
+  const userId = req.decoded.userId
+  console.log(" userId ====== ", userId);
+  const Id =  parseInt(req.query.userId)
+
+  const response = await CashDeposit.aggregate([
+    {  
+      $match: {
+        userId: Id,
+        sportsId: req.query.sportsId,
+        cashOrCredit: { $in: ["Bet", "Commission", "loosing"] },
+        $and: [
+          {
+            createdAt: {$gte: req.query.startDate}
+          },
+          {
+            createdAt: {$lte: req.query.endDate}
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        'betIdEvent': { $toObjectId: "$betId" }
+      }
+    },
+    {
+      $lookup: {
+        from: 'bets',
+        localField: 'betIdEvent',
+        foreignField: '_id',
+        as: 'bets'
+      }
+    }, 
+    {
+      $group:{
+        _id: {$arrayElemAt: ["$bets.matchId", 0]},
+        amount: { $sum: "$amount"},
+        userId: { $first: "$userId" },
+        name: { $first: { $arrayElemAt: ["$bets.event", 0] } }
+      }
+    }
+  ]);
+  return res.send({
+    success: true,
+    message: 'Sport wise Reports !',
+    results: response,
+  });
+}
 
 
 
-loginRouter.get('/getDailyReport',reportValidator.validate('getDailyPLReport'), getDailyReport);
-loginRouter.get('/dailySportsWiseReport',reportValidator.validate('dailyPLSportsWiseReport'), dailySportsWiseReport);
-loginRouter.get('/dailyMarketsReports',reportValidator.validate('dailyPlMarketsReports'), dailyMarketsReports);
+loginRouter.get('/getDailyReport', getDailyReport);
+loginRouter.get('/dailySportsWiseReport', dailySportsWiseReport);
+loginRouter.get('/dailyMatchWiseReports',  dailyMatchWiseReports);
 
 module.exports = { loginRouter };
