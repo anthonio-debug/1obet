8271cf6af1b6b0123bee327fa57f17b2feb16e48
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-09-02 02:47:05 +0500

    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

--- app/routes/dailyReports.js
+++ app/routes/dailyReports.js
@@ -305,7 +305,7 @@ const getDailyReport = async(req, res) => {
     {
       $group:{
         _id: "$userId",
-        // parent: ,
+        parent: true,
         amount: { $sum: "$upLineAmount"},
         name: { $first: { $arrayElemAt: ["$userInfo.userName", 0] } }
       }
