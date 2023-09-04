aae13046edce2de60838d411eeeeb09571c0c350
bhattitayab <bhattitayab747@gmail.com>
2023-09-04 17:42:19 +0500

    getMatchType Funtion replaced  in ysfm code

--- restApiSystem/src/api/apiRequestsSCT.js
+++ restApiSystem/src/api/apiRequestsSCT.js
@@ -562,16 +562,29 @@ function apiRequests() {
 
 
 function getMatchType(competitionName, name, sportsId) {
-  const keywords = /(T20|Twenty20|ODI|One Day|T10|Ten10|Test)/i;
+  const keywords = /(T20|twenty20|Twenty20|twenty 20|Twenty 20|ODI|One Day|one day|T10|Ten10||ten 10|Ten 10|Test|TEST)/i;
   if (sportsId == '4') {
     const nameMatch = name.match(keywords);
     const competitionNameMatch = competitionName && competitionName.match(keywords);
-
-    if (nameMatch) {
-      return nameMatch[0];
+    let returnMatch = ""
+    if (nameMatch){
+      returnMatch = nameMatch[0];
     } else if (competitionNameMatch) {
-      return competitionNameMatch[0];
+      returnMatch = competitionNameMatch[0];
     }
+
+    if(["ODI", "One Day", "one day"].includes(returnMatch)){
+      returnMatch = "ODI"
+    }
+    else if(["Test", "test"].includes(returnMatch)){
+      returnMatch = "TEST"
+    }
+    else if(["twenty20", "Twenty20", "Twenty 20", "twenty 20"].includes(returnMatch)){
+      returnMatch = "T20"
+    }
+    else if(["T10", "Ten10", "ten 10", "Ten 10"].includes(returnMatch)){
+      returnMatch = "T10"
+    }
+    return returnMatch;
   }
-  return '';
 }
