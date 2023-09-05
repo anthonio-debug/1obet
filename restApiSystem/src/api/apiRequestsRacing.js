7efea4a3f165ec257ed15bb87c4c21fd7a54aae5
ysfm <ysfm@jsistem.com>
2023-09-05 09:23:48 -0400

    Update apiRequestsRacing.js

--- restApiSystem/src/api/apiRequestsRacing.js
+++ restApiSystem/src/api/apiRequestsRacing.js
@@ -58,7 +58,7 @@ function apiRequests() {
       const response = await axios.get('http://136.244.77.249:33333/results/?ids='+resultCheckerArray[0].marketId);
       const lastResults = response.data;
 
-      console.log('Last results',checking_array.join(','),lastResults);
+      console.log('Last results',resultCheckerArray[0], resultCheckerArray.length ,lastResults);
 
       for (let index = 0; index < lastResults.length; index++) {
           const element = lastResults[index];
