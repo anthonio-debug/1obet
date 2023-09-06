192bef9868ebdbb3506fe07ccd0c87e009e5a090
bhattitayab <bhattitayab747@gmail.com>
2023-09-07 00:55:54 +0500

    2nd Profit Lose API

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1662,15 +1662,15 @@ const EventWiseprofitLose = async(req, res) => {
           as: 'bets'
         }
       }, 
-      // {
-      //   $group:{
-      //     _id: {$arrayElemAt: ["$bets.matchId", 0]},
-      //     amount: { $sum: "$amount"},
-      //     userId: { $first: "$userId" },
-      //     date: { $first: "$date" },
-      //     name: { $first: { $arrayElemAt: ["$bets.event", 0] } },
-      //   }
-      // }
+      {
+        $group:{
+          _id: {$arrayElemAt: ["$bets.matchId", 0]},
+          amount: { $sum: "$amount"},
+          userId: { $first: "$userId" },
+          date: { $first: "$date" },
+          name: { $first: { $arrayElemAt: ["$bets.event", 0] } },
+        }
+      }
     ]);
     return res.send({
       success: true,
