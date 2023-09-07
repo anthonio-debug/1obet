b637953eb93d4aecf62316371722f8a2ce0fa3c4
ysfm <ysfm@jsistem.com>
2023-09-07 17:43:49 -0400

    Update toolStart.js

--- result-saver-src/toolStart.js
+++ result-saver-src/toolStart.js
@@ -44,7 +44,7 @@ function toolStart() {
                 $match: {
                     readyForScore: { $ne: true },
                     winnerInfo: null,
-                    runners: { $ne: null },
+                    //runners: { $ne: null },
                     sportID:{ $in: [1,2,4] },
                     status: 'OPEN',
                     "eventData.isShowed": true,
