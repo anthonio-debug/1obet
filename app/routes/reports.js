ae38401e8d94249e513b9439d8d3f81355c3d45a
ysfm <ysfm@jsistem.com>
2023-09-07 21:38:29 -0400

    Update reports.js

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -244,7 +244,7 @@ async function getFinalReport(req, res) {
       if (userRecord.clientPL != userRecord.cash) {
         usedValue = userRecord.cash + userRecord.balance;
       } else {
-        usedValue = userRecord.clientPL;
+        usedValue = userRecord.clientPL + userRecord.balance;
       }
 
     }
