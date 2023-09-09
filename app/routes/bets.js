27598d0f3abc7775ba7500bd5e411a8aad35eac7
bhattitayab <bhattitayab747@gmail.com>
2023-09-09 16:31:10 +0500

    BS1 B1 LS1 LS1

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -359,23 +359,22 @@ const placeBet = async (req, res) => {
 
         if (req.body.type == 0) {
 
-          const apiBackOdds2 = [apiSelectedOdds.bs1, apiSelectedOdds.bs2, apiSelectedOdds.bs3];
+          const apiBackOdds2 = [apiSelectedOdds.b1, apiSelectedOdds.b2, apiSelectedOdds.b3];
           const apiBackOdds = apiBackOdds2.map(item => Number(item))
 
-          const DbBackOdds2 = [dbSelectedOdds.bs1, dbSelectedOdds.bs2, dbSelectedOdds.bs3];
+          const DbBackOdds2 = [dbSelectedOdds.b1, dbSelectedOdds.b2, dbSelectedOdds.b3];
           const DbBackOdds = DbBackOdds2.map(item => Number(item))
 
-          const DbBackScores2 = [dbSelectedOdds.b1, dbSelectedOdds.b2, dbSelectedOdds.b3];
+          const DbBackScores2 = [dbSelectedOdds.bs1, dbSelectedOdds.bs2, dbSelectedOdds.bs3];
           const DbBackScores = DbBackScores2.map(item => Number(item))
           console.log(" DbBackOdds ============ ", DbBackOdds);
           const index = DbBackOdds.indexOf(betRate)
           TargetScore = DbBackScores[index]
 
-
           if (index == -1) {
             console.log(`index ================== ${index}`);
             console.log(`betRate ==================  ${betRate}`);
-            return res.status(404).send({ message: `Index miss matched` });
+            return res.status(404).send({ message: `Bet miss matched` });
           }
           if (apiBackOdds[index] < betRate) {
             console.log(`No availableToBack odds matched with the bet rate ${betRate}`);
@@ -383,13 +382,13 @@ const placeBet = async (req, res) => {
           }
         }
         else if (req.body.type == 1) {
-          const apiBackOdds2 = [apiSelectedOdds.ls1, apiSelectedOdds.ls2, apiSelectedOdds.ls3];
+          const apiBackOdds2 = [apiSelectedOdds.l1, apiSelectedOdds.l2, apiSelectedOdds.l3];
           const apiBackOdds = apiBackOdds2.map(item => Number(item));
 
-          const DbBackOdds2 = [dbSelectedOdds.ls1, dbSelectedOdds.ls2, dbSelectedOdds.ls3];
+          const DbBackOdds2 = [dbSelectedOdds.l1, dbSelectedOdds.l2, dbSelectedOdds.l3];
           const DbBackOdds = DbBackOdds2.map(item => Number(item));
 
-          const DbBackScores2 = [dbSelectedOdds.l1, dbSelectedOdds.l2, dbSelectedOdds.l3];
+          const DbBackScores2 = [dbSelectedOdds.ls1, dbSelectedOdds.ls2, dbSelectedOdds.ls3];
           const DbBackScores = DbBackScores2.map(item => Number(item));
 
           console.log(" DbBackOdds ============ ", DbBackOdds);
