842ee01422362aa1a458706c1c83f8e4c6f1d79d
bhattitayab <bhattitayab747@gmail.com>
2023-09-10 19:30:47 +0500

    Bets

--- app/routes/currentPosition.js
+++ app/routes/currentPosition.js
@@ -103,7 +103,7 @@ const currentPositionDetails = async (req, res) => {
       },
       {
         $group: {
-          _id: "$bets.runner",
+          _id: "$bets",
           marketId: { $first: { $arrayElemAt: ["$bets.marketId", 0] } },
           matchId: { $first: { $arrayElemAt: ["$bets.matchId", 0] } },
           // marketId: { $first: { $arrayElemAt: ["$bets.marketId", 0] } }
