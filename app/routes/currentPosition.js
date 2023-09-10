28a26307401dd100b03c47ed5a5665c065dc4505
bhattitayab <bhattitayab747@gmail.com>
2023-09-10 23:17:31 +0500

    loosingAmount

--- app/routes/currentPosition.js
+++ app/routes/currentPosition.js
@@ -106,8 +106,9 @@ const currentPositionDetails = async (req, res) => {
           _id: "$bets.runner",
           marketId: { $first: { $arrayElemAt: ["$bets.marketId", 0] } },
           matchId: { $first: { $arrayElemAt: ["$bets.matchId", 0] } },
-          loosingAmount: { $sum: "$amount" },          
-          maxWinningAmount: { $sum: "$bets.loosingAmount" },
+          loosingAmount: { $sum: "$amount" },    
+          rumaxWinningAmountnner: { $first: { $arrayElemAt: ["$bets.loosingAmount", 0] } },
+          // maxWinningAmount: { $sum: "$bets.loosingAmount" },
           runner: { $first: { $arrayElemAt: ["$bets.runner", 0] } },
           share: { $first: "$share" }
 
