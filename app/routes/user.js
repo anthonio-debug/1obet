5f29202ee94f8b1392d693700576986e9754313d
bhattitayab <bhattitayab747@gmail.com>
2023-09-12 23:08:53 +0500

    token set to nul

--- app/routes/user.js
+++ app/routes/user.js
@@ -368,7 +368,7 @@ function getNonExpiringToken(userId, createdBy, role, isActive) {
     isActive: isActive,
     exp: new Date().getTime() + 120000
   };
-  var token = jwt.sign(payload, config.secret, {expiresIn: '2m'});
+  var token = jwt.sign(payload, config.secret, {});
   return token;
 }
 
