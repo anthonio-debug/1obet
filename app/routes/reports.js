c53414520589fea7fce94f9eefe9d3cf60e70880
ysfm <ysfm@jsistem.com>
2023-09-12 09:59:23 -0400

    Update reports.js

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -604,7 +604,7 @@ async function user_book(req, res) {
       const alllParent = await getParents(record._id.userId);
 
       if (alllParent.length < 2) {
-        parentInfo.push({id: currentUser.userId, downLineShare: 100 - currentUser.downLineShare, username: currentUser.userName});
+        parentInfo.push({id: currentUser.userId, downLineShare: currentUser.downLineShare, username: currentUser.userName});
       } else {
 
         var subChild = null; 
@@ -623,7 +623,7 @@ async function user_book(req, res) {
           }
 
         } else {
-          parentInfo.push({id: currentUser.userId, downLineShare: 100 - currentUser.downLineShare, username: currentUser.userName});
+          parentInfo.push({id: currentUser.userId, downLineShare:  currentUser.downLineShare, username: currentUser.userName});
         }
 
 
