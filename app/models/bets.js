57adc044f4c95792ff02e8eb11a07203f0ea8253
bhattitayab <bhattitayab747@gmail.com>
2023-09-14 19:38:06 +0500

    type spell in coreect

--- app/models/bets.js
+++ app/models/bets.js
@@ -36,7 +36,7 @@ const betSchema = new mongoose.Schema({
   runnerName: { type: String },
   fancyRate: { type: Number, default: 0 },
   lastCheckResult: { type: Number, default: 0 },
-  calculateExp: { ype: Boolean, default: true},
+  calculateExp: { type: Boolean, default: true},
   exposureAmount: { type: Number, default: 0 },
   runnersPosition: { type: Array },
 });
