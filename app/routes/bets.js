87af56ca08f928c29c519d72397da1fb685c917f
bhattitayab <bhattitayab747@gmail.com>
2023-09-16 22:28:07 +0500

    Minor in 3rd

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -855,30 +855,31 @@ async function calculateExposure(marketId, userId, type, selectedRunner, loosing
   console.log("=================== expoisureType ================", expoisureType);
 
   let newPosition;
-  if(expoisureType == 3){
-    // if(type == 0){
-      console.log(" ================= fancy  Back is called  =================  ");
-      newPosition = lastrunnersPosition.map((item)=>{
-        if(item.runner == type){
-          item.amount = item.amount + winningAmount
-        }else {
-          item.amount = item.amount + (-loosingAmount)
-        }
-        return item 
-      })
-    // }
-    // else if(type == 1){
-    //   console.log(" ================= Fancy  Lay is called  =================  ");
-    //   newPosition = lastrunnersPosition.map((item)=>{
-    //     if(item.runner == type){
-    //       item.amount = item.amount + winningAmount
-    //     }else {
-    //       item.amount = item.amount + (-loosingAmount)
-    //     }
-    //     return item 
-    //   });
-    // }
-  }else if(expoisureType == 2){
+  // if(expoisureType == 3){
+  //   // if(type == 0){
+  //     console.log(" ================= fancy  Back is called  =================  ");
+  //     newPosition = lastrunnersPosition.map((item)=>{
+  //       if(item.runner == type){
+  //         item.amount = item.amount + winningAmount
+  //       }else {
+  //         item.amount = item.amount + (-loosingAmount)
+  //       }
+  //       return item 
+  //     })
+  //   // }
+  //   // else if(type == 1){
+  //   //   console.log(" ================= Fancy  Lay is called  =================  ");
+  //   //   newPosition = lastrunnersPosition.map((item)=>{
+  //   //     if(item.runner == type){
+  //   //       item.amount = item.amount + winningAmount
+  //   //     }else {
+  //   //       item.amount = item.amount + (-loosingAmount)
+  //   //     }
+  //   //     return item 
+  //   //   });
+  //   // }
+  // }else 
+  if(expoisureType == 2){
     newPosition = lastrunnersPosition.map((item)=>{
       if(item.runner == selectedRunner){
         item.amount = item.amount + winningAmount
