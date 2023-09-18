73b8165e6f0d77beb8ea397d3171140b0fc35970
bhattitayab <bhattitayab747@gmail.com>
2023-09-18 17:00:45 +0500

    market id

--- app/routes/casinoGames.js
+++ app/routes/casinoGames.js
@@ -248,7 +248,7 @@ async function getAllSelectedCasinos(req, res) {
   console.log(" ================== subMarketId =========================", subMarketId);
   console.log(" ================== marketId =========================", marketId);
 
-  if (marketIds.includes(marketId) || subMarketId.includes(subMarketDetail.Id) || user.betLockStatus == true || user.blockedSubMarketsByParent.includes(subMarketDetail.Id)) {
+  if ((marketIds.length &&  marketIds.includes(marketId)) || (subMarketId.length && subMarketId.includes(subMarketDetail.Id)) || user.betLockStatus == true || (user.blockedSubMarketsByParent.lemgth &&  user.blockedSubMarketsByParent.includes(subMarketDetail.Id))) {
     return res.status(404).send({ message: 'Betting disabled' });
   }
   let query = {};
