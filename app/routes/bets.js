fd564e205192710ec3cc60cac26477a4648df0ed
bhattitayab <bhattitayab747@gmail.com>
2023-09-23 18:57:07 +0500

    runnerForSaveInbets

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1565,7 +1565,7 @@ const placeBet = async (req, res) => {
             console.log(" ================= Back is called  =================  ");
             //  $Clickedrunner_new_value = ( $Clickedrunner_prev_value )  + ( currentWinningAmount ) 
             //  $Otherrunner_new_value =  ( $Otherrunner_prev_value)  + ( BetAmount In fact liability amount which will be in minus ) = (-100 ) +  ( -100 )  = 200
-            newPosition = lastrunnersPosition.map((item)=>{
+            newPosition = runnerForSaveInbets.map((item)=>{
               if(item.runner == 0){
                 item.amount = item.amount + winningAmount
               }else {
@@ -1577,7 +1577,7 @@ const placeBet = async (req, res) => {
             console.log(" ================= Lay is called  =================  ");
             // $Clickedrunner_new_value = ( $Clickedrunner_prev_value )  + ( -  (loosing money )liablityAmount ) => ( 67 ) + ( -34 ) = 33
             // $Otherrunner_new_value =  ( $Otherrunner_prev_value)  + ( BetAmount )   ( - 100 ) + ( + 100 )
-            newPosition = lastrunnersPosition.map((item)=>{
+            newPosition = runnerForSaveInbets.map((item)=>{
               if(item.runner == 1){
                 item.amount = item.amount + (-loosingAmount)
               }else {
