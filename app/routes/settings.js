ea8e16b85a96c261262292a1e027c3f2c227b7fd
ysfm <ysfm@jsistem.com>
2023-09-24 12:33:58 -0400

    Update settings.js

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2024,8 +2024,10 @@ const getWaitingBetsForManuel = async (req, res) => {
           } 
         }
         
-
-
+       const u1 = await  User.findOne({userId: item.userId},{userName: 1});
+       if (u1) {
+        item.userName = u1.userName;
+       }
         groups[main_group_key].bets.push(item);
     }
 
