605f1da8282da9f4b5aaf474e7b03e5d8bdba29b
ysfm <ysfm@jsistem.com>
2023-09-27 16:42:57 -0400

    Update reports.js

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -253,14 +253,14 @@ async function getFinalReport(req, res) {
   if (currentUser.createdBy !== 0) {
     const parentUserData = await User.findOne({ userId: currentUser.createdBy }, { _id: 1, userId: 1, balance: 1, userName: 1 });
     if (parentUserData) {
-      results.negativeClients.push({ userName: parentUserData.userName, userId: parentUserData.userId, clientPL: (currentUser.credit  - currentUser.clientPL) * -1 });
+      results.negativeClients.push({ userName: parentUserData.userName, userId: parentUserData.userId, clientPL: ( currentUser.clientPL) * -1 });
       results.totalNegativeClientPL = results.totalNegativeClientPL + ( currentUser.clientPL) * -1;
     } else {
-      results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: (currentUser.credit - currentUser.clientPL) * -1});
+      results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: ( currentUser.clientPL) * -1});
       results.totalNegativeClientPL = results.totalNegativeClientPL + (currentUser.clientPL) * -1;
     }
   } else {
-    results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: (currentUser.credit  - currentUser.clientPL) * -1});
+    results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: ( currentUser.clientPL) * -1});
     results.totalNegativeClientPL = results.totalNegativeClientPL + (currentUser.clientPL) * -1;
   }
 
