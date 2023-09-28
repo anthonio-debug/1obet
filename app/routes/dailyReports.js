a22c8dcef31c974d7532e0223021e466f15893f6
bhattitayab <bhattitayab747@gmail.com>
2023-09-28 16:25:14 +0500

    =============

--- app/routes/dailyReports.js
+++ app/routes/dailyReports.js
@@ -587,27 +587,46 @@ const tesTingsheet = async(req, res) =>{
   }
 
   const userId      = req.decoded.userId;
+  const resp        = [];
   const users       = await User.distinct("userId", { createdBy:  userId });
   const currentUser = await User.findOne({ userId: userId })
-  const parentUser  = await User.findOne({ userId: currentUser.createdBy })
+  if(currentUser.role == 0){
+    resp.push(
+      {
+        _id: currentUser.userId,
+        name: "cash",
+        amount : currentUser.cash
+      },
+      {
+        _id: currentUser.userId,
+        name: currentUser.userName,
+        amount : currentUser.balance
+      }
+    )
+  }
+  else {
+    const parentUser  = await User.findOne({ userId: currentUser.createdBy });
+    console.log(" ================== parentUser ================  ", parentUser);
+    resp.push(    
+      {
+        _id: currentUser.userId,
+        name: "cash",
+        amount : currentUser.cash
+      },
+      {
+        _id: currentUser.userId,
+        name: currentUser.userName,
+        amount : currentUser.balance
+      },
+      {
+        _id: parentUser.userId,
+        name: parentUser.userName,
+        amount : currentUser.clientPL * (-1)
+      }
+    )
+  }
 
-  const resp = [
-    {
-      _id: currentUser.userId,
-      name: "cash",
-      amount : currentUser.cash
-    },
-    {
-      _id: currentUser.userId,
-      name: currentUser.userName,
-      amount : currentUser.balance
-    },
-    {
-      _id: parentUser.userId,
-      name: parentUser.userName,
-      amount : currentUser.clientPL * (-1)
-    }
-  ]
+  console.log(" ================== currentUser ================  ", currentUser);
 
   const response = await User.aggregate([
     {  
