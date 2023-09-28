b60a2f8a98a29731ca8194c22d78cd7e348d0c83
ysfm <ysfm@jsistem.com>
2023-09-28 10:11:25 -0400

    Update reports.js

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -234,9 +234,9 @@ async function getFinalReport(req, res) {
 
     
     if (userRecord.role == 5) {
-      usedValue = userRecord.clientPL+userRecord.credit;
+      usedValue = userRecord.clientPL+userRecord.creditRemaining;
     } else {
-        usedValue = userRecord.clientPL+userRecord.credit;
+        usedValue = userRecord.clientPL+userRecord.creditRemaining;
     }
 
     if (usedValue > -1) {
