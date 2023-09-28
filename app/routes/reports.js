7323cfb6777a0c3867243153b45a1651e1ca4dfa
ysfm <ysfm@jsistem.com>
2023-09-28 09:57:46 -0400

    Final sheet credit

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -234,9 +234,9 @@ async function getFinalReport(req, res) {
 
     
     if (userRecord.role == 5) {
-      usedValue = userRecord.clientPL;
+      usedValue = userRecord.clientPL+currentUser.credit;
     } else {
-        usedValue = userRecord.clientPL;
+        usedValue = userRecord.clientPL+currentUser.credit;
     }
 
     if (usedValue > -1) {
@@ -254,14 +254,14 @@ async function getFinalReport(req, res) {
     const parentUserData = await User.findOne({ userId: currentUser.createdBy }, { _id: 1, userId: 1, balance: 1, userName: 1 });
     if (parentUserData) {
       results.negativeClients.push({ userName: parentUserData.userName, userId: parentUserData.userId, clientPL: ( currentUser.clientPL) * -1 });
-      results.totalNegativeClientPL = results.totalNegativeClientPL + ( currentUser.clientPL) * -1;
+      results.totalNegativeClientPL = results.totalNegativeClientPL + ( currentUser.clientPL + currentUser.credit) * -1;
     } else {
-      results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: ( currentUser.clientPL) * -1});
-      results.totalNegativeClientPL = results.totalNegativeClientPL + (currentUser.clientPL) * -1;
+      results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: ( currentUser.clientPL+ currentUser.credit) * -1});
+      results.totalNegativeClientPL = results.totalNegativeClientPL + (currentUser.clientPL+ currentUser.credit) * -1;
     }
   } else {
-    results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: ( currentUser.clientPL) * -1});
-    results.totalNegativeClientPL = results.totalNegativeClientPL + (currentUser.clientPL) * -1;
+    results.negativeClients.push({ userName: currentUser.userName, userId: currentUser.userId, clientPL: ( currentUser.clientPL+ currentUser.credit) * -1});
+    results.totalNegativeClientPL = results.totalNegativeClientPL + (currentUser.clientPL + currentUser.credit) * -1;
   }
 
 
