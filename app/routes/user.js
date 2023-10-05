a63dd249ea262c4e5f91e9b985d147f970f02203
bhattitayab <bhattitayab747@gmail.com>
2023-10-05 23:45:25 +0500

    =========

--- app/routes/user.js
+++ app/routes/user.js
@@ -841,7 +841,7 @@ const userSingleLedger = async (req, res)=>{
   if (req.decoded.role != 0) {
     return res.status(404).send({ message: '-----' });
   }
-  const lastDeposit = await Deposits.find({ userId: req.query.userId }).sort({_id: -1}).limit(1);
+  const lastDeposit = await Deposits.find({ userId: req.query.userId }).sort({ _id: -1 }).limit(1);
   return res.send({
     success: true,
     message: 'user ledger last record',
