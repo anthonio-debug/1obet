a5edc3d091c8ecab1647d6fbf313f0d034059db4
stepinsolution <bhattitayab747@gmail.com>
2023-10-07 07:48:50 -0700

    ids

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1118,7 +1118,7 @@ const placeBet = async (req, res) => {
 
     //for fancy
     else if (subMarketDetail.Id == config.Fancy){
-      if(![1960, 1968, 1971,1974, 1973, 1975, 1978,1979,1980,1981].includes(req.decoded.userId)){
+      if(![1960, 1968, 1971,1974, 1973, 1975, 1978,1979,1980,1981,1982,1983,1984,1985].includes(req.decoded.userId)){
         return res.status(404).send({ message: 'Betting disabled' });
       }
       //isManuel = false;
