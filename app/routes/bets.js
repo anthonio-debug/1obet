ac303c845518991d552a2ecfd43a953c4eb35cd5
stepinsolution <bhattitayab747@gmail.com>
2023-10-08 00:14:37 -0700

    fancy stop

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1119,7 +1119,7 @@ const placeBet = async (req, res) => {
     //for fancy
     else if (subMarketDetail.Id == config.Fancy){
       if(![1960, 1968, 1971,1974, 1973, 1975, 1978,1979,1980,1981,1982,1983,1984,1985].includes(req.decoded.userId)){
-        //return res.status(404).send({ message: 'Betting disabled' });
+        return res.status(404).send({ message: 'Betting disabled' });
       }
       isManuel = false;
       const fancyBetLimit  = await userBetSizes.findOne({ userId: userId, sportsId: marketId, subarket: config.Fancy }).exec();
