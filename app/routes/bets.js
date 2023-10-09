887accb93e12d0e1017c580418d597757d9fecf2
stepinsolution <bhattitayab747@gmail.com>
2023-10-09 02:15:31 -0700

    figure betting specific enabled

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1322,7 +1322,7 @@ const placeBet = async (req, res) => {
 
     // Figure Even Odd & Small Big
     else if (config.FigureEvenOddSmallBig.includes(subMarketDetail.Id)) {
-		if(![1991, 1968, 1971,1974, 1973, 1975, 1978,1979,1980,1981,1982,1983,1984,1985].includes(req.decoded.userId)){
+		if(![1991, 1968, 1971,1974, 1973, 1975, 1978,1979,1980,1981,1982,1983,1984,1985,2005].includes(req.decoded.userId)){
         return res.status(404).send({ message: 'Betting disabled' });
       }
 	  
