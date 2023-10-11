169e392e7706e5c69a315e38a88170d4fd77e640
0129general <generalsoft29@protonmail.com>
2023-10-11 08:06:32 -0300

    Add marketShares API

--- app/routes/marketShares.js
+++ app/routes/marketShares.js
@@ -8,54 +8,114 @@ const Bets = require('../models/bets');
 const loginRouter = express.Router();
 
 const marketGainWithDuplicates = async (req, res) => {
-  // console.log('req:', req);
   const errors = validationResult(req);
   if (errors.errors.length !== 0) {
     return res.status(400).send({ errors: errors.errors });
   }
-  const Id = parseInt(req.query.userId);
-  console.log(' Id ========== ', Id);
+  const userId = Number(req.query.userId);
 
-  const response = await Bets.aggregate([
-    {
-      $match: {
-        userId: Id,
-        $and: [
-          {
-            createdAt: { $gte: req.query.startDate },
-          },
-          {
-            createdAt: { $lte: req.query.endDate },
-          },
-        ],
-      },
-    },
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'userId',
-        foreignField: 'userId',
-        as: 'user_info',
-      },
-    },
-    {
-      $group: {
-        _id: '$marketId',
-        userId: { $first: '$userId' },
-        runnerName: { $first: '$runnerName' },
-        positions: { $sum: '$position' },
-        availableBalance: {
-          $first: { $arrayElemAt: ['$user_info.availableBalance', 0] },
+  const matchId = req.query.matchId;
+  const currentUser = await User.findOne({ userId: userId });
+
+  if (currentUser.role == 5) {
+    const match = await Events.findById(matchId);
+    const parent = await User.findOne({ userId: currentUser.createdBy });
+    const response = await CashDeposit.aggregate([
+      {
+        $match: {
+          matchId: matchId,
+          $or: [
+            {
+              $and: [
+                {
+                  userId: userId,
+                },
+                {
+                  cashOrCredit: { $in: ['Bet'] },
+                },
+              ],
+            },
+            {
+              cashOrCredit: { $in: ['Commission'] },
+            },
+          ],
         },
-        createdAt: { $first: '$createdAt' },
       },
-    },
-  ]);
-  return res.send({
-    success: true,
-    message: 'Market Positions Reports !',
-    results: response,
-  });
+      {
+        $addFields: {
+          betsId: { $toObjectId: '$betId' },
+        },
+      },
+      {
+        $lookup: {
+          from: 'bets',
+          localField: 'betsId',
+          foreignField: '_id',
+          as: 'betsDetails',
+        },
+      },
+      {
+        $group: {
+          _id: '$betId',
+          pl: { $sum: '$amount' },
+          sattledAt: { $first: '$date' },
+          price: { $first: { $arrayElemAt: ['$betsDetails.betAmount', 0] } },
+          name: { $first: { $arrayElemAt: ['$betsDetails.runnerName', 0] } },
+          createdAt: {
+            $first: { $arrayElemAt: ['$betsDetails.createdAt', 0] },
+          },
+          size: { $first: { $arrayElemAt: ['$betsDetails.betRate', 0] } },
+          type: { $first: { $arrayElemAt: ['$betsDetails.type', 0] } },
+        },
+      },
+    ]);
+    return res.send({
+      success: true,
+      message: 'Detailed reports',
+      results: response,
+      isDetailed: true,
+      dealer: parent.userName,
+      currentUser: currentUser.userName,
+      Winner: match?.winner,
+    });
+  } else {
+    const childUsers = await User.distinct('userId', { createdBy: userId });
+    const users = [userId, ...childUsers];
+    console.log(' users ===================  ', users);
+
+    const response = await CashDeposit.aggregate([
+      {
+        $match: {
+          userId: {
+            $in: users,
+          },
+          matchId: matchId,
+          cashOrCredit: { $in: ['Bet', 'Commission', 'loosing'] },
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'userId',
+          foreignField: 'userId',
+          as: 'userInfo',
+        },
+      },
+      {
+        $group: {
+          _id: '$userId',
+          amount: { $sum: '$amount' },
+          name: { $first: { $arrayElemAt: ['$userInfo.userName', 0] } },
+        },
+      },
+    ]);
+
+    return res.send({
+      success: true,
+      message: 'Commission reports',
+      results: response,
+    });
+  }
 };
 
 loginRouter.get('/marketShares', marketGainWithDuplicates);
