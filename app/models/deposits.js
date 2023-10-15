3fd573388b45d57e6417834fb572ca0a5bec4635
stepinsolution <bhattitayab747@gmail.com>
2023-10-15 10:44:59 -0700

    betSession Number

--- app/models/deposits.js
+++ app/models/deposits.js
@@ -28,6 +28,7 @@ let depositsSchema = new Schema({
   createdAt: { type: String },
   date: { type: Number },
   betType: {type: Number},
+  betSession: {type: Number},
   betDateTime: {type: Number}
 });
 
