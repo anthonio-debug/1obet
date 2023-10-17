008ffab2cd72cecbf9673da8e3b599dde6f1de84
bhattitayab <bhattitayab747@gmail.com>
2023-10-17 18:05:22 +0500

    =======

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -30,7 +30,7 @@ const checkMarketBlocked  = async (user) => {
 
 }
 
-const WinLoseTransManagement = async (balance, payload, user, action) => {
+const WinLoseTransManagement = async (balance, payload, users, action) => {
 
   const client = new MongoClient(config.DBHost, { useUnifiedTopology: true });
   await client.connect();
