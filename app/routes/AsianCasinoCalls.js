a56db954fb2f8c5b1c6dfa128fcac8fa87a548b7
bhattitayab <bhattitayab747@gmail.com>
2023-10-18 18:27:43 +0500

    Test case

--- app/routes/AsianCasinoCalls.js
+++ app/routes/AsianCasinoCalls.js
@@ -696,7 +696,7 @@ async function credit(req, res) {
   }
 }
 
-router.get('/balance', balance);
-router.get('/debit', debit);
-router.get('/credit', credit);
+router.get('/asian/balance', balance);
+router.get('/asian/debit', debit);
+router.get('/asian/credit', credit);
 module.exports = { router };
