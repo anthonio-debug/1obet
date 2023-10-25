b6d8b54bdcbcbca02fef6cac028d460d2565898d
bhattitayab <bhattitayab747@gmail.com>
2023-10-25 19:26:35 +0500

    ===========

--- app/routes/AsianCasinoCalls.js
+++ app/routes/AsianCasinoCalls.js
@@ -712,6 +712,27 @@ async function debit(req, res) {
             { id: trans.referenceId}, 
             {$set : { cancelProcessed: true }
           });
+          const updatedavailableBalance = user?.availableBalance + (amount);
+          const userResponse = await users.updateOne(
+            { userId: parseInt(payload.user.id)},
+            {
+              $set: {
+                availableBalance: updatedavailableBalance
+              }
+            },
+            { session }
+          )
+          const updatedUser = await users.findOne({ userId: parseInt(payload.user.id) })
+          return res.json({
+            partnerKey: config.worldCasinoOnlinePartnerKey,
+            status:{
+              "code": "SUCCESS",
+              "message": ""
+            },
+            balance: (updatedUser.availableBalance) / casinoMultiples,
+            userId: updatedUser.userId.toString(),
+            timestamp: timestamp
+          });
         } 
       }
 
@@ -800,14 +821,13 @@ async function debit(req, res) {
     await session.commitTransaction();
 
     const updatedUser = await users.findOne({ userId: parseInt(payload.user.id)});
-    const testAmt = payload.transactionData.amount * casinoMultiples
     return res.json({
       partnerKey: config.worldCasinoOnlinePartnerKey,
       status:{
         "code": "SUCCESS",
         "message": ""
       },
-      balance: ( updatedUser.availableBalance - testAmt) / casinoMultiples,
+      balance: ( updatedUser.availableBalance) / casinoMultiples,
       userId: updatedUser.userId.toString(),
       timestamp: timestamp
     });
@@ -944,6 +964,27 @@ async function credit(req, res) {
             { id: trans.referenceId}, 
             {$set : { cancelProcessed: true }
           });
+          const updatedavailableBalance = user?.availableBalance - (amount);
+          const userResponse = await users.updateOne(
+            { userId: parseInt(payload.user.id)},
+            {
+              $set: {
+                availableBalance: updatedavailableBalance
+              }
+            },
+            { session }
+          )
+          const updatedUser = await users.findOne({ userId: parseInt(payload.user.id) })
+          return res.json({
+            partnerKey: config.worldCasinoOnlinePartnerKey,
+            status:{
+              "code": "SUCCESS",
+              "message": ""
+            },
+            balance: (updatedUser.availableBalance) / casinoMultiples,
+            userId: updatedUser.userId.toString(),
+            timestamp: timestamp
+          });
         } 
       }
 
@@ -1039,7 +1080,6 @@ async function credit(req, res) {
     await session.commitTransaction();
 
     const updatedUser = await users.findOne({ userId: parseInt(payload.user.id) })
-    const testAmt = payload.transactionData.amount * casinoMultiples
     console.log(" Amount Returnning to Casino from Credit  ", updatedUser.availableBalance / casinoMultiples);
     return res.json({
       partnerKey: config.worldCasinoOnlinePartnerKey,
@@ -1047,7 +1087,7 @@ async function credit(req, res) {
         "code": "SUCCESS",
         "message": ""
       },
-      balance: (updatedUser.availableBalance + testAmt) / casinoMultiples,
+      balance: (updatedUser.availableBalance) / casinoMultiples,
       userId: updatedUser.userId.toString(),
       timestamp: new Date().getTime().toString()
     });
