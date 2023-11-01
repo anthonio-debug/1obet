aba471f6fa9646145fbaf8f0a0ffc26e1546aa97
cryptofan35 <kirylkrauchuk@gmail.com>
2023-11-02 05:46:14 +0800

    Revert "commit"
    This reverts commit 9a4b61346107245370751aeebb5d77ca0bb903e6.

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -35,7 +35,6 @@ const loginRecord = require('../models/loginRecord');
 // process.env.TZ = 'UTC';
 
 const SelectedCasino = require('../models/selectedCasino');
-const BetPlaceHold = require('../models/betPlaceHold');
 
 function updateDefaultTheme(req, res) {
   const errors = validationResult(req);
@@ -166,7 +165,6 @@ function updateDefaultBetSizes(req, res) {
       { _id: betLimit._id },
       { $set: { maxAmount: betLimit.maxAmount } },
       { new: true, upsert: true }
-
     );
   });
 
@@ -230,7 +228,7 @@ async function updateMatchType(req, res) {
   }
   try {
     const { _id, matchType, iconStatus } = req.body;
-   const EventData = await Events.findByIdAndUpdate(
+    Events.findByIdAndUpdate(
       _id,
       { $set: { matchType: matchType, iconStatus: iconStatus } },
       (err, updatedMatch) => {
@@ -241,22 +239,6 @@ async function updateMatchType(req, res) {
         }
       }
     );
-    const updatedBet = await BetPlaceHold.findOneAndUpdate({
-      eventId: _id
- }, // sportsId is always 4
- {
-     $set: {
-         sportsId:6,
-         secondsValue: 4,
-         eventId: eventId
-     }
- }, {
-     new: true,
-     upsert: true
- } 
- // Create new document if not found
-);
-console.log(updatedBet)
     res.status(200).json({
       success: true,
       message: 'Updated Successfully',
