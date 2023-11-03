5d05aa24b64fa4be72c27c15fc70cf602b5705d8
bhattitayab <bhattitayab747@gmail.com>
2023-11-03 22:41:33 +0500

    ============

--- app/routes/dailyPLReports.js
+++ app/routes/dailyPLReports.js
@@ -447,7 +447,7 @@ const dailyPLMatchWiseDetailedReport = async(req, res) =>{
         },
         { 
           $group:{
-            _id: "$_id",
+            _id: "$betId",
             pl: { $sum: "$amount"},
             sattledAt: { $first: "$date" },
             sportsId: { $first: "$sportsId" },
