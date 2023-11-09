6c5c26dd92b1ec5dfa010f61a4787693aec21653
cryptofan35 <kirylkrauchuk@gmail.com>
2023-11-09 15:54:03 +0800

    removed matchId set in placebets

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2004,7 +2004,6 @@ const placeBet = async (req, res) => {
         " ======================== AsianTable Odds ======================== "
       );
       const DBOddDetails = await AsianOdds.findOne({ tableId: oddsId });
-      matchId = DBOddDetails._id;
       if (!DBOddDetails) {
         return res.status(404).send({
           message: `Frontend provided odds _id do not found in db & _id =  ${oddsId}`,
