15b2303dd07f8a050fa9d7cbccb590f810845f1d
cryptofan35 <kirylkrauchuk@gmail.com>
2023-11-10 22:39:42 +0800

    removed unprovided api

--- restApiSystem/src/api/apiRequestsAsian.js
+++ restApiSystem/src/api/apiRequestsAsian.js
@@ -19,7 +19,7 @@ const tableNames = [
     tableName: "TEST TEENPATTI(TEST TEENPATTI)",
     imageUrl: "TEST_TEENPATTI.png",
   },
-  { tableId: "lucky7", tableName: "LUCKY 7-A", imageUrl: "LUCKY_7-A.jpg" },
+  // { tableId: "lucky7", tableName: "LUCKY 7-A", imageUrl: "LUCKY_7-A.jpg" },
   { tableId: "lucky7eu", tableName: "LUCKY 7-B", imageUrl: "LUCKY_7-B.png" },
   { tableId: "card32eu", tableName: "32 CARD-B", imageUrl: "32_CARD-B.png" },
   {
@@ -28,7 +28,7 @@ const tableNames = [
     imageUrl: "AMAR_AKBAR_ANTHONY(AAA).png",
   },
   { tableId: "ab20", tableName: "ANDAR BAHAR", imageUrl: "ANDAR_BAHAR.jpg" },
-  { tableId: "abj", tableName: "ANDAR BAHAR 2", imageUrl: "ANDAR_BAHAR_2.png" },
+  // { tableId: "abj", tableName: "ANDAR BAHAR 2", imageUrl: "ANDAR_BAHAR_2.png" },
   { tableId: "worli", tableName: "WORLI MATKA", imageUrl: "ANDAR_BAHAR_2.png" },
 ];
 
