26042f23101f3abcc8761914b9b1146e5b0d09fd
bhattitayab <bhattitayab747@gmail.com>
2023-11-11 16:30:52 +0500

    Dashboard Games

--- app/models/asianTable.js
+++ app/models/asianTable.js
@@ -4,6 +4,8 @@ const AsianTableSchema = new mongoose.Schema({
   tableId: { type: String },
   tableName: { type: String, required: true },
   imageUrl: { type: String },
+  imageUrl: { type: String },
+  isDashboard: { type: Boolean, default: false },
 });
 
 const AsianTable = mongoose.model("asiantable", AsianTableSchema);
--- app/routes/settings.js
+++ app/routes/settings.js
@@ -5,7 +5,6 @@ const Settings = require("../models/settings");
 const moment = require("moment");
 const User = require("../models/user");
 const settingsValidation = require("../validators/settings");
-const termsAndConditions = require("../models/termsAndConditions");
 const PrivacyPolicy = require("../models/privacyPolicy");
 const Competition = require("../models/listCompetitions");
 const Odds = require("../models/odds");
@@ -26,6 +25,7 @@ const Session = require("../models/Session");
 const MarketIDS = require("../models/marketIds");
 const Bets = require("../models/bets");
 const BetPlaceHold = require("../models/betaPlaceHold");
+const AsianTable = require("../models/asianTable");
 const mongoose = require("mongoose");
 const {
   handleDrawBet,
@@ -2400,6 +2400,35 @@ async function GetRule(req, res) {
   }
 }
 
+async function SetAsianDashboard(req, res) {
+  const errors = validationResult(req);
+  if (errors.errors.length !== 0) {
+    return res.status(400).send({ errors: errors.errors });
+  }
+  if (req.decoded.role !== "0") {
+    return res
+      .status(404)
+      .send({ message: "only company can add privacy policies" });
+  }
+  try {
+    const response = await AsianTable.findOneAndUpdate(
+      { tableId : req.body.tableId }, 
+      { $set: { isDashboard: req.body.isDashboard } }
+    );
+
+    return res.send({
+      success: true,
+      message: "Successfully Updated !",
+      results: response,
+    });
+  } catch (Error) {
+    console.error(`Error ${Error}`);
+    return res.send({
+      message: `Something went wrong !`,
+    });
+  }
+}
+
 loginRouter.post(
   "/updateDefaultTheme",
   settingsValidation.validate("updateDefaultTheme"),
@@ -2430,6 +2459,8 @@ loginRouter.post(
 
 router.get("/GetRule", GetRule);
 loginRouter.post("/addRules", addRules);
+loginRouter.post("/SetAsianDashboard",   settingsValidation.validate("SetAsianDashboard"), SetAsianDashboard);
+
 
 loginRouter.post(
   "/updateDefaultBetSizes",
