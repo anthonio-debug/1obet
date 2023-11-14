c1943dfed3a017ca83731f7deeb6e928f68e8155
bhattitayab <bhattitayab747@gmail.com>
2023-11-15 02:45:24 +0500

    Msg test changed

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -172,7 +172,7 @@ const placeBet = async (req, res) => {
 
     const blockedUsersCount = await User.countDocuments({ userId: {$in: parentUserIds}, bettingAllowed: false })
     if (blockedUsersCount > 0) {
-      return res.status(404).send({ message: "Bet disbaled ..." });
+      return res.status(404).send({ message: "Beting disbaled" });
     }
 
     const marketIds = await User.distinct("blockedMarketPlaces", {
