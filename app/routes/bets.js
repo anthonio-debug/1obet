ea213f55abf9656ba7e34d386ad1f7515cbcfc2b
bhattitayab <bhattitayab747@gmail.com>
2023-11-15 16:36:33 +0500

    return res.status(404).send({           message: `Bet Miss Matched `,         });

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -797,6 +797,11 @@ const placeBet = async (req, res) => {
         " ============================ GH & HR ============================ "
       );
       const DBOddDetails = await RaceOdds.findById(oddsId);
+      if(!DBOddDetails){
+        return res.status(404).send({
+          message: `Bet Miss Matched `,
+        });
+      }
       console.log(" ============================ ", DBOddDetails);
       const OddDetailsTeam = DBOddDetails?.runners.find(
         (runner) => runner.selectionId == selectionId
