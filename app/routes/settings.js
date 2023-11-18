d3bde5a5bf3e3d273e6b09f882bd7c1105c1a47d
stepinsolution <bhattitayab747@gmail.com>
2023-11-18 09:54:48 -0800

    socore for session removed from setsessionscore

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2195,15 +2195,7 @@ const setSessionScore = async (req, res) => {
     { $set: { score: parseInt(req.body.score), manuelSave: true } }
   );
 
-  await Bets.findOneAndUpdate(
-    {
-      eventId: req.body.eventId,
-    },
-    {
-      sessionNo: req.body.sessionNo,
-      score: req.body.score,
-    }
-  );
+ 
 
   return res.status(200).send({
     success: true,
