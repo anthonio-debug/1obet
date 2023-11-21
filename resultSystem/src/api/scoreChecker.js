d965eeeb6423dc3d5b13a43e70a758085a712599
cryptofan35 <kirylkrauchuk@gmail.com>
2023-11-21 16:46:33 +0800

    updated aaa

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -863,9 +863,21 @@ function scoreChecker() {
                   betData[i].runner == widOdd ||
                   betData[i].runner == widSeven
                 ) {
-                  handleWinningBet(betData[i]);
+                  if(betData[i].type == 1){
+                    handleLosingBet(betData[i]);
+                  } else if(betData[i].type == 0){
+                    handleWinningBet(betData[i]);
+                  } else {
+                    handleDrawBet(betData[i], 0)
+                  }
                 } else {
-                  handleLosingBet(betData[i]);
+                  if(betData[i].type == 0){
+                    handleLosingBet(betData[i]);
+                  } else if(betData[i].type == 1){
+                    handleWinningBet(betData[i]);
+                  } else {
+                    handleDrawBet(betData[i], 0)
+                  }
                 }
               }
             }
