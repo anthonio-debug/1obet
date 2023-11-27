927cf2a4f29cada44b546187628373104282ac7c
bhattitayab <bhattitayab747@gmail.com>
2023-11-28 04:56:52 +0500

    .toArray()

--- resultSystem/src/CalculateBets/calculations.js
+++ resultSystem/src/CalculateBets/calculations.js
@@ -410,7 +410,7 @@ async function handleWinningBet(bet, winner) {
                 ]
               }
             ]
-          }, {winningAmount: 1, _id: 0 })
+          }, {winningAmount: 1, _id: 0 }).toArray()
 
           console.log(" winnings =================  ", winnings );
 
@@ -441,7 +441,7 @@ async function handleWinningBet(bet, winner) {
                 ]
               }
             ] 
-          }, {loosingAmount: 1, _id: 0 })
+          }, {loosingAmount: 1, _id: 0 }).toArray()
           console.log(" loosings =================  ", loosings );
 
           for (const singleWin of winnings) {
