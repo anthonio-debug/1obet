ebd9c4f36695f1a7536e38189cfe7b50457389b3
cryptofan35 <kirylkrauchuk@gmail.com>
2023-12-01 02:12:21 +0800

    Added listbook

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const apiURL= "http://185.58.225.212:8080/api/"
 
 async function listEvents(req, res) {
     try {
-        const apiURL= "http://185.58.225.212:8080/"
         const body = {
             "filter": {
               "textQuery": "string",
@@ -39,13 +39,24 @@ async function listEvents(req, res) {
             }
           }
         const response = await axios.post(`${apiURL}/listEvents`, body)
-        res.status(200).json({data: response, msg:'List Events'})
+        res.status(200).json({success: true, data: response})
     } catch (err) {
         res.status(500).json({success:false, msg: "Failed to get events"})
     }
 }
 
-router.get('/testSportsEvent', listEvents);
+async function listEvents(req, res) {
+    try {
+        const marketIds = "1.221889053,1.221889055"
+        const response = await axios.post(`${apiURL}listMarketBook/testqms/${marketIds}`)
+        res.status(200).json({success: true, data: response})
+    } catch (err) {
+        res.status(500).json({success: false, msg: "Failed to get "})
+    }
+}
+
+
+router.get('/testSports/events', listEvents);
 
 module.exports = { router, listEvents };
 
