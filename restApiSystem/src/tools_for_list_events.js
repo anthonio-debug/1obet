d1a32957bd7b811ef682a8e0f75f52d138ce10e4
CryptoFan <kirylkrauchuk@gmail.com>
2023-12-04 12:37:44 -0600

    added apiRequestsTestSCT for new 3-party

---
+++ restApiSystem/src/tools_for_list_events.js
@@ -0,0 +1,126 @@
+'use strict';
+module.exports = ToolForEvent;
+
+const sportsIds = ["1", "7522", "2", "4", "7", "27454571", "468328"];
+
+const inPlayEvents = require('../../app/models/events');
+const MarketIDs = require('../../app/models/marketIds');
+const Odds = require('../../app/models/odds');
+
+
+const apiRequests = require('./api/apiRequestsSCT.js')();
+
+let lastType = 0;
+
+function ToolForEvent() {
+    return { init };
+
+    async function init(_io, express) {
+        apiRequests.init(_io, express);
+        fetchEvents();
+        setInterval(fetchEvents, 2 * 60 * 1000);
+        setInterval(fetchMarkets, 10 * 1000);
+        setInterval(apiRequests.takeScores, 5 * 1000);
+
+        setInterval(() => {
+            for (const sportsId of sportsIds) {
+                apiRequests.setInplay(sportsId);
+            }
+        }, 10 * 1000);
+
+        setInterval(async () => {
+            for (const sportsId of sportsIds) {
+                await apiRequests.checkInPlay(sportsId);
+            }
+        }, 15 * 1000);
+
+        setInterval(() => {
+            for (const sportsId of sportsIds) {
+                fetchOdds(true, sportsId);
+            }
+        }, 1000);
+    }
+    async function fetchEvents() {
+        try {
+            for (const sportsId of sportsIds) {
+                await apiRequests.eventsBySupportJobs(sportsId);
+            }
+        } catch (error) {
+            console.error('Error fetching events:', error);
+        }
+    }
+
+    async function fetchMarkets() {
+        try {
+            const documents = await inPlayEvents.findOne({ status: 'OPEN' })
+                .sort({ lastCheckMarket: 1 })
+                .limit(1)
+                .exec();
+
+            if (documents) {
+                await apiRequests.listMarketsByCronJob(documents.Id, documents.sportsId);
+                await inPlayEvents.updateMany(
+                    { Id: documents.Id },
+                    { $set: { lastCheckMarket: Date.now() } }
+                );
+                fetchOddsForEvent(documents.Id);
+            }
+        } catch (error) {
+            console.error('Error fetching markets:', error);
+        }
+    }
+    async function fetchOddsForEvent(eventId) {
+        try {
+            const documents = await MarketIDs.find({ inPlay: false, eventId: eventId })
+                .sort({ lastCheck: 1 })
+                .limit(20)
+                .exec();
+            let marketIds = [];
+
+            if (documents.length > 0) {
+                documents.forEach(element => {
+                    marketIds.push(element.marketId);
+                });
+            }
+
+            await MarketIDs.updateMany(
+                { marketId: { $in: marketIds } },
+                { $set: { lastCheck: Date.now() } }
+            );
+
+            if (marketIds.length > 0) {
+                apiRequests.getOddsFromProvider(documents, eventId);
+            }
+        } catch (error) {
+            console.error('Error fetching odds for event:', error);
+        }
+    }
+
+    async function fetchOdds(inPlay, sportId) {
+        try {
+            const documents = await MarketIDs.find({ inPlay: inPlay, sportID: sportId })
+                .sort({ lastCheck: 1 })
+                .limit(20)
+                .exec();
+            let marketIds = [];
+
+            if (documents.length > 0) {
+                documents.forEach(element => {
+                    marketIds.push(element.marketId);
+                });
+            }
+
+            await MarketIDs.updateMany(
+                { marketId: { $in: marketIds } },
+                { $set: { lastCheck: Date.now() } }
+            );
+
+            if (marketIds.length > 0) {
+                apiRequests.getOddsFromProvider(documents, sportId);
+            }
+        } catch (error) {
+            console.error('Error fetching odds:', error);
+        }
+    }
+
+}
