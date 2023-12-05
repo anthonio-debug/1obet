98ba5842e7cc01b6f34e745b2898a0173da44f3e
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-12-06 00:11:12 +0500

    logs

--- app/routes/user.js
+++ app/routes/user.js
@@ -892,6 +892,8 @@ function searchSingleUser(req, res) {
 const battorsList = async (req, res) => {
   try{
 
+
+    console.log(" lastBet ======= ", lastBet);
     if (req.decoded.role != 0) {
       return res.status(404).send({ message: '-----' });
     }
