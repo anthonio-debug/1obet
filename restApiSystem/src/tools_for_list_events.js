6e2aa22d2fc270b9b04ffdcf8e280ed771a257b8
CryptoFan <kirylkrauchuk@gmail.com>
2023-12-05 06:22:50 -0600

    updated fetcMarkets function

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -52,7 +52,7 @@ function ToolForEvent() {
 
     async function fetchMarkets() {
         try {
-            const documents = await inPlayEvents.findOne({ status: 'OPEN' })
+            const documents = await inPlayEvents.findOne({ status: 'OPEN', isShowed: true })
                 .sort({ lastCheckMarket: 1 })
                 .limit(1)
                 .exec();
