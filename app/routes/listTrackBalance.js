334cea61fc3bed1e7dc17d56c077020e833a6eb9
CryptoFan <kirylkrauchuk@gmail.com>
2023-12-09 15:38:40 -0600

    updated listTrackBalance query

--- app/routes/listTrackBalance.js
+++ app/routes/listTrackBalance.js
@@ -5,6 +5,7 @@ const Bets = require("../models/bets");
 async function listTrackBalance(req, res) {
   try {
     const userId = parseInt(req.params.userId);
+
     const betList = await Bets.aggregate([
       {
         $match: {
@@ -16,7 +17,7 @@ async function listTrackBalance(req, res) {
         $lookup: {
           from: "deposits",
           localField: "userId",
-          foreignField: userId,
+          foreignField: "userId",
           as: "depositDetail",
         },
       },
@@ -25,7 +26,7 @@ async function listTrackBalance(req, res) {
       },
       {
         $lookup: {
-          from: "Events",
+          from: "inplayevents",
           localField: "depositDetail.matchId",
           foreignField: "_id",
           as: "eventDetail",
@@ -38,53 +39,24 @@ async function listTrackBalance(req, res) {
       },
       {
         $group: {
-          _id: "$_id",
-          position: { $first: "$position" }, // Assuming position is the field that represents exposure in the bets collection
+          _id: {
+            marketId: "$marketId",
+            eventId: "$eventDetail.Id",
+            userId: "$userId",
+          },
+          position: { $first: "$position" },
           totalDeposit: { $sum: "$depositDetail.amount" },
           data: { $first: "$$ROOT" },
         },
-      },
-      {
-        $project: {
-          _id: 0,
-          betId: "$data._id",
-          price: "$data.betRate",
-          runnersPosition: "$data.runnersPosition",
-          calculateExp: "$data.calculateExp",
-          runnerId: "$data.runnerName",
-          createdAt: "$data.createdAt",
-          size: "$data.betAmount",
-          runner: "$data.runner",
-          marketId: "$data.marketId",
-          betRate: "$data.betRate",
-          type: "$data.type",
-          isfancyOrbookmaker: "$data.isfancyOrbookmaker",
-          fancyData: "$data.fancyData",
-          fancyRate: "$data.fancyRate",
-          betSession: "$data.betSession",
-          roundId: "$data.roundId",
-          asianTableId: "$data.asianTableId",
-          marketId: "$data.marketId",
-          betAmount: "$data.betAmount",
-          exposureAmount: "$data.exposureAmount",
-          sportsId: "$data.sportsId",
-          totalDeposit: 1,
-          position: 1,
-        },
-      },
+      }
     ]);
-
-    const total = await Bets.countDocuments({ userId: userId, calculateExp: true });
-
-    // Check if totalDeposit is equal to the position for each bet
-    const isBalanceMatching = betList.every((bet) => bet.totalDeposit === bet.position);
-
+    
     res.status(200).json({ success: true, data: betList, total: total, isBalanceMatching: isBalanceMatching });
   } catch (err) {
-    res.status(500).json({ success: false, msg: "Failed to get bet list" });
+    res.status(500).json({ success: false, msg: `Failed to get bet list:  ${err.message}` });
   }
 }
 
-router.post("/listTrackBalance/:userId", listTrackBalance);
+router.get("/listTrackBalance/:userId", listTrackBalance);
 
 module.exports = { router };
