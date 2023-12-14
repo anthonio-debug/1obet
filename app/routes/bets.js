99ddec845a2a81c57622a4eff14b473d1d46c9b1
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-12-15 02:53:32 +0500

    deposit

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -4036,7 +4036,7 @@ const SingleUserAllBets = async (req, res) => {
         }
       },
       {
-        $match: { "Wdeposit.userId": Number(req.query.userId) }
+        $match: { "deposit.userId": Number(req.query.userId) }
       }
     ]).exec();
 
