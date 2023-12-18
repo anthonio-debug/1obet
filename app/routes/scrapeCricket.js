cf4f047a77e02cb49f97a8ea34ad5581a615a3ff
keepsimple7 <keepsimple95@outlook.com>
2023-12-18 11:51:18 +0900

    log: socket live

--- app/routes/scrapeCricket.js
+++ app/routes/scrapeCricket.js
@@ -58,6 +58,7 @@ async function updateCricketData(req, res) {
   if (type === 'live') {
     const socketData = convertSchema(entities)
     if (socketData) {
+      console.log('cricket live socket', socketData)
       io.emit('score', socketData)
     }
   }
