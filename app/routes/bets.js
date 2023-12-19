1a7879e855ae52b5f6a53fafc81e5ddad9abcfb9
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-12-19 16:19:22 +0500

    currentOver

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1701,7 +1701,8 @@ const placeBet = async (req, res) => {
       }
       let type = eventDetail.matchType;
       let inning = scores.inning;
-      let currentOver = inning === 1 ? scores.over1 : scores.over2
+      let currentOver = inning == 1 ? scores.over1 : scores.over2
+      console.log(`currentOver ==== ${scores.over1}  ${scores.over2}  ${currentOver}`);
       let score = inning === 1 ? scores.score1 : scores.score2
       let sessionAddition = 0;
       if (inning == 2){
