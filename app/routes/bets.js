5fcfc3bd00fd1bba4b18ffcff212d8bd94a95c06
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-12-20 01:46:11 +0500

    runners instead of Runners

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -660,8 +660,9 @@ const placeBet = async (req, res) => {
             // const response = await axios.get(url);
             // const oddsData = response.data;
             const oddsData = await apiCallForOdds(id);
-            console.log(" ==============================  oddsData[0]?.Runners", oddsData[0]?.Runners);
-            const runnerFromAPI = oddsData[0]?.Runners.find(
+            
+            console.log(" ==============================  oddsData[0]?.runners", oddsData[0]?.runners);
+            const runnerFromAPI = oddsData[0]?.runners?.find(
               (runner) => runner.SelectionId == selectionId
             );
             let selectedOddsValue = 0;
