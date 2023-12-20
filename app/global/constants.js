1609d3d728183157688069764e1de14b91cfaab1
keepsimple7 <keepsimple95@outlook.com>
2023-12-20 11:33:04 +0900

    feat: const to es5

--- app/global/constants.js
+++ app/global/constants.js
@@ -1,10 +1,19 @@
 // export const API_DOMAIN = `betfairoddsapi.com`
-export const API_DOMAIN = `bfoddsapi.in`
+const API_DOMAIN = `bfoddsapi.in`
 
-export const FANCY_URL = `https://betfairoddsapi.com:3444/api`
-export const LIVE_BET_TV_URL = `https://betfairoddsapi.com:3445/api`
-export const ASIAN_URI = `https://${API_DOMAIN}:3445/api`
-export const CALC_FANCY_URI = `https://${API_DOMAIN}:3444/api`
-export const CALC_LIVE_BET_TV_URI = `https://${API_DOMAIN}:3445/api`
-export const FANCY_URI = `https://${API_DOMAIN}:3443/api`
+const FANCY_URL = `https://betfairoddsapi.com:3444/api`
+const LIVE_BET_TV_URL = `https://betfairoddsapi.com:3445/api`
+const ASIAN_URI = `https://${API_DOMAIN}:3445/api`
+const CALC_FANCY_URI = `https://${API_DOMAIN}:3444/api`
+const CALC_LIVE_BET_TV_URI = `https://${API_DOMAIN}:3445/api`
+const FANCY_URI = `https://${API_DOMAIN}:3443/api`
 
+module.exports = {
+  API_DOMAIN,
+  FANCY_URL,
+  LIVE_BET_TV_URL,
+  ASIAN_URI,
+  CALC_FANCY_URI,
+  CALC_LIVE_BET_TV_URI,
+  FANCY_URI,
+}
