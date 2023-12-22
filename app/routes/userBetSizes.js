f0c33178485e0356558034f491ce52e58b288d03
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-12-22 20:25:37 +0500

    _id

--- app/routes/userBetSizes.js
+++ app/routes/userBetSizes.js
@@ -116,8 +116,8 @@ async function getAllBetSizes(req, res) {
         {
           $lookup: {
             from: "betlimits",
-            localField: "_id",
-            foreignField: "betSizeId",
+            localField: "betSizeId",
+            foreignField: "_id",
             as: "limits",
           },
         },
