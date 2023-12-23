b6cb2d1752aeb4e13f464e07feda405c45551a62
keepsimple7 <keepsimple95@outlook.com>
2023-12-23 17:23:12 +0200

    fix

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -66,7 +66,7 @@ function ToolForResults() {
 
       for (const result of results) {
         // console.log("result.betDocument.length:-->", result.betDocument.length);
-        console.log("result.betDocument.length:-->", result.betDocument.length, "--- result--> ", result);
+        console.log("result.betDocument.length:-->", result.betDocument.length);
         console.log("result.betDocument.sportsId:-->", result.betDocument.sportsId);
         if (!result.betDocument) continue;
 
