382898a1cecaac2d4024eb1681244f8a352e5e43
keepsimple7 <keepsimple95@outlook.com>
2023-12-25 07:23:44 +0200

    feat

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -24,13 +24,13 @@ function ToolForEvent() {
 
       setInterval(fetchEvents, 2 * 60 * 1000);
       setInterval(fetchMarkets, 10 * 1000);
-      setInterval(handleSetInplay, 10 * 1000);
+      // setInterval(handleSetInplay, 10 * 1000);
 
-      // setInterval(() => {
-      //   for (const sportsId of sportsIds) {
-      //     apiRequests.setInplay(sportsId);
-      //   }
-      // }, 10 * 1000);
+      setInterval(() => {
+        for (const sportsId of sportsIds) {
+          apiRequests.setInplay(sportsId);
+        }
+      }, 10 * 1000);
 
       setInterval(() => {
         for (const sportsId of sportsIds) {
