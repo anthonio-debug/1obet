d3747364b0d8bbe1b312f38ce2af1efad87fd195
muhammadazam52564 <muhammadazam52564@gmail.com>
2023-12-29 03:15:32 +0500

    Teast change in ledger ...

--- app/routes/deposits.js
+++ app/routes/deposits.js
@@ -618,13 +618,14 @@ function getLedgerDetails(req, res) {
 
       cashPipeline.push({
         $group: {
-          _id: {
-            $cond: {
-              if: { $in: ["$cashOrCredit", ['Cash', 'Credit']] }, 
-              then: "$_id", 
-              else: "$marketId"
-            }
-          },
+          _id: "$_id",
+          //  {
+          //   $cond: {
+          //     if: { $in: ["$cashOrCredit", ['Cash', 'Credit']] }, 
+          //     then: "$_id", 
+          //     else: "$marketId"
+          //   }
+          // },
           originalId: { $first: "$_id" },
           description:  { $first: "$description" },
           amount:  { $sum: "$amount" },
