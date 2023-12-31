8a914dafd8540a4c2a185d1b37b5152daa95ab30
CryptoFan <86724931+cryptofan35@users.noreply.github.com>
2023-12-30 16:34:03 -0800

    uncomment ToolForListEvent

--- apiSystem.js
+++ apiSystem.js
@@ -94,7 +94,7 @@ async function main() {
   // ToolForTestSport.init(io, express);
 
   // init events list
-  // ToolForListEvent.init(io, express);
+  ToolForListEvent.init(io, express);
 
   // init events list
   ToolForResult.init(io, express);
