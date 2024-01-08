80e3481707b54e38b128509a3c1be2ccc5ad9319
muhammadazam52564 <muhammadazam52564@gmail.com>
2024-01-08 17:30:32 +0500

    days  check in dates

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -3918,8 +3918,9 @@ const eventsAPICalls  = async (req, res) => {
 const postmanwork = async (req, res) => {
 
   try {
+    const _30dayAgo = new Date(new Date().setDate(new Date().getDate() - 30)).getTime();
     if(Number(req.body.type) === 2){
-      const bets = await Bets.find({});
+      const bets = await Bets.find({ createdAt:  _30dayAgo });
       for (const bet of bets){
         const resp = await  Cash.updateMany(
           {betId: bet._id},
