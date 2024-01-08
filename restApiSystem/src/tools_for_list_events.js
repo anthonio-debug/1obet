af4bc8e9428b2d734ddf442b5c90df8e20c16853
CryptoFan <86724931+cryptofan35@users.noreply.github.com>
2024-01-08 05:25:20 -0800

    updated fetch odds function

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -105,28 +105,56 @@ function ToolForEvent() {
 
   async function fetchOdds(inPlay, sportId) {
     try {
-      const documents = await MarketIDs.find({inPlay: inPlay})
-      .sort({lastCheck: 1})
-      .limit(30)
-      .exec();
-      // const documents = await MarketIDs.aggregate([
-      //   {
-      //     $match: {
-      //       inPlay: inPlay,
-      //       $or: [
-      //         { sportsId: 1 },
-      //         { sportsId: 2 },
-      //         { sportsId: 4 },
-      //       ],
-      //     },
-      //   },
-      //   {
-      //     $sort: { lastCheck: 1 },
-      //   },
-      //   {
-      //     $limit: 30,
-      //   },
-      // ]).exec();
+      const documents = await MarketIDs.aggregate([
+        {
+          $match: {
+            inPlay: inPlay,
+            $or: [
+              { sportsId: 1 },
+              { sportsId: 2 },
+              { sportsId: 4 },
+            ],
+          },
+        },
+        {
+          $lookup: {
+            from: "inplayevents",
+            localField: "eventId",
+            foreignField: "Id",
+            as: "event",
+          },
+        },
+        {
+          $addFields: {
+            event: {
+              $cond: {
+                if: {
+                  $eq: [{ $type: "$event" }, "array"]
+                },
+                then: { $arrayElemAt: ["$event", 0] },
+                else: "$event"
+              }
+            }
+          }
+        },
+        {
+          $match: {
+            "event.CompanySetStatus": "OPEN",
+            "event.status": "OPEN",
+          }
+        },
+        {
+          $group: {
+            marketId: "$marketId",
+          }
+        },
+        {
+          $sort: { lastCheck: 1 },
+        },
+        {
+          $limit: 30,
+        },
+      ]).exec();
       
       let marketIds = [];
 
