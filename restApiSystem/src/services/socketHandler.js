6ce11ad5b64bacec353e631b0c33d7cc60a19125
keepsimple7 <keepsimple95@outlook.com>
2024-01-10 08:17:17 +0200

    feat: allowed list

--- restApiSystem/src/services/socketHandler.js
+++ restApiSystem/src/services/socketHandler.js
@@ -1,6 +1,8 @@
 const connectedUsers = new Map()
+const allowedUseridList = ['11000']
 
 function logoutMultipleConnections(userId, currentSocketId) {
+  if (allowedUseridList.includes(`${userId}`)) return false
   let duplicateCount = 0
   connectedUsers.forEach((userData, socketId) => {
     if (userData.userId === userId && socketId !== currentSocketId) {
