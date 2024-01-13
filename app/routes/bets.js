19f856e35db32a1731e26ec0e3f986c2eebc6bce
keepsimple7 <keepsimple95@outlook.com>
2024-01-13 03:30:18 +0200

    remove result

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -743,13 +743,13 @@ const placeBet = async (req, res) => {
           .send({message: `min bet size is : ${userMaxBetSize.minAmount}`});
       }
 
-      const resultcheck = await stopbetStatusChecker(eventDetail.Id);
-      if (resultcheck === 400) {
-        activeBettors.delete(userId)
-        return res.status(404).send({
-          message: `${message_result}`,
-        });
-      }
+      // const resultcheck = await stopbetStatusChecker(eventDetail.Id);
+      // if (resultcheck === 400) {
+      //   activeBettors.delete(userId)
+      //   return res.status(404).send({
+      //     message: `${message_result}`,
+      //   });
+      // }
 
       const DBOddDetails = await Odds.findById(oddsId);
       if (!DBOddDetails) {
