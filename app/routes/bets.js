d9451d590dbca8c0ac69668a8de12b894ce1f403
muhammadazam52564 <muhammadazam52564@gmail.com>
2024-01-23 15:54:55 +0500

    bowling review result  keyword in array

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -147,7 +147,7 @@ const stopbetStatusChecker = async (id) => {
       const stopbetStatus = ["no ball", "noball", "free hit", "freehit",
         "thirdumpire", "third umpire", "review", "stumps", "bad", "crowed",
         "rain", "suspend", "delay", "pitch", "plood", "bowled", "injured",
-        "rain stops play"];
+        "rain stops play", "bowling review"];
         const regexPattern = new RegExp(stopbetStatus.map(word => `\\b${word.replace(/\s+/g, '\\s+')}\\b`).join('|'), 'i');
         if (regexPattern.test(result)){
             return 400
