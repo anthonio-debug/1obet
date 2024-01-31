0a0c487fe2fd74f6baea4197458b5fe17fd63fc2
keepsimple7 <keepsimple95@outlook.com>
2024-01-31 16:20:41 +0200

    fix: sports

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -70,9 +70,9 @@ function ToolForEvent() {
             .exec();
         } else {
           const now = new Date()
-          const from = now.getTime()
-          const tenHoursLater = new Date(now.getTime() + 10 * 60 * 60 * 1000)
-          const to = tenHoursLater.getTime()
+          const from = new Date(now.getTime() - (30 * 60 * 1000))
+          const someHoursLater = new Date(now.getTime() + 10 * 60 * 60 * 1000)
+          const to = someHoursLater.getTime()
           documents = await inPlayEvents.findOne({
             status: 'OPEN',
             CompanySetStatus: "OPEN",
