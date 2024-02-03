28fc24d691127527437f77354dc163150bb686f5
keepsimple7 <keepsimple95@outlook.com>
2024-02-03 13:25:40 +0200

    feat: soccer api

--- app/routes/listCricket.js
+++ app/routes/listCricket.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Crickets = require("../models/Crickets");
 const InPlayEvents = require("../models/events")
+const Score = require("../models/score")
 let config = require('config');
 
 async function listCricket(req, res) {
@@ -91,8 +92,96 @@ async function editCricket(req, res) {
   }
 }
 
+async function listSoccer(req, res) {
+  let query = {};
+  let page = 1;
+  let sort = -1;
+  var limit = config.pageSize;
+  if (
+    req.query.numRecords &&
+    !isNaN(req.query.numRecords) &&
+    req.query.numRecords > 0
+  )
+    limit = Number(req.query.numRecords);
+  if (req.query.sort) sort = Number(req.query.sort);
+  if (req.query.page) page = Number(req.query.page);
+
+  // Crickets.find(query)
+  // .sort({ state: 'live', timestamp: -1 })
+  Score.aggregate([
+    {
+      $addFields: {
+        sortField: {
+          $switch: {
+            branches: [
+              { case: { $eq: ["$state", "live"] }, then: 1 },
+              { case: { $eq: ["$state", "info"] }, then: 2 }
+            ],
+            default: 3
+          }
+        }
+      }
+    },
+    { $sort: { sortField: 1, timestamp: 1 } }
+  ])
+    .exec((err, allRecords) => {
+      if (err) return res.status(404).send({message: 'Something went wrong'});
+
+      // Apply custom sorting logic
+      // const sortedRecords = allRecords.sort((a, b) => {
+      //   if (a.state === 'live' && b.state !== 'live') {
+      //     return -1;
+      //   } else if (a.state !== 'live' && b.state === 'live') {
+      //     return 1;
+      //   } else {
+      //     // If states are the same or both not 'live', sort by timestamp
+      //     return b.timestamp - a.timestamp;
+      //   }
+      // });
+
+      // Implement your own pagination logic
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+      const paginatedRecords = allRecords.slice(startIndex, endIndex);
+
+      return res.send({
+        success: true,
+        message: 'Paginated and sorted Crickets list',
+        total: allRecords.length,
+        results: paginatedRecords,
+      });
+    });
+}
+
+async function editSoccer(req, res) {
+  const {_id, eventId} = req.body;
+
+  try {
+    await Score.updateOne(
+      {_id: _id},
+      {$set: {eventId: eventId}},
+    );
+
+    const cricketInfo = await Score.findOne({_id: _id})
+
+    const scoreKey = cricketInfo.scoreKey;
+
+    await InPlayEvents.updateOne(
+      {Id: eventId},
+      {$set: {seriesKey: scoreKey}},
+    );
+
+    // Check if the update was successful
+    res.status(200).json({success: true, message: 'Cricket updated successfully'});
+  } catch (error) {
+    console.error('Error updating cricket:', error);
+    res.status(500).json({success: false, message: 'Internal server error'});
+  }
+}
 
 router.get("/listCricket", listCricket);
 router.post("/editCricket", editCricket);
+router.get("/list-soccer", listSoccer);
+router.get("/edit-soccer", editSoccer);
 
 module.exports = {router};
