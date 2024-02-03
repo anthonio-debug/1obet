34fdc9ccfb2c8811960b870c13423746954b8a5b
keepsimple7 <keepsimple95@outlook.com>
2024-02-03 11:42:35 +0200

    fix: score

--- app/routes/scrapeSoccer.js
+++ app/routes/scrapeSoccer.js
@@ -20,7 +20,7 @@ async function updateSoccerScore(req, res) {
       for (const item of entities) {
         await Score.findOneAndUpdate(
           {scoreKey: item.scoreKey},
-          {$set: {data: item, sportsId: '1'}},
+          {$set: {data: item, sportsId: '1', scoreKey: item.seriesKey}},
           {upsert: true, new: false, setDefaultsOnInsert: true}
         );
       }
@@ -32,7 +32,7 @@ async function updateSoccerScore(req, res) {
       for (const item of entities) {
         await Score.findOneAndUpdate(
           {scoreKey: item.seriesKey},
-          {$set: {data: item, sportsId: '1'}},
+          {$set: {data: item, sportsId: '1', scoreKey: item.seriesKey}},
           {upsert: true, new: true, setDefaultsOnInsert: true}
         );
       }
