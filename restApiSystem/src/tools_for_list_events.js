408d95f68702f9e5fa72fc87b30a809b3d559acd
keepsimple7 <keepsimple95@outlook.com>
2024-02-08 11:43:43 +0200

    fix: ignore boomaker and another market

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -96,7 +96,13 @@ function ToolForEvent() {
               {$set: {inplay: true}}
             );
           }
-          const existedMarkets = await MarketIDs.findOne({eventId: documents.Id, status: "OPEN", inPlay: true})
+          const existedMarkets = await MarketIDs.findOne({
+            eventId: documents.Id, status: "OPEN", inPlay: true,
+            $and: [
+              {marketName: {$ne: 'BOOKMAKER'}},
+              {marketName: {$ne: null}},
+            ]
+          })
 
           if (documents && !existedMarkets?._id) {
             await inPlayEvents.updateMany(
