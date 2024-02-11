48e095beca332d8d4ed56fb98ed62a8569de71d4
keepsimple7 <keepsimple95@outlook.com>
2024-02-11 17:34:49 +0200

    fix: bet lock

--- app/routes/betLocks.js
+++ app/routes/betLocks.js
@@ -1,86 +1,84 @@
-const express               = require('express');
-const { validationResult }  = require('express-validator');
-let config                  = require('config');
-const betLockValidator      = require('../validators/betLocks');
-const User                  = require('../models/user');
-const Market                = require('../models/marketTypes');
-const Events                = require('../models/events');
-const SubMarket             = require('../models/subMarketTypes');
-const loginRouter           = express.Router();
+const express = require('express');
+const {validationResult} = require('express-validator');
+let config = require('config');
+const betLockValidator = require('../validators/betLocks');
+const User = require('../models/user');
+const Market = require('../models/marketTypes');
+const Events = require('../models/events');
+const SubMarket = require('../models/subMarketTypes');
+const loginRouter = express.Router();
 
 async function addBetLock(req, res) {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send({ errors: errors.errors });
+      return res.status(400).send({errors: errors.errors});
     }
-    const { matchId, allUsers, lock, matchOdds, userIds } = req.body;
+    const {matchId, allUsers, lock, matchOdds, userIds} = req.body;
     console.log("matchId ==== ", matchId);
     const event = await Events.findById(matchId)
-    if(!event){
-      res.status(404).send({ message: 'Event Id is Invalid' });
+    if (!event) {
+      res.status(404).send({message: 'Event Id is Invalid'});
     }
 
     const marketId = event.sportsId
     console.log("marketId ====== ", marketId);
-    let subMarketIds 
-    if(matchOdds){
+    let subMarketIds
+    if (matchOdds) {
       console.log("matchOdds ====== ");
       subMarketIds = await SubMarket.distinct('Id', {
         name: 'Match Odds',
         marketId: marketId
       })
-    }else {
-      subMarketIds = await  SubMarket.distinct('Id', {
-        name: { $ne: 'Match Odds' },
+    } else {
+      subMarketIds = await SubMarket.distinct('Id', {
+        name: {$ne: 'Match Odds'},
         marketId: marketId
       })
     }
 
-    if(allUsers && lock){
+    if (allUsers && lock) {
       console.log(" All Users && Lock ");
-      const users =  await User.find({ createdBy: req.query.userId})
-      users.map((user)=>{
+      const users = await User.find({createdBy: req.query.userId})
+      users.map(async (user) => {
         let blockedSubMarkets = user.blockedSubMarketsByParent
-        let allSubMarkets     = blockedSubMarkets.concat(subMarketIds)
+        let allSubMarkets = blockedSubMarkets.concat(subMarketIds)
         const finalSubMarkets = [...new Set(allSubMarkets)];
         user.blockedSubMarketsByParent = finalSubMarkets
-        user.save();
+        await user.save();
       });
-    }
-    else if(allUsers && !lock){
+    } else if (allUsers && !lock) {
       console.log(" Not  All Users && Lock");
-      const users =  await User.find({ createdBy: req.query.userId})
-      users.map((user)=>{
+      const users = await User.find({createdBy: req.query.userId})
+      users.map((user) => {
         let blockedSubMarkets = user.blockedSubMarketsByParent
-        let allSubMarkets     = blockedSubMarkets.concat(subMarketIds)
+        // let allSubMarkets = blockedSubMarkets.concat(subMarketIds)
         const finalSubMarkets = blockedSubMarkets.filter(item => !subMarketIds.includes(item));
         user.blockedSubMarketsByParent = finalSubMarkets
         user.save();
       });
 
-    }
-    else if(!allUsers){
+    } else if (!allUsers) {
       console.log(" Not  All Users ");
 
-      const usersToUnlock =  await User.find({ createdBy: req.query.userId, userId: { $nin: userIds }})
-      usersToUnlock.map((user)=>{
+      const usersToUnlock = await User.find({createdBy: req.query.userId, userId: {$nin: userIds}})
+      usersToUnlock.map((user) => {
         let blockedSubMarkets = user.blockedSubMarketsByParent
         const finalSubMarkets = blockedSubMarkets.filter(item => !subMarketIds.includes(item));
         user.blockedSubMarketsByParent = finalSubMarkets
         user.save();
       });
 
-      const usersToLock =  await User.find({ userId: { $in: userIds }})
-      usersToLock.map((user)=>{
+      const usersToLock = await User.find({userId: {$in: userIds}})
+      usersToLock.map((user) => {
         let blockedSubMarkets = user.blockedSubMarketsByParent
-        let allSubMarkets     = blockedSubMarkets.concat(subMarketIds)
+        let allSubMarkets = blockedSubMarkets.concat(subMarketIds)
         const finalSubMarkets = [...new Set(allSubMarkets)];
         user.blockedSubMarketsByParent = finalSubMarkets
         user.save();
       });
 
-    } 
+    }
     return res.send({
       success: true,
       message: 'Betlock created successfully',
@@ -88,7 +86,7 @@ async function addBetLock(req, res) {
     });
   } catch (err) {
     console.error(err);
-    res.status(404).send({ message: 'betlock not saved' });
+    res.status(404).send({message: 'betlock not saved'});
   }
 }
 
@@ -98,4 +96,4 @@ loginRouter.post(
   addBetLock
 );
 
-module.exports = { loginRouter };
+module.exports = {loginRouter};
