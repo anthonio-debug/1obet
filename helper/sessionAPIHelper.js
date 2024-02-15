81a3fd3007bb66925ac25e4b3420ecbada3a4b5a
keepsimple7 <keepsimple95@outlook.com>
2024-02-16 00:08:27 +0200

    feat: session api

---
+++ helper/sessionAPIHelper.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const SESSION_API_URI = `http://142.93.36.1/api/v1`
+
+async function fetchSession(eventId) {
+  try {
+    // const eventId = '33002177'
+    const response = await axios.get(`${SESSION_API_URI}/listMarketBookSession?match_id=${eventId}`)
+    let res = response.data;
+    // console.log('session list: ', JSON.stringify(res))
+    return res || []
+  } catch (err) {
+    console.error('session api fetchSession: ', err)
+    return []
+  }
+}
+
+async function fetchMarketOdds(marketId) {
+  try {
+    // const marketId = '1.166536383'
+    const response = await axios.get(`${SESSION_API_URI}/listMarketBookOdds?market_id=1.166536383${marketId}`)
+    let res = response.data;
+    // console.log('session list: ', JSON.stringify(res))
+    return res || []
+  } catch (err) {
+    console.error('session api fetchSession: ', err)
+    return []
+  }
+}
+
+module.exports = {fetchSession, fetchMarketOdds}
