74d7907b81d56a1b7a5fff489ae8435f796c714a
keepsimple7 <keepsimple95@outlook.com>
2024-02-16 11:55:07 +0200

    fix: horse racing api

--- restApiSystem/src/api/apiRequestResult.js
+++ restApiSystem/src/api/apiRequestResult.js
@@ -103,73 +103,6 @@ function apiRequestResult() {
       });
   }
 
-  async function getRacingResult_old(eventId, sportsId, competitionId) {
-
-    console.log('getWaitingResult for Racings ');
-
-    const currentTime = new Date().getTime();
-    await MarketIDs.updateMany({eventId: eventId}, {lastResultCheckTime: currentTime})
-
-    // console.log(marketIds);
-
-    try {
-      const requestData = {
-        "filter": {
-          "eventIds": [eventId],
-          "eventTypeIds": [sportsId],
-          "marketTypes": ["WIN", "PLACE"],
-        },
-        "maxResults": 30,
-        "marketProjection": ["EVENT", "EVENT_TYPE", "MARKET_START_TIME", "RUNNER_DESCRIPTION", "RUNNER_METADATA", "COMPETITION"]
-      }
-
-      const url = `${horseRaceUrl}/listMarketCatalogue`;
-      let response = await axios.post(
-        url,
-        requestData,
-        header
-      );
-      const results = response.data.result;
-      const markets = await MarketIDs.find({eventId: eventId}).exec()
-      // console.log(results);
-      for (let index = 0; index < results.length; index++) {
-        const result = results[index];
-        const marketIndex = _.findIndex(markets, function (o) {
-          return o.marketId == result.marketId;
-        });
-        if (marketIndex != -1) {
-          if (result.winnerSelectionId == '-1') {
-            await MarketIDs.findOneAndUpdate({_id: markets[marketIndex]._id}, {$set: {winnerInfo: 'Canceled'}});
-            await Events.findOneAndUpdate({Id: markets[marketIndex].eventId}, {$set: {winner: 'Canceled'}});
-            continue;
-          }
-          if (typeof markets[marketIndex].runners !== 'undefined') {
-            const runnerIndex = _.findIndex(markets[marketIndex].runners, function (o) {
-              return o.SelectionId == result.winnerSelectionId;
-            });
-            if (runnerIndex != -1) {
-              await MarketIDs.findOneAndUpdate({_id: markets[marketIndex]._id}, {$set: {winnerInfo: markets[marketIndex].runners[runnerIndex].runnerName}});
-              await Events.findOneAndUpdate({Id: markets[marketIndex].eventId}, {$set: {winner: markets[marketIndex].runners[runnerIndex].runnerName}});
-            } else {
-              await MarketIDs.findOneAndUpdate({_id: markets[marketIndex]._id}, {$set: {winnerInfo: result.winnerSelectionId}});
-              await Events.findOneAndUpdate({Id: markets[marketIndex].eventId}, {$set: {winner: result.winnerSelectionId}});
-            }
-          } else {
-            await MarketIDs.findOneAndUpdate({_id: markets[marketIndex]._id}, {$set: {winnerInfo: result.winnerSelectionId}});
-            await Events.findOneAndUpdate({Id: markets[marketIndex].eventId}, {$set: {winner: result.winnerSelectionId}});
-
-          }
-          await Events.findOneAndUpdate({Id: markets[marketIndex].eventId}, {$set: {isResultSaved: true}});
-
-        } else {
-          console.log('Record not found in getRacingResult');
-        }
-      }
-    } catch (error) {
-      console.error("getRacingResult", error);
-    }
-  }
-
   async function getRacingResult(markets) {
 
     console.log('=========== getWaitingResult for Racings');
@@ -185,7 +118,7 @@ function apiRequestResult() {
       const requestData = {
         "marketIds": marketIds
       }
-      const url = `${horseRaceUrl}/listMarketBook`;
+      const url = `${config.newThirdURL}/listMarketBook`;
       let response = await axios.post(
         url,
         requestData,
