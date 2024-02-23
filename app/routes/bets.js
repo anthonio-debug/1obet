16a5681af935fc6eee8a263915f857ae4d1fd32d
keepsimple7 <keepsimple95@outlook.com>
2024-02-23 16:22:05 +0200

    fix: gamestatus

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1721,7 +1721,7 @@ const placeBet = async (req, res) => {
       // const apiFancyOddsRes = await getFancyOdds([selectionId])
       let apiFancyOddsRes = await fetchSession(eventDetail.Id)
       apiFancyOddsRes = apiFancyOddsRes.filter(item => item.SelectionId === selectionId)
-      if (apiFancyOddsRes[0]?.GameStatus !== 'ACTIVE') {
+      if (apiFancyOddsRes[0]?.GameStatus === 'SUSPENDED' || apiFancyOddsRes[0]?.GameStatus === 'Ball Running') {
         activeBettors.delete(userId)
         return res.status(404).send({
           message: `Status not available for selected team ${selectionId}`,
