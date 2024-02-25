63b8476d863fa1e76b2e0d1da46a2a8063eebe26
stepinsolution <bhattitayab747@gmail.com>
2024-02-25 13:26:46 +0400

    fancy delay changed to 5sec

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2354,7 +2354,7 @@ const placeBet = async (req, res) => {
     if (delayExcludedMarkets.includes(subMarketDetail.Id)) {
       delay = 1;
       if (subMarketDetail.Id == config.Fancy || subMarketDetail.Id == config.BookMaker) {
-        delay = 5000;
+        delay = 4900;
       }
     }
 
