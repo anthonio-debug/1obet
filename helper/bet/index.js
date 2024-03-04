02d69d2c395f1d27a013b9be12b84644fbab4c3a
keepsimple7 <keepsimple95@outlook.com>
2024-03-04 21:57:45 +0200

    fix

--- helper/bet/index.js
+++ helper/bet/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const checkActiveBettors = async (bet) => {
   const userId  =bet?.userId
-  const activeBettors = await axios.get('http://localhost:4000/active-bettors')
+  // const activeBettors = await axios.get('http://localhost:4000/active-bettors')
   return false
   // return global.ActiveBettors.has(userId)
 }
