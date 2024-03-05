08d5b3ede28432f40f2be8ebef14b6a5caee9597
keepsimple7 <keepsimple95@outlook.com>
2024-03-05 19:54:26 +0200

    fix

--- helper/bet/index.js
+++ helper/bet/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const checkActiveBettors = async (bet) => {
-  try {
   const userId  =bet?.userId
+  try {
   // const url = `https://production.1obet.net/api/active-bettors`
   const url = `'http://127.0.0.1:4000/api/active-bettors'`
 
