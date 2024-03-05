bd2d741ab9613f8a566e136c37d32c862d54e79a
keepsimple7 <keepsimple95@outlook.com>
2024-03-05 20:06:17 +0200

    fix

--- helper/bet/index.js
+++ helper/bet/index.js
@@ -3,7 +3,8 @@ const checkActiveBettors = async (bet) => {
   const userId  =bet?.userId
   try {
   // const url = `https://production.1obet.net/api/active-bettors`
-  const url = `'http://127.0.0.1:4000/api/active-bettors'`
+  // const url = `'http://127.0.0.1:4000/api/active-bettors'`
+  const url = `'http://localhost:4000/api/active-bettors'`
 
   const activeBettorsRes = await axios.get(url)
   const activeBettors = new Map(Object.entries(activeBettorsRes?.data?.results));
