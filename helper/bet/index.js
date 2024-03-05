fb61cb9d1c3e9278ac50eaa292d289bf65902626
keepsimple7 <keepsimple95@outlook.com>
2024-03-05 19:53:30 +0200

    fiat

--- helper/bet/index.js
+++ helper/bet/index.js
@@ -1,13 +1,18 @@
 const axios = require("axios");
 const checkActiveBettors = async (bet) => {
+  try {
   const userId  =bet?.userId
-  const url = `https://production.1obet.net/api/active-bettors`
-  // const url = `'http://127.0.0.1:4000/api/active-bettors'`
+  // const url = `https://production.1obet.net/api/active-bettors`
+  const url = `'http://127.0.0.1:4000/api/active-bettors'`
 
   const activeBettorsRes = await axios.get(url)
   const activeBettors = new Map(Object.entries(activeBettorsRes?.data?.results));
+    console.log('checkActiveBettors: ', userId)
   return activeBettors.has(`${userId}`)
-  // return false
+  } catch (error) {
+    console.error('checkActiveBettors: ', userId, error?.data || error.message || error)
+    return false
+  }
 }
 
 module.exports = { checkActiveBettors }
