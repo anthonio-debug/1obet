5a03a1bc41ac0da07f30b34abfb49985cdd606e7
keepsimple7 <keepsimple95@outlook.com>
2024-03-05 19:34:56 +0200

    fix

--- helper/bet/index.js
+++ helper/bet/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const checkActiveBettors = async (bet) => {
   const userId  =bet?.userId
-  const activeBettorsRes = await axios.get('http://localhost:4000/api/active-bettors')
+  const activeBettorsRes = await axios.get('http://127.0.0.1:4000/api/active-bettors')
   const activeBettors = new Map(Object.entries(activeBettorsRes?.data?.results));
   return activeBettors.has(`${userId}`)
   // return false
