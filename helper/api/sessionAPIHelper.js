0a4b8b30d322d7774a3db943a7cb8c6458975d48
keepsimple7 <keepsimple95@outlook.com>
2024-03-19 16:17:13 +0200

    feat: sessionapi to v2

--- helper/api/sessionAPIHelper.js
+++ helper/api/sessionAPIHelper.js
@@ -1,14 +1,15 @@
 const axios = require("axios");
-const SESSION_API_URI = `http://142.93.36.1/api/v1`
+const SESSION_API_URI = `http://142.93.36.1/api/v2`
 
 async function fetchSession(eventId) {
   try {
     // const eventId = '33002177'
-    const url = `${SESSION_API_URI}/listMarketBookSession?match_id=${eventId}`
+    // http://142.93.36.1/api/v2/getSessions?EventTypeID=4&matchId=33061168
+    const url = `${SESSION_API_URI}/getSessions?EventTypeID=4&matchId=${eventId}`
     const response = await axios.get(url)
     let res = response.data;
     // console.log('session list: ', JSON.stringify(res))
-    return res || []
+    return JSON.parse(res) || []
   } catch (error) {
     console.error('session api fetchSession: ', eventId, error?.data || error.message || error)
     return []
