2cb499a1490953fc256fea7e316dfebe94d84324
stepinsolution <bhattitayab747@gmail.com>
2024-06-01 14:26:50 +0400

    console

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2877,7 +2877,9 @@ const placeBet = async (req, res) => {
       });
 
       let nowUser = await User.findOne({ userId }).exec();
+      const lastMaxWithdraw = await Cash.findOne({ userId: userId }).sort({ _id: -1 });
       console.log("===========================================================");
+      console.log(lastMaxWithdraw.availableBalance);
       if (nowUser.availableBalance < expAmount - prevExpAmount) {
         activeBettors.delete(userId)
         return res.status(404).send({ message: " Insufficient balance " });
