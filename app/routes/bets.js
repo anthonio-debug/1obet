8e776537d95f1e697271c599f0c936b0d382db11
stepinsolution <bhattitayab747@gmail.com>
2024-06-01 13:57:32 +0400

    console

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2874,7 +2874,7 @@ const placeBet = async (req, res) => {
         backFancyRate,
         layFancyRate,
       });
-      console.log("I am checking.............................................................");
+      
       let nowUser = await User.findOne({ userId }).exec();
       if (nowUser.availableBalance < expAmount - prevExpAmount) {
         activeBettors.delete(userId)
@@ -3057,7 +3057,7 @@ const placeBet = async (req, res) => {
           );
 
           activeBettors.delete(userId)
-
+          console.log("I am checking.............................................................");
           return res.send({
             success: true,
             message: "Bet placed successfully! ",
