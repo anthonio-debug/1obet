ffbf62c02660428b81ce80f1c89e3d305e998234
stepinsolution <bhattitayab747@gmail.com>
2024-06-04 14:42:49 +0400

    checking markets

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -238,14 +238,7 @@ try {
   const response = await axios.get(url2)
   let res = response.data;
   // console.log('session list: ', JSON.stringify(res))
-  if (isIterable(res)) {
-    const items = res.map((item) => {
-      return JSON.parse(item)
-    })
-    return items
-  } else {
-    return []
-  }
+  res.status(200).json({response: true, data: res})
 } catch (error) {
   console.log('url: ', url2)
   console.error('session api fetchSession: ', eventId, error?.data || error.message || error)
