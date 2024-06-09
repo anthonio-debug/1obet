051ea0c0a216c64caf4df658f7c480ff8f892446
stepinsolution <bhattitayab747@gmail.com>
2024-06-09 22:55:12 +0400

    added getScoreLimitlessByEventId function in sportsTestAPI

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -622,7 +622,7 @@ async function getScoreLimitlessByEventId(req, res) {
   } catch (error) {
     res
       .status(500)
-      .json({ success: false, msg: "Failed and Error: " + "------=="  + error.message });
+      .json({ success: false, msg: "Failed and Error: " + "------=="  + response });
   }
 }
 
