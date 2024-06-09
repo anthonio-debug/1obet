67d48559f0abaa5db141825cad54ed142792c688
stepinsolution <bhattitayab747@gmail.com>
2024-06-09 23:03:09 +0400

    added getScoreLimitlessByEventId function in sportsTestAPI

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -614,7 +614,7 @@ async function getOddsLimitlessByMarketId(req, res) {
 async function getScoreLimitlessByEventId(req, res) {
   const eventId = req.params.eventId;
 
-  const url = `http://142.93.36.1/api/v2/score?EventTypeID=4&matchId=33330794`;
+  const url = `http://142.93.36.1/api/v2/score?EventTypeID=4&matchId=${eventId}`;
   try {
     const response = await axios.get(url);
     res.status(200).json({ success: true, data: response.data });
@@ -622,6 +622,7 @@ async function getScoreLimitlessByEventId(req, res) {
     res.status(500).json({ success: false, msg: "Failed and Error: " + error.message });
   }
 }
+
 
 
 router.get('/testSports/events', listEvents)
