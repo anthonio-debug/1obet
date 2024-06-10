2cec0360c942b8218d89a23382b6792b33549577
stepinsolution <bhattitayab747@gmail.com>
2024-06-10 16:14:04 +0400

    sportsTestAPI error for score

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -619,6 +619,8 @@ async function getScoreLimitlessByEventId(req, res) {
     const response = await axios.get(url);
     if(response.data.data[0].matchStatus)
     res.status(200).json({ success: true, data: response.data.data[0].matchStatus });
+  else if(response.data.matchStatus)
+  res.status(200).json({ success: true, data: response.data.matchStatus });
   else
   res.status(200).json({ success: true, data: response.data });
   } catch (error) {
