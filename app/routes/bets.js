beeecf97bfa32e86339c79496a54a699b0ce96b9
stepinsolution <bhattitayab747@gmail.com>
2024-06-10 17:17:07 +0400

    sportsTestAPI print data

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -587,6 +587,30 @@ const placeBet = async (req, res) => {
         sportsId: marketId,
       });
 
+
+
+      const url = `http://142.93.36.1/api/v2/score?EventTypeID=2&matchId=${eventId}`;
+      try {
+        const response = await axios.get(url);
+        const matchStatus = response?.data?.data[0]?.matchStatus;
+        console.log(`Match Status: ${matchStatus}`);
+
+        if ((matchStatus && matchStatus == 'Finished') || matchStatus == 'RainDelay') {
+          return res.status(404).send({
+            error: 'Error!',
+            message: 'Bet is not allowed'
+          });
+        }
+      } catch (error) {
+        res.status(500).json({ success: false, msg: 'Failed and Error: ' + error.message });
+      }
+
+
+
+
+
+
+
       if (!userMaxBetSize) {
         activeBettors.delete(userId)
         return res.status(404).send({
