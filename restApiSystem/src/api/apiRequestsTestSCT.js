27753b161b0cd44f5b8281770a0b2751cdc9d304
stepinsolution <bhattitayab747@gmail.com>
2024-06-12 17:37:32 +0400

    consoles only

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -465,7 +465,7 @@ function apiRequests() {
   async function getOddsFromProvider(marketIdsArray) {
     let tempArray = [];
     let tempArrayForIDs = [];
-    console.log("market ids for getting sports odds----------------------------",marketIds);
+    console.log("market ids for getting sports odds----------------------------",marketIdsArray);
     for (let index = 0; index < marketIdsArray.length; index++) {
       const el = marketIdsArray[index];
       tempArray.push({
