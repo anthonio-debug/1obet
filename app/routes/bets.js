ac5120fadf8307684e55992b8f0a4647599a43b1
stepinsolution <bhattitayab747@gmail.com>
2024-06-13 21:03:56 +0400

    console fancyname to remove Adv' word

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -725,7 +725,7 @@ const placeBet = async (req, res) => {
               const ApiResponseOdds =
                 runnerFromAPI?.ex?.availableToBack;
 
-              //console.log("ApiResponseOdds  : : Type: 0::::::::::::::::::::::::::::::",ApiResponseOdds);
+              
               if (ApiResponseOdds && ApiResponseOdds.length > 0) {
                 selectedOddsValue = ApiResponseOdds[0].price;
               }
@@ -737,7 +737,7 @@ const placeBet = async (req, res) => {
               const ApiResponseOdds =
                 runnerFromAPI.ex?.availableToLay;
               
-                //console.log("ApiResponseOdds  : : Type: 1::::::::::::::::::::::::::::::",ApiResponseOdds);
+                
 
               if (ApiResponseOdds && ApiResponseOdds.length > 0) {
                 selectedOddsValue = ApiResponseOdds[0]?.price;
