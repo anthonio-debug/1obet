fb7afd65429225893e94f4465322fa65049a0c1b
Super Dev <denysyudakov@gmail.com>
2024-06-17 18:49:32 +0300

    add two field

--- app/routes/currentPosition.js
+++ app/routes/currentPosition.js
@@ -280,6 +280,8 @@ const getCurrentPosition2 = async (req, res) => {
           _id: "$_id",
           marketId: { $first: { $arrayElemAt: ["$bets.marketId", 0] } },
           matchId:  { $first: { $arrayElemAt: ["$bets.matchId", 0] } },
+          loosingAmount1: { $first: "$bets.loosingAmount" },
+          winningAmount1: { $first: "$bets.winningAmount" },
           loosingAmount: { $first: "$amount" },
           maxWinningAmount: {
             $first: {
