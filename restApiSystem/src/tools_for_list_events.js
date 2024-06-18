a59d21ee4e271697358f1c01cc7be8eb86732b90
bhattitayab <bhattitayab747@gmail.com>
2024-06-19 03:08:32 +0400

    limitless api configurations

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -186,7 +186,7 @@ function ToolForEvent() {
     try {
       const now = moment().utc(); // Get the current time in UTC
       const startTime = moment(now).subtract(1000, 'minutes').valueOf(); // Get the timestamp in milliseconds
-      const endTime = moment(now).add(50, 'minutes').valueOf(); // Add 5 hours and get the timestamp in milliseconds
+      const endTime = moment(now).add(1600, 'minutes').valueOf(); // Add 5 hours and get the timestamp in milliseconds
      console.log("--------------------------------------------------------------------");
       const documents = await MarketIDs.aggregate([
         {
