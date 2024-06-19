772aefbd575cdc016d22e7f545b796a1b4c217c8
Super Dev <denysyudakov@gmail.com>
2024-06-19 19:10:03 +0300

    Update bets.js

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -625,44 +625,6 @@ const placeBet = async (req, res) => {
         userId: userId,
         sportsId: marketId,
       });
-<<<<<<< HEAD
-
-
-      if(userId==20126){
-        //eventDetail = await Events.findById(matchId);
-        
-                //Code added for matchStatus started
-        const url = `http://142.93.36.1/api/v2/score?EventTypeID=2&matchId=${eventDetail.Id}`;
-        try {
-          const response = await axios.get(url);
-          const matchStatus = response?.data?.data[0]?.matchStatus;
-          console.log(`Match Status: ${matchStatus}`);
-  
-          if ((matchStatus && matchStatus == 'Finished') || (matchStatus && matchStatus == 'RainDelay')) {
-            return res.status(404).send({
-              error: 'Error!',
-              message: 'Bet is not allowed'
-            });
-          }
-        } catch (error) {
-          res.status(500).json({ success: false, msg: 'Failed and Error: ' + error.message });
-        }
-        
-      
-    //Code added for matchStatus ended
-        }
-        
-        
-
-
-
-
-
-
-
-
-=======
->>>>>>> f2846699fd429ba402d65319e3a87d022ae18f1c
       if (!userMaxBetSize) {
         activeBettors.delete(userId)
         return res.status(404).send({
