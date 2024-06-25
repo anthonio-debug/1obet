8ab19adc303caa6a2b784dfa3dff0c801146231c
cryptofan35 <kirylkrauchuk@gmail.com>
2024-06-25 11:48:07 -0400

    implemented directURL for all sports except races

--- app/models/liveStream.js
+++ app/models/liveStream.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const liveStreamSchema = new mongoose.Schema({
     liveUrl : {type: String, required: true},
     type: {type: String, required: true},
-    sportsId: {type: Number}
+    sportsId: {type: Number},
+    directUrl: {type: Boolean, default: false}
 });
 
 const LiveStream = mongoose.model('livestream', liveStreamSchema);
--- app/routes/liveTv.js
+++ app/routes/liveTv.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const loginRouter = express.Router();
 const router = express.Router();
 const LiveStream = require('../models/liveStream');
+const Events = require("../models/events");
 
 const {LIVE_BET_TV_URL} = require("../global/constants");
 
@@ -32,14 +33,36 @@ async function liveTv(req, res) {
 }
 
 async function liveStream(req, res) {
+  const eventId = req.params.eventId;
   try {
-    const liveStreams = await LiveStream.find()
+    if(evenId !== 0) {
+      const event = await Events.find({Id : eventId.toString(), liveUrl: {$ne : null}});
 
-    res.status(200).json({
-      success: true,
-      message: "Live Tv Streaming Urls",
-      liveStreamUrls: liveStreams,
-    });
+      if(event.length > 0) {
+        const liveStream = [{sportsId: Number(event[0].sportId), liveUrl: event[0].liveUrl, directUrl: true}];
+        res.status(200).json({
+          success: true,
+          message: "Live Tv Streaming Urls",
+          liveStreamUrls: liveStream,
+        });    
+      } else {
+        const liveStreams = await LiveStream.find()
+    
+        res.status(200).json({
+          success: true,
+          message: "Live Tv Streaming Urls",
+          liveStreamUrls: liveStreams,
+        });
+      }
+    } else {
+      const liveStreams = await LiveStream.find()
+    
+        res.status(200).json({
+          success: true,
+          message: "Live Tv Streaming Urls",
+          liveStreamUrls: liveStreams,
+        });
+    } 
   } catch (error) {
     console.error(error);
     res.status(200).json({
@@ -55,6 +78,6 @@ async function liveStream(req, res) {
 
 // Define the route for the API
 loginRouter.get("/liveTv/:eventId", liveTv);
-loginRouter.get("/liveStream", liveStream);
+loginRouter.get("/liveStream/:eventId", liveStream);
 
 module.exports = { loginRouter, router };
