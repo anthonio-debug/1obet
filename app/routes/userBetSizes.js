c2655b3704a1bf0efa9155429d126a28f3162d63
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-06-27 12:11:34 +0500

    Add the Check for company end

--- app/routes/userBetSizes.js
+++ app/routes/userBetSizes.js
@@ -96,11 +96,11 @@ async function getAllBetSizes(req, res) {
     if (!user) {
       return res.status(404).send({ message: 'USER_NOT_FOUND' });
     }
-    if (user.role === "0") {
-      return res.status(200).send()
-    }
-    
+
     const parent = await User.findOne({ userId: user.createdBy });
+    if (!parent) {
+      return res.status(400).send({ message: "This is company User" });
+    }
 
     // 
     let queryResult;
@@ -332,7 +332,6 @@ async function getAllBetSizes(req, res) {
       message: 'BET_SIZES_FETCHED_SUCCESSFULLY',
       results: queryResult,
     });
-
   } catch (error) {
     // Log the error
     console.error('Error:', error);
