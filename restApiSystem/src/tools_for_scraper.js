966ebbfe006dcd2f8ded8dbe751b8841c0dfdfd3
bhattitayab <bhattitayab747@gmail.com>
2024-07-03 18:20:02 +0400

    added player 1 filtering

--- restApiSystem/src/tools_for_scraper.js
+++ restApiSystem/src/tools_for_scraper.js
@@ -150,6 +150,71 @@ function ToolForScraper() {
             );
             if (eventId) {
               
+              // console.log("YAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO:");
+              // console.log("YAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO:");
+              // console.log("YAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO:");
+              // console.log("YAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO:");
+              // console.log("YAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO:");
+              // console.log("YAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO:");
+              // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA:",apiCricketScore);
+              // console.log("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC:",cricketScore);
+              // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFTER:");
+
+              console.log("Cooooooooooooooooooooment:::::::::::::::",apiCricketScore.comment);
+               console.log("RESSSSSSSSSSSSSSSSSSSSSSSSULT:::::::::::::::",apiCricketScore.result);
+
+    
+      console.log("pLAYER INNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN TRUE:");
+      let FindInMe = apiCricketScore.result;
+      let FindInMeRes = FindInMe.toLowerCase();
+      let findMe1 = FindInMeRes.search("Players IN");
+      let findMe2 = FindInMeRes.search("players in");
+      
+      console.log("FFFFFFFFFFFF@@@@@@@@@@@@@22222222222222222222222::",findMe1);
+      console.log("FFFFFFFFFFFF@@@@@@@@@@@@@22222222222222222222222::",findMe2);
+      console.log("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE:",eventId);
+      if( findMe1 >=0 ||  findMe2 >=0){
+        console.log("FIND ME IS GREATER AND FIND ME 2 ALSO GREATER");
+      await InPlayEvents.findOneAndUpdate(
+        { Id: eventId },
+        { $set: { player_in:1 } }
+      )
+    }
+    
+    if(event.player_in==1){
+
+      let FindInMe = apiCricketScore.comment;
+      let FindInMeRes = FindInMe.toLowerCase();
+      let findMe1 = FindInMeRes.search("won by");
+      let findMe2 = FindInMeRes.search("match finished");
+      let findMe3 = FindInMeRes.search("match tied");
+      
+  
+  
+      if( findMe1 >=0 ||  findMe2 >=0 ||  findMe3 >=0){
+  
+      await InPlayEvents.findOneAndUpdate(
+        { Id: eventId },
+        { $set: { inplay:false } }
+      )
+    }
+
+    }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
