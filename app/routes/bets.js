85f90c1778b8d48e0d984e042a6aef69fabd078b
Super Dev <denysyudakov@gmail.com>
2024-07-04 16:03:59 +0300

    Update bets.js

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2836,6 +2836,9 @@ const placeBet = async (req, res) => {
       }
 
       if (rates?.length > 1 && !multipeResponseForSecurityCheck.includes(rates[rates.length - 1])) {
+        console.log('===========================================')
+        console.log(multipeResponseForSecurityCheck)
+        console.log('===========================================')
         activeBettors.delete(userId);
         return res.status(404).send({
           message: `Bet Miss Matched `
