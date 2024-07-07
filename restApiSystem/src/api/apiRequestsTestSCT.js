57f636ddb339167c721680c1ecbb756891020efb
bhattitayab <bhattitayab747@gmail.com>
2024-07-07 12:45:53 +0400

    console for odds

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -617,7 +617,7 @@ function apiRequests() {
                         runnerCheckerArray.push(marketId);
                       }
                     }
-
+                    console.log("Json to be saved into odds collection:::::::::::::::::::::::::::::::",json1);
                     let el = new Odds(json1);
                     await el.save();
 
