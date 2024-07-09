54f28d5f5cc46a71bb8f0d1d3bad989658c62122
Super Dev <denysyudakov@gmail.com>
2024-07-09 18:25:04 +0300

    [update] userbook

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -675,6 +675,7 @@ async function user_book2(req, res) {
         username: '$userDetails.userName',
         downLineShare: '$userDetails.downLineShare',
         marketName: '$marketDetails.marketName',
+        createdBy: '$userDetails.createdBy',
         runners: {
           $ifNull: [{ $arrayElemAt: ['$marketDetails.runners', 0] }, []]
         },
@@ -700,6 +701,7 @@ async function user_book2(req, res) {
         runners: { $first: '$runners' },
         runnersPosition: { $first: '$runnersPosition' },
         createdAt: { $first: '$createdAt' },
+        createdBy: { $first: '$createdBy' },
         username: { $first: '$username' }
       }
     }
