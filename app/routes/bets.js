712f24fec230d4d9f9186860f79287279e2307a9
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-07-11 20:11:31 +0500

    ----------------

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1770,7 +1770,7 @@ const placeBet = async (req, res) => {
 
     // For Fancy
     else if (subMarketDetail.Id == config.Fancy || subMarketDetail.subMarketName == "Fancy 2") {
-      console.log(subMarketDetail.subMarketName)
+      console.log("+++++++++++++++++++++++++++++++++++++", subMarketDetail.subMarketName)
       const userMaxBetSize = await userBetSizes.findOne({
         userId: userId,
         sportsId: marketId,
