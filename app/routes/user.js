fe22dc55bfebf5d06acfe6a1a910024f8b0a594f
bhattitayab <bhattitayab747@gmail.com>
2024-07-12 04:16:01 +0400

    getcurrent user to selected data

--- app/routes/user.js
+++ app/routes/user.js
@@ -698,7 +698,8 @@ function getCurrentUser(req, res) {
   if (errors.errors.length !== 0) {
     return res.status(400).send({ errors: errors.errors });
   }
-  User.findOne({ userId: req.decoded.userId }, async (err, user) => {
+  const fieldsToSelect = 'userName,availableBalance,balance,exposure,_id,isActive,role,status';
+  User.findOne({ userId: req.decoded.userId },fieldsToSelect, async (err, user) => {
     if (err || !user) return res.status(404).send({ message: 'user not found' });
 
     return res.send({
