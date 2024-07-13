7da2ab0a76212719e41155a1afa3161ea861116c
bhattitayab <bhattitayab747@gmail.com>
2024-07-13 09:33:41 +0400

    markets test

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -260,7 +260,27 @@ async function getMarketsByEventId(req, res) {
 
     const marketsData = response.data.result;
 
-    res.status(200).json({ success: true, data: marketsData });
+
+    if (marketsData.length > 0) {
+      let marketIds = [];
+
+    marketsData.forEach((element) => {
+    let tempRunners = [];
+          if (config.activeProvider == 'old') {
+            marketStatus = element.status;
+          }
+          for (let k = 0; k < element?.runners?.length; k++) {
+            tempRunners.push({
+              SelectionId: element?.runners[k]?.selectionId,
+              runnerName: element?.runners[k]?.runnerName
+            });
+          }
+          console.log("MarketName:",element.marketName);
+        });
+      }
+
+
+    res.status(200).json({ successs: true, data: marketsData });
   } catch (err) {
     res
       .status(500)
