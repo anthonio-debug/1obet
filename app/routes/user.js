aef0523cd971ff1b47f8fdbb62deb28f6b3bbcad
bhattitayab <bhattitayab747@gmail.com>
2024-07-14 08:01:53 +0400

    getcurrent user hiding password

--- app/routes/user.js
+++ app/routes/user.js
@@ -698,7 +698,8 @@ function getCurrentUser(req, res) {
   if (errors.errors.length !== 0) {
     return res.status(400).send({ errors: errors.errors });
   }
-  User.findOne({ userId: req.decoded.userId }, async (err, user) => {
+  const fieldsToSelect = '"balance":1,"exposure":1,"userName":1,"availableBalance":1,"isActive":1,"status":1,"userId":1,"role":1';
+  User.findOne({ userId: req.decoded.userId },'"balance":1,"exposure":1,"userName":1,"availableBalance":1,"isActive":1,"status":1,"userId":1,"role":1', async (err, user) => {
     if (err || !user) return res.status(404).send({ message: 'user not found' });
 
     return res.send({
