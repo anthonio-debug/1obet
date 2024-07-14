cd57b0704e683e5eb42ca90fa10b14cbf907ef5a
bhattitayab <bhattitayab747@gmail.com>
2024-07-14 23:10:45 +0400

    event_info checking for markets

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -116,7 +116,7 @@ function apiRequests() {
             const lOdds = await Odds.findOne({ marketId: marketId.id }).sort({ createdAt: -1 });
             if (lOdds) event_information.marketIds[index].last_odds = lOdds;
           }
-          console.log("------------------------------------------------>>>>>>>>");
+          console.log("------------------------------------------------>>>>>>>>",event_information);
           const marketIds = event_information.marketIds.map((item) => item.id);
           let totalMatched = 0;
           if (marketIds.length) {
