dbd2ebeed603972cdb7dded81f168ff66b8520bf
Super Dev <denysyudakov@gmail.com>
2024-07-15 17:46:08 +0500

    Update CasinoCalls.js

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -1000,6 +1000,11 @@ async function rollbackFun(req, res) {
 
 function casino(req, res) {
   const { action, remote_id } = req.query;
+  if(remote_id==6896479){
+    console.log('===============================================')
+    console.log(JSON.stringify(req.query, null, 2))
+    console.log('===============================================')
+  }
   if (!remote_id || !action) {
     return res.send({ status: '400', msg: 'Invalid Request' });
   }
