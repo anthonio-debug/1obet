a7cdbae90fe8d64e2a582f537298ea25c5f19c57
bhattitayab <bhattitayab747@gmail.com>
2024-07-20 11:22:16 +0400

    remove decial for volume

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -648,6 +648,16 @@ function apiRequests() {
 
 
                  
+                  console.log("------------------------------->"+element.totalMatched);
+
+                  let totalMatched = element.totalMatched;
+                  let totalMatchedStr = totalMatched.toString();
+// Ensure sentence is a string before using replace
+if (typeof totalMatchedStr === 'string') {
+  totalMatchedStr = totalMatchedStr.replace('.', '');
+} else {
+    console.log('sentence is not a string');
+}
 
 
 
@@ -708,7 +718,7 @@ function apiRequests() {
                         runnerCheckerArray.push(marketId);
                       }
                     }
-                    console.log('sportsId:' + json1 + '-->marketId:' + json1.marketId);
+                    console.log('sportsId:' + json1.sportsId + '-->marketId:' + json1.marketId);
                     let el = new Odds(json1);
                     await el.save();
 
