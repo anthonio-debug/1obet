00de620b9730367fd29ab9459970ddacee07ca93
bhattitayab <bhattitayab747@gmail.com>
2024-07-25 16:01:36 +0400

    rahul odds

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -824,7 +824,7 @@ async function cronOdds(req, res) {
     const odds = await getOdds(marketIds, sportID);
     result = [...result, ...odds];
   }
-  res.json({ status: true, data: "here is result...." });
+  res.json({ status: true, data: result });
 }
 
 async function getTheSportsMatchScoreEvents(req, res) {
