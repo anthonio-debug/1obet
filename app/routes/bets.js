43b380bc977ca85a060bacafc82f990505e8664e
bhattitayab <bhattitayab747@gmail.com>
2024-08-01 20:29:28 +0400

    related markets for races

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -3585,8 +3585,10 @@ async function getMatchedBets(req, res) {
     
     if(marketId){
       const market = await MarketIDS.findOne({ marketId: marketId })
+      console.log("-----------------",market);
       const marketOpendate = market.openDate
       const eventId = await Events.findOne({ Id: market.eventId })
+      console.log("-----------------",eventId);
 
     }else{
       const eventId = await Events.findById(matchId);
