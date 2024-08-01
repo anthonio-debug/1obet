fba3dfff4cf941c4b54b0ce1403079ce10327075
bhattitayab <bhattitayab747@gmail.com>
2024-08-01 20:43:04 +0400

    related markets for races

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -3487,8 +3487,8 @@ async function getMatchedBets(req, res) {
     const userIDs = await getAllUserIDs(createdByIDs);
     //const { id, matchId } = req.query;
     const matchId = req.query.id;
-    //const marketId = req.query.marketId;
-    const marketId = '1.231243057';
+    const marketId = req.query.marketId;
+    //const marketId = '1.231243057';
     if (loginUser.role == '5') {
       userIDs.push(loginUser.userId);
     }
