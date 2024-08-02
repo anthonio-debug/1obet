7ff0d8031617c01c23e6abd5db97369ae32be295
Mujahid789 <mujahidinfo789@gmail.com>
2024-08-02 19:08:42 +0500

    -----

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -3639,10 +3639,10 @@ async function getMatchedBets(req, res) {
 
           ]);
           // console.log("....................................." , marketData);
-          const marketsData = marketData.map((data) => data)
-          console.log("MMMMMMMMMMMMM", relatedEvents);
+          const events = marketData.map((data) => data)
+          console.log("MMMMMMMMMMMMM", events);
           // relatedEvents = marketData;
-          res.status(200).json({ success: true, message: 'Related Markets:', marketsData });
+          res.status(200).json({ success: true, message: 'Related Markets:', events });
 
         } catch (error) {
           console.error('Error updating odds:', error);
