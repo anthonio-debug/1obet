980baf281b52752e93d30f24d8e913ce83ca9a79
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-03 19:02:20 +0500

    testing

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2464,8 +2464,7 @@ const placeBet = async (req, res) => {
     /* ============================================================ =============== */
 
     const delayExcludedMarkets = [...config.FigureEvenOddSmallBig, ...config.asianSubMarket, config.Fancy, config.overByOver, config.BookMaker, config.Toss];
-    console.log("delayExcludedMarkets==============", delayExcludedMarkets);
-
+    
     if (delayExcludedMarkets.includes(subMarketDetail.Id)) {
       delay = 1;
       if (subMarketDetail.Id == config.Fancy || subMarketDetail.Id == config.BookMaker) {
@@ -2478,12 +2477,12 @@ const placeBet = async (req, res) => {
     setTimeout(async () => {
       console.log("subMarketDetail.Id========================", subMarketDetail.Id);
       console.log("multipeResponse========================", multipeResponse);
-      if (multipeResponse.length == 0 && !delayExcludedMarkets.includes(subMarketDetail.Id)) {
+      if (multipeResponse.length >= 0 && !delayExcludedMarkets.includes(subMarketDetail.Id)) {
         activeBettors.delete(userId);
         return res.status(404).send({
           message: `Bet Miss Matched-40 `
         });
-      } else if (multipeResponse.length > 0 && !delayExcludedMarkets.includes(subMarketDetail.Id)) {
+      } else if (multipeResponse.length >= 0 && !delayExcludedMarkets.includes(subMarketDetail.Id)) {
         betRate = multipeResponse[multipeResponse.length - 1];
       }
 
