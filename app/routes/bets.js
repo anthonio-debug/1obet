5ae2b8d97b056ce3d839ed0909c56946ee02acd8
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-04 16:03:57 +0500

    Add the runnerForSaveInbets data in fancy check

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1900,6 +1900,14 @@ const placeBet = async (req, res) => {
       const apiFancyOdds = buildFancyOdd(apiFancyOddsRes);
       const DBOddDetails = await FancyOdds.findById(oddsId);
       const dbFancyOdds = DBOddDetails?.data?.data?.t3;
+
+      let runners = dbFancyOdds;
+      _3rdPartyMarketId = selectedMarketId;
+      runnerForSaveInbets = runners.map((runner) => ({
+        runner: runner.sid,
+        amount: 0
+      }));
+
       /* bookmaker check start */
       const dbBookmakerMarketId = DBOddDetails?.data?.data?.t2[0]?.bm1[0]?.ssid;
       if (!dbBookmakerMarketId) {
