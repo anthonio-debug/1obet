4de54a5908b1eb7787158723f375464f7bf61401
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-11 08:30:27 +0500

    remove map

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -701,7 +701,7 @@ async function deleteOdds(req, res) {
     var arra = [];
     for (let index = 0; index < response.length; index++) {
       let element = response[index];
-      console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++" + element.map(data => console.log(data)));
+      console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++" + element);
       //arra[index] = element.name;
     }
 
