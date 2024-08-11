c91a450484a3f93a293aefbcb7cf45bc1425437b
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-11 17:12:20 +0500

    remove condititon for register user

--- app/routes/user.js
+++ app/routes/user.js
@@ -73,7 +73,7 @@ async function registerUser(req, res) {
       }
       // Check if the downline share is greater than the parent's downline share
       const   parentUser = await User.findOne({ userId: req.decoded.userId });
-      if ((parentUser.role != 0 && parentUser.downLineShare <= req.body.downLineShare) || req.body.downLineShare >= 100 || (req.body.role != 5 && req.body.downLineShare == 0)) {
+      if ((parentUser.role != 0 && parentUser.downLineShare <= req.body.downLineShare) || req.body.downLineShare >= 100 ) {
         return res.status(404).send({
           message: `Max allowed downline share is 1 - ${parentUser.downLineShare - 1}`
         });
