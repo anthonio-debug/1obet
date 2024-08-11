adb38ee8fa9fa37a806fdc3a6cf56a09fe50b1c7
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-11 17:57:30 +0500

    check the register function

--- app/routes/user.js
+++ app/routes/user.js
@@ -72,7 +72,9 @@ async function registerUser(req, res) {
         req.body.downLineShare = 0;
       }
       // Check if the downline share is greater than the parent's downline share
-      const   parentUser = await User.findOne({ userId: req.decoded.userId });
+      const parentUser = await User.findOne({ userId: req.decoded.userId });
+      console.log(`parentuser===============${parentUser.downLineShare}`);
+      console.log(`req.body.downLineShare===============${req.body.downLineShare}`);
       if ((parentUser.role != 0 && parentUser.downLineShare <= req.body.downLineShare) || req.body.downLineShare >= 100 || (req.body.role != 5 && req.body.downLineShare == 0)) {
         return res.status(404).send({
           message: `Max allowed downline share is 1 - ${parentUser.downLineShare - 1}`
@@ -592,8 +594,8 @@ function updateUserData(user, updateData, res, isDigitUpdate = false) {
   });
 }
 
-async function blockCashWithdraw(req, res) { 
-    const errors = validationResult(req);
+async function blockCashWithdraw(req, res) {
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
@@ -732,7 +734,7 @@ function getCurrentUser(req, res) {
   }
   const fieldsToSelect = '"balance":1,"exposure":1,"userName":1,"availableBalance":1,"isActive":1,"status":1,"userId":1,"role":1';
   User.findOne({ userId: req.decoded.userId }, async (err, user) => {
-    
+
     if (err || !user) return res.status(404).send({ message: 'user not found' });
 
     return res.send({
