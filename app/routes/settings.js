2d9695d39c6f3989fcbc35a755669c15d373a35d
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-12 14:34:40 +0500

    add the exposure in the exposure in bets record

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -600,6 +600,7 @@ async function betsRecords(req, res) {
         $group: {
           _id: "$userId",
           userName: { $first: "$userName" },
+          exposure: { $first:"exposure"},
           availableBalance: { $first: "$availableBalance" },
           balance: { $first: "$balance" },
           clientPL: { $first: "$clientPL" },
