76bb5832b5595069f678782670794adacb36e8df
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-12 15:29:37 +0500

    add log for the count markets

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -471,6 +471,11 @@ function apiRequests() {
           if (!marketID) {
             const countOfMarket = await MarketIDS.countDocuments({ eventId: eventId, status: 'OPEN' });
 
+
+            console.log("countOfMarket::::::::::::::::::::::::::::", countOfMarket);
+            console.log("sportID === '1' ? config.soccerEventsAllowedCount : sportID === '2'::::::::::::::::::::::::::::", sportID === '1' ? config.soccerEventsAllowedCount : sportID === '2');
+            console.log("sportID === '2' ? config.tennistEventsAllowedCount : sportID === '4'::::::::::::::::::::::::::::", sportID === '2' ? config.tennistEventsAllowedCount : sportID === '4');
+            console.log("sportID === '4' ? config.cricketEventsAllowedCount : config.allSportsEventsAllowedCount::::::::::::::::::::::::::::", sportID === '4' ? config.cricketEventsAllowedCount : config.allSportsEventsAllowedCount);
             if (countOfMarket > (sportID === '1' ? config.soccerEventsAllowedCount : sportID === '2' ? config.tennistEventsAllowedCount : sportID === '4' ? config.cricketEventsAllowedCount : config.allSportsEventsAllowedCount)) {
               console.log("This condition is going to true")
               return;
