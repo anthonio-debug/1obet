57192d0a300a4c7477e943d8528313f33b737545
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-12 13:22:18 +0500

    commented the log

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -237,7 +237,7 @@ function ToolForEvent() {
       await MarketIDs.updateMany({ marketId: { $in: marketIds } }, { $set: { lastCheck: Date.now() } });
       
       if (marketIds.length > 0) {
-        console.log("244444444444444uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu--",marketIds.length);
+        // console.log("244444444444444uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu--",marketIds.length);
         //this code runs
         apiRequests.getOddsFromProvider(documents, intervalId);
       }
