eefc370b53841b3818ac90cd48cc1f5124bf2665
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-13 18:37:04 +0500

    Apply the check for market status

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2117,7 +2117,7 @@ const placeBet = async (req, res) => {
             const oddsData = await apiCallForOdds(DBOddDetails.marketId);
             const marketStatus = oddsData[0]?.status;
             console.log("Check the code Im here", oddsData)
-            if (marketStatus != 'OPEN') {
+            if (marketStatus != 'OPEN'|| !marketStatus) {
               activeBettors.delete(userId);
               console.log("Check the code Im here")
               return res.status(400).send({
