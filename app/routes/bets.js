a11396c652ac3c5aa9c0c0631f9bac4245a424fa
bhattitayab <bhattitayab747@gmail.com>
2024-08-14 00:44:34 +0400

    bets list not showing

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -3809,7 +3809,7 @@ async function getMatchedBets(req, res) {
                 sportsId: { $toString: "$sportID" },
                 Id: "$eventId",
                 marketIds: "$marketId",
-                name: { $marketName },
+                name:  "$marketName" ,
                 countryCode: { $first: '$event.countryCode' },
                 openDate: 1,
                 status: 1,
