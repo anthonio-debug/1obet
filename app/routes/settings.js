3f7a55907748ebcbfd997788d2315ac70074eedc
Mujahid789 <mujahidinfo789@gmail.com>
2024-08-13 15:36:26 +0500

    check status

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1067,6 +1067,7 @@ async function bettorDashboardGames(req, res) {
           sportsId: { $first: '$sportID' },
           openDate: { $first: '$openDate' },
           openDate2: { $first: '$event.openDate' },
+          status2: { $first: '$event.status' },
           status: { $first: '$status' },
           inPlay: { $first: '$inPlay' },
           countryCode: { $first: '$event.countryCode' },
