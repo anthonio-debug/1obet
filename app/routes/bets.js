8a35cabad774735596ea07b7508a77886a131801
bhattitayab <bhattitayab747@gmail.com>
2024-08-15 20:58:33 +0400

    races related events fix

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -3673,7 +3673,7 @@ async function getMatchedBets(req, res) {
     // Fetch all user IDs using optimized function
     const userIDs = await getAllUserIDs(createdByIDs);
     const matchId = req.query.id;
-    const marketId = req.query.marketId;
+    const marketId = req.query.matchId;
 
     //for sports id has matchId while for races matchId has machId like following: id=1.231812456&matchId=66bd1e37dc2d435bee4a6278&eId=33494827
 
@@ -3787,12 +3787,12 @@ async function getMatchedBets(req, res) {
 
     }
     
-    if(userId==20126){
-      console.log("=======================>>>>>>>>>",eventId);
-      console.log("=======================>>>....>>>>>>",marketOpendate);
-      console.log("=======================>>>....,",matchId);
+    //if(userId==20126){
+      // console.log("=======================>>>>>>>>>",eventId);
+      // console.log("=======================>>>....>>>>>>",marketOpendate);
+      // console.log("=======================>>>....,",matchId);
 
-    }
+    //}
     
 
 
