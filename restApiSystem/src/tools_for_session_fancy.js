3835ee6a3fd513105ffced0b9296fe988f2e16f9
bhattitayab <bhattitayab747@gmail.com>
2024-08-17 16:44:31 +0400

    fancy places bet even if status is SUSPENDED

--- restApiSystem/src/tools_for_session_fancy.js
+++ restApiSystem/src/tools_for_session_fancy.js
@@ -159,6 +159,7 @@ if(bookmakerOdds!=0){
            console.log("b lengthhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh..:",bookmakerOdds.length);
 
             const fancyData = buildFancyStructure(bookmakerMarketList, bookmakerOdds, fancyOdds, eventId)
+            console.log('fancy oddsssssssssssssss returned',fancyData);
             if (!FancyOddsMap.has(eventId) || !isObjectEqual(FancyOddsMap.get(eventId), fancyData)) {
               FancyOddsMap.set(eventId, fancyData)
               let newFancyOdds = new FancyOdds({
