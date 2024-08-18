763d3c199f1f21e4c7f6d35bce633c8ce40a580d
bhattitayab <bhattitayab747@gmail.com>
2024-08-18 14:01:45 +0400

    delete queries for odds and races commented

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -726,8 +726,8 @@ async function deleteOdds(req, res) {
   const eventId = req.params.eventId;
 
   try {
-    await Odds.deleteMany({  });
-    await RaceOdds.deleteMany({  });
+    //await Odds.deleteMany({  });
+    //await RaceOdds.deleteMany({  });
     await InPlayEvents.updateMany({ Id: eventId }, { $set: { hasFancy: true } });
 
     const response = await MarketIDS.aggregate([{ $project: { name: "$marketName" } }]);
