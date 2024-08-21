6504cce77e9d86eefa40b331eb9cf44279b428f9
bhattitayab <bhattitayab747@gmail.com>
2024-08-21 16:13:46 +0400

    limitless markets work

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -855,11 +855,11 @@ async function getOdds(marketIds, sportsId) {
   return new Promise((resolve, reject) => {
     const odds = [];
     const oddUrl = `http://84.8.153.51/api/v2/getMarketsOdds?EventTypeID=${sportsId}&marketId=${marketIds}`;
-
+    console.log("===================================================================================4",marketIds);
     axios
       .get(oddUrl)
       .then(async (oddRes) => {
-        console.log("===================================================================================4");
+        
         if (!oddRes || !oddRes?.data) return;
         if (oddRes.data.length) {
           console.log("===================================================================================1");
