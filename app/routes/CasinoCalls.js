844914ff039eb279b4d494acf0d3534e01732c34
bhattitayab <bhattitayab747@gmail.com>
2024-08-23 18:45:40 +0400

    casino betting locks applying

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -47,7 +47,7 @@ const checkMarketBlocked = async (user) => {
   const marketId = config.casinoMarketId;
   //if any of the parents hierarchy
   if (marketIds.includes(marketId) || user.casinoAllowed == false || user.bettingAllowed == false || anyParentCasinoBlocked || anyParentBettingBlocked ) {
-    return 1;
+    return 0;
   } else {
     return 0;
   }
