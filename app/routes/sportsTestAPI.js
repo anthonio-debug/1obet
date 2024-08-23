040b1d57ffb5423140489bc9f3502834e8b81af6
Mujahid789 <mujahidinfo789@gmail.com>
2024-08-23 16:06:12 +0500

    -

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -834,7 +834,7 @@ async function saveOdds(oddData, sportsId) {
       },
     });
   }
-  const activeRunners = runners.filter((e) => e.Status === "ACTIVE");
+  // const activeRunners = runners.filter((e) => e.Status === "ACTIVE");
   const odd = {
     eventId: oddData.eventid,
     marketId: oddData.marketId,
