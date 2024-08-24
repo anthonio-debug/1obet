631497aa3dd33398084a1e4fdcb24374dd01f4b3
bhattitayab <bhattitayab747@gmail.com>
2024-08-24 05:53:57 +0400

    server time

--- app/routes/sportsHighlights.js
+++ app/routes/sportsHighlights.js
@@ -113,8 +113,8 @@ async function getAllSportsHighlight(req, res) {
             }
           }
         ]);
-        sportsHighlights[i].totalMatched = marketData[0] ? marketData[0].totalMatched : 0
-        sportsHighlights[i].serverTime = '';
+        sportsHighlights[i].totalMatched = marketData[0] ? marketData[0].totalMatched : 0;
+        sportsHighlights[i].serverTime = serverTime;
         console.log("------------------------------->",8,'===============>>>',sportsHighlights)
         
       }
