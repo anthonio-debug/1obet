413abec342d43b2a27e84bddd7b055ad0e3c045a
bhattitayab <bhattitayab747@gmail.com>
2024-08-24 14:51:23 +0400

    bets.js delayAddition set to zero

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -2620,7 +2620,7 @@ const placeBet = async (req, res) => {
     if (delayExcludedMarkets.includes(subMarketDetail.Id)) {
       delay = 1;
       if (subMarketDetail.Id == config.Fancy || subMarketDetail.Id == config.BookMaker) {
-        delay = 4000;
+        //delay = 4000;
       }
     } else {
       //delay += delayAddition * 1000;
