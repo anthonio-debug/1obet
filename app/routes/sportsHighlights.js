97a87f873d43ae1085e8fcd43344380aa530c6fa
bhattitayab <bhattitayab747@gmail.com>
2024-08-24 05:47:45 +0400

    server time

--- app/routes/sportsHighlights.js
+++ app/routes/sportsHighlights.js
@@ -115,10 +115,11 @@ async function getAllSportsHighlight(req, res) {
         ]);
         sportsHighlights[i].totalMatched = marketData[0] ? marketData[0].totalMatched : 0
         sportsHighlights[i].serverTime = serverTime;
+        console.log("------------------------------->",7,'===============>>>',sportsHighlights)
         
       }
     }
-    console.log("------------------------------->",7,'===============>>>',sportsHighlights)
+    
     const ids = await inPlayEvents.distinct("Id", {
       sportsId: sportId,
       openDate: {
