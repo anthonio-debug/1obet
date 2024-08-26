1a517808d7776eea66f9cc1b71e2bb331bfaa7fb
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-26 19:36:11 +0500

    --------

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1901,7 +1901,7 @@ const placeBet = async (req, res) => {
           await new Promise(resolve => setTimeout(resolve, 500));
           console.log("set time ", i)
           const response = await fetchSession(eventDetail.Id);
-
+          
           if (!Array.isArray(response)) {
             console.error("Expected an array but got:", response);
             return;
@@ -1911,14 +1911,14 @@ const placeBet = async (req, res) => {
           // apiFancyOddsResponse.push(apiFancyOddsRes);
 
           // console.log("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF:", apiFancyOddsRes);
-
+          
           const gameStatus = apiFancyOddsRes[0]?.GameStatus;
           // console.log(`apiFancyOddsRes[0]?.GameStatus==================${gameStatus}`);
           // console.log(`GameStatus==================${gameStatus}`);
-
+          
           if (gameStatus === 'SUSPENDED' || gameStatus === 'Ball Running') {
             activeBettors.delete(userId);
-
+            
             if (!hasError) { // Send response only once
               hasError = true;
               res.status(404).send({
@@ -1933,6 +1933,7 @@ const placeBet = async (req, res) => {
       }
 
       // console.log("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF: outside ", apiFancyOddsRes);
+      console.log("set time ===========================================")
 
       const apiFancyOdds = buildFancyOdd(apiFancyOddsRes);
       const DBOddDetails = await FancyOdds.findById(oddsId);
