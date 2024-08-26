8e4be6acf35a431211c8c60603b2b5f69e5b90e4
Mujahid789 <mujahidinfo789@gmail.com>
2024-08-26 23:48:46 +0500

    add increase time

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -93,7 +93,8 @@ function ToolForEvent() {
         } else {
           const now = new Date();
           const from = new Date(now.getTime() - 30 * 60 * 1000);
-          const someHoursLater = new Date(now.getTime() + 10 * 60 * 60 * 1000);
+          // const someHoursLater = new Date(now.getTime() + 10 * 60 * 60 * 1000);
+          const someHoursLater = new Date(now.getTime() + 10 * 60 * 60 * 100000);
           const to = someHoursLater.getTime();
           documents = await inPlayEvents
             .findOne({
