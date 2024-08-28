8cb066b541d24c7b2ca97c6cdbcca0b066407f64
Mujahid789 <mujahidinfo789@gmail.com>
2024-08-28 13:54:53 +0500

    -----

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -95,7 +95,7 @@ async function updateOddsFormLimitless() {
   
               for (let element of marketsData) {
 
-                const time30Minutes = 240 * 60 * 1000; 
+                const time30Minutes = 30 * 60 * 1000; 
                 const currentTime = Date.now();
                 const marketStartTime = element.openDate
                 console.log("||||||||||||||||\\\\\\ marketStartTime",marketStartTime);
@@ -106,7 +106,7 @@ async function updateOddsFormLimitless() {
                 
   
                   if (remainingTime < time30Minutes) {
-                      await MarketIDs.findOneAndUpdate({ marketId: element.marketId }, { $set: { ReadyForOdds: true } });
+                      await MarketIDs.findOneAndUpdate({ eventId: element.eventId }, { $set: { ReadyForOdds: true } });
                       console.log("======================-------------- ReadyForOdds=true");
                   }
   
