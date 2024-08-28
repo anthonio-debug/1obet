537cc6c04cbe6b9fef4599fe6fa859de5be08c28
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-28 19:59:18 +0500

    add the default url empty

--- app/models/events.js
+++ app/models/events.js
@@ -52,7 +52,7 @@ const inPlayEventsSchema = new mongoose.Schema({
   isResultSaved: { type: Boolean, default: false },
   seriesKey: { type: String },
   CompanySetStatus: { type: String, default: "PENDING" },
-  liveUrl: { type: String },
+  liveUrl: { type: String, default: "" },
   theSportsId: { type: String },
   hasBookmaker: { type: Boolean, default: false },
   hasFancyMatch: { type: Boolean, default: false }
