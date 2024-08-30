8644d43938615843643365b158fd283d5f9a12f5
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-08-30 19:19:41 +0500

    Check the extra markets for soccer

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -493,9 +493,9 @@ function apiRequests() {
               element.marketName === 'Over/Under 0.5 Goals' ||
               element.marketName === 'Over/Under 1.5 Goals' ||
               element.marketName === 'Over/Under 2.5 Goals'
-              // || element.marketName === "Over/Under 3.5 Goals"
-              // || element.marketName === "Over/Under 4.5 Goals"
-              // || element.marketName === "Over/Under 5.5 Goals"
+              || element.marketName === "Over/Under 3.5 Goals"
+              || element.marketName === "Over/Under 4.5 Goals"
+              || element.marketName === "Over/Under 5.5 Goals"
             )
               marketIds.push({
                 id: element.marketId,
