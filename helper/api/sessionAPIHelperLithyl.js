c1c03781c48b6c0167f2bb88c4b50cba0e038506
bhattitayab <bhattitayab747@gmail.com>
2024-09-01 16:26:40 +0400

    new fancies api

--- helper/api/sessionAPIHelperLithyl.js
+++ helper/api/sessionAPIHelperLithyl.js
@@ -6,9 +6,10 @@ async function fetchSession(eventId) {
   // const eventId = '33002177'
   // http://142.93.36.1/api/v2/getSessions?EventTypeID=4&matchId=33061168
   const url = `${SESSION_API_URI}/getOFBData?id=${eventId}`
-   console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::",url);
+   
   try {
     const response = await axios.get(url)
+    console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::...",response);
     let res = response.data;
     // console.log('session list: ', JSON.stringify(res))
     if (isIterable(res)) {
