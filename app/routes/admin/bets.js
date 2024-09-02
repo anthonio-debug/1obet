427cf124402a41fd794b3623e419a50675babd07
Arham Khan <81346957+Arham47@users.noreply.github.com>
2024-09-02 18:55:47 +0500

    changes

--- app/routes/admin/bets.js
+++ app/routes/admin/bets.js
@@ -112,12 +112,7 @@ const GetAllBets = async (req, res) => {
     )
 
     let result = await Bets.aggregate(pipeline).exec()
-    let cricketResult = await Crickets.findOne({ eventId: eventId }).sort({ createdAt: -1 }).exec();
-    let cricketArr=cricketResult.overs.forEach(element => {
-      element.activeTeam=cricketResult.activeTeam
-      element.team1ShortName=cricketResult.team1ShortName
-      element.team2ShortName=cricketResult.team2ShortName
-    });
+
 
     // const results = result.slice((Number(page) - 1) * limit, page * limit);
 
@@ -130,7 +125,7 @@ const GetAllBets = async (req, res) => {
       status: true,
       message: "Bets List !",
       results: result,
-      cricket: cricketArr,
+      cricket: cricketResult,
       total: result.length,
       limit: limit,
       page: page,
