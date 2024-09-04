0ddefc909223abadaf1a73f8ced327e7dda8a59d
Mujahid789 <mujahidinfo789@gmail.com>
2024-09-04 13:51:54 +0500

    add readyforOdds for toss

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -104,16 +104,18 @@ async function updateOddsFormLimitless() {
 
                 // console.log("remainingTime======", remainingTime/60000);
                 
-  
                   if (remainingTime < time30Minutes) {
-                     const result= await MarketIDs.updateMany({ eventId: element.eventId }, { $set: { ReadyForOdds: true } });
+                    const result= await MarketIDs.updateMany({ eventId: element.eventId , marketName:{$ne:"Toss"}}, { $set: { ReadyForOdds: true } });
+
+                    await MarketIDs.updateMany({ eventId: element.eventId , marketName:"Toss", ReadyForOdds:false}, { $set: { ReadyForOdds: false } });
                       // console.log("======================-------------- Result", result);
                       // console.log("======================-------------- ReadyForOdds=true");
                   }
   
                   if (remainingTime > time30Minutes) {
                       // console.log("cron jobs code running for updating odds ======----- ");
-  
+                      const result= await MarketIDs.updateMany({ eventId: element.eventId , marketName:"Toss", ReadyForOdds:false}, { $set: { ReadyForOdds: true } });
+
                       const oddUrl = `http://84.8.153.51/api/v2/getMarketsOdds?EventTypeID=${element.sportID}&marketId=${element.marketId}`;
   
                       const oddsResponse = await axios.get(oddUrl);
