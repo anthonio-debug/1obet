bf85a93e3a470c6038b688e4b3613f63ecd802b2
Mujahid789 <mujahidinfo789@gmail.com>
2024-09-05 19:25:46 +0500

    save markets from getMarketList

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -1129,14 +1129,14 @@ async function cronOdds2(req, res) {
   // console.log("===================================================================================3");
   const { eventId, sportID } = req.params;
 
-  console.log ("+_+_+_++_+_++_+ SportsId +_+", sportID)
+  // console.log ("+_+_+_++_+_++_+ SportsId +_+", sportID)
 
   const url = `http://84.8.153.51/api/v2/getMarkets?EventTypeID=${sportID}&EventID=${eventId}`;
 
   try {
     const response = await axios.get(url);
 
-    console.log("=-=--==---=-=--=-===--= market api response", response);
+    // console.log("=-=--==---=-=--=-===--= market api response", response);
     
     /////////////////////////
     // const response = {
