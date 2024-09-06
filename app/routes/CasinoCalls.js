5ce6f0b72c7ec3ab2c3170394031026b02e969b8
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-06 16:22:26 +0500

    dd

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -821,7 +821,7 @@ async function creditFun(req, res) {
   try {
     const payload = req.query;
     const transactionId = payload.transaction_id
-    const currentUserTest = await User.findOne({ remoteId: parseInt(payload.remote_id) });
+
       if (payload.gameplay_final == 1 || !payload.remote_id) {
         await User.updateOne({ remoteId: currentUser.remote_id }, { $set: { exposure: 0 } })
         return
