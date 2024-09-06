e2c35278a2c2d18296455761e0630922a77208f5
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-06 16:01:44 +0500

    ss

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -677,14 +677,18 @@ async function processQueue() {
   const session = dbClient.startSession();
   const maxRetries = 3; // Maximum retry attempts
   const retryDelay = 100; // Delay in milliseconds before retrying
-
+  const payload = req.query;
+ 
   const attemptTransaction = async (retryCount) => {
     try {
       await session.startTransaction();
-      const payload = req.query;
+      
       const transactionId = payload.transaction_id;
   
       const currentUser = await User.findOne({ remoteId: parseInt(payload.remote_id) });
+      if (payload.gameplay_final == 1 || !payload.remote_id) {
+        await User.updateOne({remoteId:currentUser.remote_id},{$set:{exposure:0}})
+      }
       if (!currentUser) {
         if (session.inTransaction()) {
           await session.abortTransaction();
