db139b15b54288cee19a0275eaabf220c27f5103
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-06 07:26:56 +0500

    ccc

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -683,6 +683,9 @@ async function processQueue() {
       await session.startTransaction();
       const payload = req.query;
       const transactionId = payload.transaction_id;
+      if (!payload.round_id) {
+        return 
+      }
   
       // Fetch the user based on remoteId
       const currentUser = await User.findOne({ remoteId: parseInt(payload.remote_id) });
