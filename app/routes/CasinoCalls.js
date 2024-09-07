b33be455a3c88889465b3d745126fb2673474623
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-07 22:45:57 +0500

    return statement removal

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -95,10 +95,7 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
       const user_prev_balance = user.balance;
       const user_prev_availableBalance = user.availableBalance;
       const user_prev_exposure = user.exposure;
-      if (payload.amount == 0) {
-        await User.updateOne({ remoteId: user.remoteId }, { $set: { exposure: 0 } })
-       
-      }
+      
       const gamesList = await SelectedCasino.findOne(
         { "games.id": payload.game_id },
         { "games.$": 1 }
@@ -349,6 +346,10 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
         //end of code to give lost money to all share holders
         const casinoDebits = new CasinoDebits(payload);
         await casinoDebits.save();
+        if (payload.amount == 0) {
+          await User.updateOne({ remoteId: user.remoteId }, { $set: { exposure: 0 } })
+         
+        }
       } else if (difference > 0) {
         // Win some Amount
 
