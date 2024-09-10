96ba1a2adddc56e68892199dd138cc3f6abd70ea
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-10 23:59:46 +0500

    commented delay

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -62,11 +62,11 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
     const betTime = new Date().getTime(); 
       
     // Prevent multiple transactions within 500 milliseconds
-    const depositLastBetTime = await Cash.find({ userId: user.userId,  description: "Casino (Casino Hold'em)" }).sort({ _id: -1 });
-    if (depositLastBetTime.length > 0 && (betTime - depositLastBetTime[depositLastBetTime.length-1].betDateTime) < 1000) {
-      console.log('Transaction occurred too quickly, skipping...');
-      return; // Skip transaction
-    }
+    // const depositLastBetTime = await Cash.find({ userId: user.userId,  description: "Casino (Casino Hold'em)" }).sort({ _id: -1 });
+    // if (depositLastBetTime.length > 0 && (betTime - depositLastBetTime[depositLastBetTime.length-1].betDateTime) < 1000) {
+    //   console.log('Transaction occurred too quickly, skipping...');
+    //   return; // Skip transaction
+    // }
     if (action === 0) {
       let amount = Number(payload.amount) * casinoMultiples;
       let UpdatedExposure = Number((user.exposure - amount).toFixed(3));
