b77de629d537872255be1a3ff71ac5e299252326
Mujahid789 <mujahidinfo789@gmail.com>
2024-09-10 10:53:02 +0500

    getOddsLimitless add lithyl api

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -649,14 +649,15 @@ async function getBookmakersLimitlessByEventId(req, res) {
 async function getOddsLimitlessByMarketId(req, res) {
   console.log("getOddsLimitlessByMarketId =-=-=-=-==- function called");
   const marketId = req.params.marketId;
-  let url = `http://84.8.153.51/api/v2/getMarketsOdds?EventTypeID=4&marketId=${marketId}`;
+  // let url = `http://84.8.153.51/api/v2/getMarketsOdds?EventTypeID=4&marketId=${marketId}`;
+  let url = `http://sportzing.in:5505/api/getOdds?market_id=${marketId}`;
   
   try {
     let response = await axios.get(url);
     console.log("getOddsLimitlessByMarketId =-=-=-=-==-response =",response.data);
     
     if (!response || !response.data) {
-      url = `http://sportzing.in:5505/api/getOdds?market_id =${marketId}`;
+      url = `http://sportzing.in:5505/api/getOdds?market_id=${marketId}`;
       response = await axios.get(url);
     }
     
