3bbc835a6e469c24f9cd6a01413b25d16900bb02
bhattitayab <bhattitayab747@gmail.com>
2024-09-11 03:52:31 +0400

    save bookmaker market by concating bm at the end

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -752,7 +752,7 @@ async function deleteOdds(req, res) {
   const eventId = req.params.eventId;
 
   try {
-    await Bets.deleteMany({marketId:'1.232738763'});
+    await Bets.deleteMany({marketId:'1.232738763bm'});
     await Odds.deleteMany({});
     await RaceOdds.deleteMany({});
     await fancyOdds.deleteMany({});
