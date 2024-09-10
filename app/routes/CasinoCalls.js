11b3b2535e8448fd64102fbd5d97aa843117c0f4
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-10 17:57:34 +0500

    changes

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -178,7 +178,7 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
               createdAt: formattedDate,  
               amount: -bettor_lost_amount,  
               balance,
-              availableBalance: lastMaxWithdraw ? lastMaxWithdraw.availableBalance - bettor_lost_amount : -bettor_lost_amount,
+              availableBalance: balance,
               maxWithdraw: lastMaxWithdraw ? lastMaxWithdraw.maxWithdraw - bettor_lost_amount : 0,
               cash: lastMaxWithdraw?.cash || 0,
               credit: lastMaxWithdraw?.credit || 0,
