29c09a2d1a93ea56d3ec52df63352e65dd93b8d0
Mujahid789 <mujahidinfo789@gmail.com>
2024-09-10 10:10:35 +0500

    add log

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -652,6 +652,7 @@ async function getOddsLimitlessByMarketId(req, res) {
   
   try {
     let response = await axios.get(url);
+    console.log("getOddsLimitlessByMarketId =-=-=-=-==-response =",response);
     
     if (!response || !response.data) {
       url = `http://sportzing.in:5505/api/getOdds?market_id =${marketId}`;
