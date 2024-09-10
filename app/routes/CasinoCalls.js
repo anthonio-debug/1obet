0fb8683a99cdd4635fa91ed3bd7d6db296115bd7
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-10 22:10:19 +0500

    push

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -59,7 +59,14 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
     const month = (now.getMonth() + 1).toString().padStart(2, '0');
     const day = now.getDate().toString().padStart(2, '0');
     const formattedDate = `${year}-${month}-${day}`;
-
+    const betTime = new Date().getTime(); 
+      
+    // Prevent multiple transactions within 500 milliseconds
+    const depositLastBetTime = await Cash.find({ userId: user.userId,  description: { $regex: "Casino (Casino Hold'em)", $options: "i" } }).sort({ _id: -1 });
+    if (depositLastBetTime.length > 0 && (betTime - depositLastBetTime[0].betDateTime) < 1000) {
+      console.log('Transaction occurred too quickly, skipping...');
+      return; // Skip transaction
+    }
     if (action === 0) {
       let amount = Number(payload.amount) * casinoMultiples;
       let UpdatedExposure = Number((user.exposure - amount).toFixed(3));
@@ -108,14 +115,7 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
       const difference = credit - debit;
       const allTrans = [];
 
-      const betTime = new Date().getTime(); 
       
-      // Prevent multiple transactions within 500 milliseconds
-      const depositLastBetTime = await Cash.find({ userId: user.userId }).sort({ _id: -1 });
-      if (depositLastBetTime.length > 0 && (betTime - depositLastBetTime[0].betDateTime) < 1000) {
-        console.log('Transaction occurred too quickly, skipping...');
-        return; // Skip transaction
-      }
 
       if (difference < 0) {
         let GameName = game ? game.name : 'N/A';
