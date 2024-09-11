d18cba48b40012bec8dd16cb0c0b262049563270
bhattitayab <bhattitayab747@gmail.com>
2024-09-11 04:54:36 +0400

    why events not show on dashbaord

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -265,7 +265,7 @@ function ToolForEvent() {
             
             ReadyForOdds:true,
             status: { $in: ['INACTIVE', 'OPEN', 'SUSPENDED'] },
-            marketName: { $ne: ['Bookmaker'] },
+            marketName: { $ne: 'Bookmaker' },
 
             $or: [{ sportID: 1 }, { sportID: 2 }, { sportID: 4 }]
           }
