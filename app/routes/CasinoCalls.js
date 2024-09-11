c3a6d531b8817bbf56deef01ed7ff9ae3a9a93db
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-11 20:48:15 +0500

    changes

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -94,7 +94,7 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
       return 0
     } else if (action === 1) { 
       const depositLastBetTime = await Cash.find({ userId: user.userId,  description: "Casino (Casino Hold'em)" }).sort({ _id: -1 });
-      if (depositLastBetTime.length > 0 && (betTime - depositLastBetTime[depositLastBetTime.length-1].betDateTime) < 1000) {
+      if (depositLastBetTime.length > 0 && (betTime - depositLastBetTime[depositLastBetTime.length-1].betDateTime) < 500) {
         console.log('Transaction occurred too quickly, skipping...');
         return; // Skip transaction
       }
