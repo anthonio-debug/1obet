fa4a0e0e61dca9ad829acc39dc6a25da60ab6b54
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-11 16:54:56 +0500

    ADD LOGS

--- app/routes/sportsHighlights.js
+++ app/routes/sportsHighlights.js
@@ -90,7 +90,6 @@ async function getAllSportsHighlight(req, res) {
     ]);
     
     let marketData = [];
-    console.log("sportsHighlights====", sportsHighlights);
     
     if (sportsHighlights.length > 0) {
       for (let i = 0; i < sportsHighlights.length; i++) {
@@ -115,6 +114,8 @@ async function getAllSportsHighlight(req, res) {
         ]);
         sportsHighlights[i].totalMatched = marketData[0] ? marketData[0].totalMatched : 0
         sportsHighlights[i].serverTime = serverTime;
+
+        console.log("MarketData", marketData)
 
       }
     }
