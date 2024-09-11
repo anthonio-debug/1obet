981060aa30a700d672067023883dce135d12f67b
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-11 16:47:38 +0500

    ADD LOGS

--- app/routes/sportsHighlights.js
+++ app/routes/sportsHighlights.js
@@ -90,7 +90,6 @@ async function getAllSportsHighlight(req, res) {
     ]);
     
     let marketData = [];
-    console.log(" ======== ids ", marketData);
 
     if (sportsHighlights.length > 0) {
       for (let i = 0; i < sportsHighlights.length; i++) {
@@ -118,7 +117,8 @@ async function getAllSportsHighlight(req, res) {
 
       }
     }
-
+    
+    console.log(" ======== CHECK BEOFRE LOG");
     const ids = await inPlayEvents.distinct("Id", {
       sportsId: sportId,
       openDate: {
@@ -128,7 +128,7 @@ async function getAllSportsHighlight(req, res) {
     })
     const totalOpenMarkets = await marketIds.countDocuments({ status: "OPEN", eventId: { $in: ids } })
 
-    console.log(" ======== ids ", ids);
+    console.log(" ======== ids  ", ids);
 
     return res.send({
       success: true,
