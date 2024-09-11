3598607e9c35a7151ea4456b49fb7d520f25b898
bhattitayab <bhattitayab747@gmail.com>
2024-09-11 23:24:57 +0400

    track race market results

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -759,7 +759,7 @@ async function deleteOdds(req, res) {
     // await MarketIDS.deleteMany({
     //   marketName: { $regex: /Overs Line|Runs Line/ }
     // });
-    await MarketIDS.deleteMany({marketId:'1.232738763bm',eventId:'33564157',marketName:'Bookmaker'});
+    await MarketIDS.deleteMany({winnerInfo: null,sportID:7});
 
     
     //await InPlayEvents.updateMany({ Id: eventId }, { $set: { hasFancy: true } });
