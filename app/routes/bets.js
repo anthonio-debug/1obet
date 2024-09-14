9ca6b84ed4642207bf0e54fd8f81d134c05c2b9c
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-09-14 18:29:34 +0500

    decrease the time of races

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -180,8 +180,9 @@ function checkMultiResponse(odds, rates) {
       }
     }
 
-    return lastMatched;
+    return lastMatched !== null ? lastMatched : null;
   }
+  return null;
 }
 
 const placeBet = async (req, res) => {
@@ -1110,7 +1111,7 @@ const placeBet = async (req, res) => {
       }));
 
       if (selectedBetRate == betRate) {
-        for (let i = 1; i < 5 + delayAddition; i++) {
+        for (let i = 1; i < 3 + delayAddition; i++) {
           setTimeout(async () => {
             // const url = `${config.horseRaceUrl}/odds/?ids=${id}`;
             // const response = await axios.get(url);
@@ -1169,7 +1170,7 @@ const placeBet = async (req, res) => {
           message: `Bet Miss Matched-17 `
         });
       } else if (type == 1 && selectedBetRate != betRate) {
-        for (let i = 0; i < 4 + delayAddition; i++) {
+        for (let i = 0; i < 3 + delayAddition; i++) {
           setTimeout(async () => {
             // const url = `${config.horseRaceUrl}/odds/?ids=${id}`;
             // const response = await axios.get(url);
@@ -1213,7 +1214,7 @@ const placeBet = async (req, res) => {
         // ELSE
         // mistmatch.....
       } else if (type == 0 && selectedBetRate != betRate) {
-        for (let i = 0; i < 4 + delayAddition; i++) {
+        for (let i = 0; i < 3 + delayAddition; i++) {
           setTimeout(async () => {
             // const url = `${config.horseRaceUrl}/odds/?ids=${id}`;
             // const response = await axios.get(url);
