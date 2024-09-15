57b44d2422aabfee730dee7384ad9003065869e7
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-15 20:43:08 +0500

    Update sportsTestAPI.js

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -136,6 +136,10 @@ const apiCallForOdds = async (marketId,counter) => {
   const response = await axios.post(url, data, header);
   console.log("}]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]Arhammmmmmmmmmmmmmmmmmmmmmmmmmmmm", response?.data?.result);
   console.log("}]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]Arhammmmmmmmmmmmmmmmmmmmmmmmmmmmm", counter);
+  const runnerFromAPI = oddsData[0]?.runners.find((runner) => runner.selectionId == selectionId);
+  const ApiResponseOdds = runnerFromAPI?.ex?.availableToBack;
+ var  selectedOddsValue = ApiResponseOdds[0].price;
+  console.log("}]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]Arhammmmmmmmmmmmmmmmmmmmmmmmmmmmmoddddddddddddddddddddddddds", selectedOddsValue)
   return response?.data?.result;
 };
 const placeBet = async (req, res) => {
