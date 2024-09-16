7470dc96c4d946afd77b2142656a14315bd5daf8
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-09-16 12:55:57 +0500

    comment log

--- app/routes/sportsHighlights.js
+++ app/routes/sportsHighlights.js
@@ -116,7 +116,7 @@ async function getAllSportsHighlight(req, res) {
         sportsHighlights[i].totalMatched = marketData[0] ? marketData[0].totalMatched : 0
         sportsHighlights[i].serverTime = serverTime;
 
-        console.log("MarketData", marketData)
+        // console.log("MarketData", marketData)
 
       }
     }
@@ -130,7 +130,7 @@ async function getAllSportsHighlight(req, res) {
     })
     const totalOpenMarkets = await marketIds.countDocuments({ status: "OPEN", eventId: { $in: ids } })
 
-    console.log(" ======== ids  ", ids);
+    // console.log(" ======== ids  ", ids);
 
     return res.send({
       success: true,
