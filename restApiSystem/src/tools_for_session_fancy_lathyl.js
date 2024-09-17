5ceb305d1e8eb312aa4bfa6cd0807022f66f08d4
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-17 20:05:03 +0500

    Update tools_for_session_fancy_lathyl.js

--- restApiSystem/src/tools_for_session_fancy_lathyl.js
+++ restApiSystem/src/tools_for_session_fancy_lathyl.js
@@ -223,7 +223,7 @@ if(bookmakerOdds!==0){
          
          
         // if (!FancyOddsMap.has(eventId) || !isObjectEqual(FancyOddsMap.get(eventId), fancyData)) {
-          console.log('fancy oddsssssssssssssss data returned for ',eventId);
+          console.log('fancy oddsssssssssssssss data returned for ',fancyData);
               FancyOddsMap.set(eventId, fancyData)
               let newFancyOdds = new FancyOdds({
                 eventId: eventId,
