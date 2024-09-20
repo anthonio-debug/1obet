c8d77c754f025770fb553883f12bef54b15af737
bhattitayab <bhattitayab747@gmail.com>
2024-09-21 00:11:46 +0400

    lable for ersults

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -122,6 +122,7 @@ function scoreChecker() {
         });
 
         await newRecord.save();
+        //Sports Results Saved
         await Bets.updateMany({ marketId: betData.marketId, sportsId: betData.sportsId }, { $set: { resultId: newRecord._id } });
 
         if (result.winnerSelectionId == -1) {
