83258620107836faf6f521b078e88ec14a2da58c
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-20 08:38:41 +0500

    Update deposits.js

--- app/routes/deposits.js
+++ app/routes/deposits.js
@@ -604,7 +604,7 @@ function getLedgerDetails(req, res) {
         {
           $unwind: {
             path: '$betInfo',
-            preserveNullAndEmptyArrays: true // Change to false if you only want deposits with matching bets
+            preserveNullAndEmptyArrays: false // Change to false if you only want deposits with matching bets
           }
         },
         {
