ae93eb80b7b82177aea0221aba525237f61e773f
Mujahid789 <mujahidinfo789@gmail.com>
2024-09-20 19:54:06 +0500

    --

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -5616,7 +5616,7 @@ async function getDuplicateEntries(req, res) {
   try {
     console.log (">>>>>>>>>>> getDuplicateEntries is running >>>>>>>>>>")
     const record =await Bets.aggregate([
-      { $sort: { createdAt: -1 } },
+      { $sort: { betDateTime: -1 } },
        { $limit: 20 },
        {
          $lookup: {
