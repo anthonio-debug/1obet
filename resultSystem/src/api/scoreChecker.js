dc92a01a559459091ed2620de83c970a0442903a
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-22 02:33:25 +0500

    Update scoreChecker.js

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -134,6 +134,7 @@ function scoreChecker() {
         await Bets.updateMany({ marketId: betData.marketId, sportsId: betData.sportsId }, { $set: { resultId: newRecord._id } });
 
         if (result.winnerSelectionId == -1) {
+          console.log("here 22222222222222222222222222222")
           //console.log("result.winnerSelectionId == -1 -->", betData.marketId);
           for (const bet of bets) {
             if (typeof bet.isManuel !== 'undefined' && bet.isManuel == true && result.manuelClose == false) {
