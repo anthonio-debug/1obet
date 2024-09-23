eb20a3fa9093898f528589bf2f80b545ae89fd2e
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-24 02:15:40 +0500

    user id commented

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -78,7 +78,7 @@ function ToolForResults() {
         if (!result.betDocument) continue;
 
         if (result.betDocument.sportsId === '1' || result.betDocument.sportsId === '2' || result.betDocument.sportsId === '4') {
-          await scoreChecker.eventsResult(result.betDocument,userId);
+          await scoreChecker.eventsResult(result.betDocument);
         } else if (result.betDocument.sportsId === '7' || result.betDocument.sportsId === '4339') {
           await scoreChecker.racingResult(result.betDocument);
         } else {
