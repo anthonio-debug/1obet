561641f1afcc49ee66500eac7ee1b25628090f3f
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-09-23 12:49:07 +0500

    logs the market data

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -3533,8 +3533,10 @@ async function getMarketsByEventId(req, res) {
     
     const response = await axios.post(url, JSON.stringify(requestData), header);
     console.log("check after Url")
-
+    
     const marketsData = response.data;
+    console.log("marketsData", marketsData)
+    
 
     res.status(200).json({ success: true, data: marketsData });
   } catch (err) {
