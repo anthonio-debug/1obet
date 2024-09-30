777874e70fe2a35454452a61fddeb902ca104082
arham47 <i.am.arhamkhan25@gmail.com>
2024-09-30 06:31:28 +0500

    Update CasinoCalls.js

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -383,7 +383,10 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
          *
          */
 
-        let bettor_won_amount = credit - debit* casinoMultiples;
+        let bettor_won_amount = (credit - debit) * casinoMultiples;
+        console.log("bettor won amount ==================",bettor_won_amount)
+        console.log("bettor won credit ==================",credit)
+        console.log("bettor won debit ==================",debit)
         let GameName = 'N/A';
         if (game)
           GameName = game.name;
