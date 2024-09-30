013b5bcb07fc45602054f00a9987adda9421d24d
Mujahid789 <mujahidinfo789@gmail.com>
2024-09-30 18:09:15 +0500

    --

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -7295,9 +7295,9 @@ async function getWinnigLossing(req, res) {
 
 async function getUserParents(req, res) {
   try {
-    const user_id=req.body.user_id
+    const user_id=req.body.userId
     //  const user_id =20243
-    let parentUserIds = await getParents(user_id);
+    let parentUserIds = await getParents(userId);
     // console.log("-=-=-=-=-=-=--=-=-= parentUserIds ",parentUserIds)
     let parentUsers = await User.find(
       { userId: { $in: parentUserIds } },
