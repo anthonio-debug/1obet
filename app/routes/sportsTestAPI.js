9c96549c8377f8ece43ee10b97bad43f13e0d2f7
Mujahid789 <mujahidinfo789@gmail.com>
2024-09-30 13:03:00 +0500

    --

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -6230,7 +6230,7 @@ async function eventsResult(betData) {
 
       };
       const response = await axios.post(url, requestData, header);
-      console.log("----------------------- response.data --------------------->> ",response.data);
+      console.log("----------------------- response.data --------------------->> ",response.data[0]);
       
       if (!response?.data?.result) return;
       const resData = response.data.result;
