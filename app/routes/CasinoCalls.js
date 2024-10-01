691fd590a3ce2d685bc86f8e63252cebfa8da16a
arham47 <i.am.arhamkhan25@gmail.com>
2024-10-02 03:09:02 +0500

    Update CasinoCalls.js

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -99,10 +99,10 @@ async function findAndProcessTransactions(user) {
         if (rounds.action === 'rollback') {
           totalRollBackAmount += Number(rounds.amount);
         }
-        const user = await users.findOne({ remoteId: Number(rounds.remote_id) });
         
-      
+        
       }
+      const user = await users.findOne({ remoteId: Number(tran.remote_id) });
       
       
         adjustedNewExposure = user.exposure + totalDebitAmount;
