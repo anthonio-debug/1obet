f30c7ea02242b3086942c6bdcfd4440604f10ddd
arham47 <i.am.arhamkhan25@gmail.com>
2024-10-02 02:10:21 +0500

    Update CasinoCalls.js

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -112,8 +112,7 @@ async function findAndProcessTransactions(user) {
               exposure: adjustedNewExposure,
               tempExposure: adjustedNewTempExposure
             }
-          },
-         
+          }
         );
      
 
