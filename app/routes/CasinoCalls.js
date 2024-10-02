8835716b90049cc7b74743d5db0a9ef9791d165c
arham47 <i.am.arhamkhan25@gmail.com>
2024-10-03 03:42:17 +0500

    Update CasinoCalls.js

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -174,7 +174,8 @@ async function findAndProcessTransactions(user) {
       const now = new Date();
       const formattedDate = now.toISOString().split('T')[0];
       const betTime = now.getTime();
-
+console.log("deposit entry user exposure==============>",user.exposure)
+console.log("deposit entry user adjustedNewExposure==============>",adjustedNewExposure)
       let betTransaction = {
         userId: user.userId,
         description: `Casino (${gameName})`,
