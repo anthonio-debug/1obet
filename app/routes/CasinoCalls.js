bf61c30b5258805d6e25abe8feda13be49f8536f
bhattitayab <bhattitayab747@gmail.com>
2024-10-02 18:46:33 +0400

    session is not a function

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -80,7 +80,7 @@ async function findAndProcessTransactions(user) {
     if (!groupedTransactions || groupedTransactions.length === 0) {
       console.log('No transactions found for the given round_id and username.');
       await session.abortTransaction();  // Abort the transaction if no records found
-      session.endSession();
+      //session.endSession();
       return;
     }
 
