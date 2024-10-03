7581f626f792e035343ed9e818ff2cbfeeb9fdb4
bhattitayab <bhattitayab747@gmail.com>
2024-10-03 20:41:03 +0400

    consoles to check user available balance

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -89,7 +89,7 @@ async function findAndProcessTransactions(user) {
 
    
     for (const tran of groupedTransactions) {
-       await new Promise(resolve => setTimeout(resolve, 300));
+       await new Promise(resolve => setTimeout(resolve, 1000));
     let totalCreditAmount = 0;
     let totalDebitAmount = 0;
     let totalRollBackAmount = 0;
