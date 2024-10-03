9a6ddc514e0813a1c23e80ec3887e8d91fda2b6d
arham47 <i.am.arhamkhan25@gmail.com>
2024-10-03 20:09:35 +0500

    Update CasinoCalls.js

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -97,9 +97,9 @@ async function findAndProcessTransactions(user) {
 
       let adjustedNewExposure = 0;
       let adjustedNewTempExposure = 0;
-      const roundIds = await CasinoCalls.findOne({ round_id: tran._id })
+      const roundIds = await CasinoCalls.distinct('round_id', { round_id: tran._id });
 
-      console.log("rouuuuuuuuuuuuuuuuuuuundID=========", tran._id.toString());
+      console.log("rouuuuuuuuuuuuuuuuuuuundID=========", roundIds);
 
       for (const rounds of roundIds) {
         console.log("userName=========", rounds.username);
