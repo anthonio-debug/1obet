54e1b7f88da91928d709a69ac09684b236d4467a
bhattitayab <bhattitayab747@gmail.com>
2024-10-03 17:12:04 +0400

    sportstest delete query for deposits

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4034,7 +4034,9 @@ async function deleteOdds(req, res) {
     //   });
     //   session.save();
     // }
-
+    const casinocallUpdate = await CasinoCalls.updateMany({ username: "user_" + userId ,gameplay_final:1}, {
+      isProcessing:true
+    })
     await Odds.deleteMany({});
 
 
