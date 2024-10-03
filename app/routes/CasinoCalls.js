a8e754adc2a95b2c3133325979bad23a8ac9766e
arham47 <i.am.arhamkhan25@gmail.com>
2024-10-04 00:43:37 +0500

    ss

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -129,8 +129,11 @@ async function findAndProcessTransactions() {
 
               // Prepare transaction data
               const adjustedNewExposure = user.exposure + totalDebitAmount * casinoMultiples;
-              const adjustedNewTempExposure = user.tempExposure - totalDebitAmount * casinoMultiples;
-              const updatedAvailableBalance = user.availableBalance + differenceDbCr;
+            const adjustedNewTempExposure = user.tempExposure - totalDebitAmount * casinoMultiples;
+            var updatedAvailableBalance=0
+            const updateAvailableBalance = (user, differenceDbCr) => Promise.resolve(user.availableBalance + differenceDbCr).then((updatedAvailableBalance) => {
+              updatedAvailableBalance=updatedAvailableBalance
+             });
 
               // Check for existing deposit entry
               const existingDeposit = await Cash.findOne({ roundId: tran._id.toString() }).session(session);
--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -26,9 +26,9 @@ function ToolForEvent() {
     if (config.activeProvider === 'NEW') {
       fetchEvents();
       setBrokenRecord();
-      setInterval(() => {
+      setTimeout(() => {
         findAndProcessTransactions()
-      },4000)
+   },5000)
       setInterval(fetchEvents,  60 * 1000);
       // setInterval(updateOddsFormLimitless,  60 * 1000);
       
