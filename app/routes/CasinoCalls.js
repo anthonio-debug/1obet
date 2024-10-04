7ad4e4cdc6f945271e05a21c8414f87988dae092
bhattitayab <bhattitayab747@gmail.com>
2024-10-04 15:52:51 +0400

    some data was missing updates from gpt

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -54,6 +54,7 @@ const checkMarketBlocked = async (user) => {
 }
 const mongoose = require('mongoose');
 async function findAndProcessTransactions(user) {
+  console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",user);
   const session = await mongoose.startSession();
   const maxRetries = 3; // Max retries for the transaction
 
