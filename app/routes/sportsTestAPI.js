ae4411cfc294fc3288635c92594b0e5a1ac3e7e5
bhattitayab <bhattitayab747@gmail.com>
2024-10-04 14:11:16 +0400

    distinct rounds by username Corrected latest code

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -7346,8 +7346,9 @@ async function getDistinctRoundIds(req, res) {
     }
     // const distinctRoundIds = await CasinoCalls.distinct('round_id',{ username });
     const distinctRoundIds = await CasinoCalls.aggregate([
-     
-      { $match: { username: "user_"+userId } },
+      {
+        $match: { username: "user_"+userId }
+      },
       {
         $group: {
           _id: "$round_id",
@@ -7365,21 +7366,38 @@ async function getDistinctRoundIds(req, res) {
             $sum: {
               $cond: [{ $eq: ["$action", "rollback"] }, 1, 0]
             }
+          },
+          debitAmountSum: {
+            $sum: {
+              $cond: [{ $eq: ["$action", "debit"] }, { $toDouble: "$amount" }, 0]
+            }
+          },
+          creditAmountSum: {
+            $sum: {
+              $cond: [{ $eq: ["$action", "credit"] }, { $toDouble: "$amount" }, 0]
+            }
+          },
+          rollbackAmountSum: {
+            $sum: {
+              $cond: [{ $eq: ["$action", "rollback"] }, { $toDouble: "$amount" }, 0]
+            }
           }
         }
       },
-
-      // Project the final structure with the counts for each action
       {
         $project: {
           _id: 0,
           round_id: "$_id",
-          debit: "$debitCount",
-          credit: "$creditCount",
-          rollback: "$rollbackCount"
+          debitCount: 1,
+          creditCount: 1,
+          rollbackCount: 1,
+          debitAmountSum: 1,
+          creditAmountSum: 1,
+          rollbackAmountSum: 1
         }
       }
-    ])
+    ]
+    )
     res.status(200).json({
       success: true,
       message: 'Round Ids fetched successfully',
