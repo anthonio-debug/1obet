b582f880bf55faea8d77988d9539effa4573aad1
bhattitayab <bhattitayab747@gmail.com>
2024-10-04 06:48:15 +0400

    casino calls optimzation working fine and also rollback and now fixing duplicate entries into deposits corrected clientPL spelling

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -133,7 +133,7 @@ const groupedTransactions = await CasinoCalls.aggregate([
   const user = await users.findOne(
     { remoteId: Number(tran.remote_id) }
  
-  );
+  )
       
       console.log("exposureeeeeeeeeeeeeeeeeeee=>",user.exposure)
 
