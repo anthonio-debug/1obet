b0e929c3c0a446b14396e54027fdb397407d1f76
bhattitayab <bhattitayab747@gmail.com>
2024-10-04 05:07:13 +0400

    casino calls optimzation working fine and also rollback and now fixing duplicate entries into deposits

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -191,7 +191,7 @@ const groupedTransactions = await CasinoCalls.aggregate([
 console.log("deposit entry user exposure==============>",user.exposure)
 console.log("deposit entry user adjustedNewExposure==============>",adjustedNewExposure)
 const checkForExistingRoundIdInDeposit = await Cash.find({ roundId: tran._id.toString() })
-console.log("checkForExistingRoundIdInDeposit.length.....................................................",checkForExistingRoundIdInDeposit.length);
+console.log("checkForExistingRoundIdInDeposit.length.....................",tran._id.toString(),"................................",checkForExistingRoundIdInDeposit.length);
       if (checkForExistingRoundIdInDeposit.length === 0) {
       let betTransaction = {
         userId: user.userId,
