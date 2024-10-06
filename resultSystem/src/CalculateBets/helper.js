eb7edf26267476580c35f0a2c26fd76b7c532992
bhattitayab <bhattitayab747@gmail.com>
2024-10-07 01:58:52 +0400

    new function in helper.js for winning-loosing etc

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -1,6 +1,18 @@
 
 function getAmountOfWinner(bet, selectionId) {
     console.log("Reached inside the function..............................");
+
+    const now = new Date();
+  const year = now.getFullYear().toString();
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const day = now.getDate().toString().padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}`;
+
+let calculatedExp = 0;
+const userId = bet.userId;
+let TotalWin = 0;
+let TotalLose = 0;
+
     const runnerPosition = bet?.runnersPosition
     var amount = null
     runnerPosition?.forEach(winner => {
