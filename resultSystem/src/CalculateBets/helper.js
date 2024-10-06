cdb686e0512fe9589df3488bb5b7d8cd0e205d27
bhattitayab <bhattitayab747@gmail.com>
2024-10-07 01:51:38 +0400

    new function in helper.js for winning-loosing etc

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -1,13 +1,3 @@
-const Bets = require('../../../app/models/bets');
-const Deposits = require('../../../app/models/deposits');
-const ExpRec = require('../../../app/models/ExpRec');
-const MarketIDS = require('../../../app/models/marketIds');
-const Events = require('../../../app/models/events');
-const User = require('../../../app/models/user');
-const config = require('config');
-
-const Session = require('../../../app/models/Session');
-const CurrentPosition = require('../../../app/models/CurrentPosition');
 
 function getAmountOfWinner(bet, selectionId) {
     console.log("Reached inside the function..............................");
@@ -30,9 +20,6 @@ function getAmountOfWinner(bet, selectionId) {
 return amount 
 }
 module.exports = {
-    getParents,
-    handleLosingCasinoBet,
-    handleWinningCasinoBet,
-    handleLosingBetCheck,
+
     getAmountOfWinner
 }
