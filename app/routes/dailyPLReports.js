e9d39635340f3bdfce00922095be1157dad78455
arham47 <i.am.arhamkhan25@gmail.com>
2024-10-06 20:08:33 +0500

    Update dailyPLReports.js

--- app/routes/dailyPLReports.js
+++ app/routes/dailyPLReports.js
@@ -5,7 +5,6 @@ const User = require('../models/user');
 
 const Events = require('../models/events');
 const loginRouter = express.Router();
-
 const getDailyPLReport = async (req, res) => {
   const errors = validationResult(req);
   if (errors.errors.length !== 0) {
@@ -14,7 +13,7 @@ const getDailyPLReport = async (req, res) => {
 
   const userId = parseInt(req.decoded.userId);
   const currentUser = await User.findOne({ userId: userId });
-  const users = new Set([userId]);  // Use a Set to avoid duplicates
+  const users = new Set([userId]); // Use a Set to avoid duplicates
   let parents = [userId];
   let childUsers = [];
   let sportsIdQuery = { $ne: null };
@@ -34,18 +33,17 @@ const getDailyPLReport = async (req, res) => {
     // If there are new child users, add them to the users Set and parents array
     if (newChildUsers.length) {
       newChildUsers.forEach(user => users.add(user));
-      parents = newChildUsers;  // Update parents with new child users
+      parents = newChildUsers; // Update parents with new child users
     } else {
       // Exit the loop if there are no new users to process
       break;
     }
-
   } while (childUsers.length > 0);
 
   const response = await CashDeposit.aggregate([
     {
       $match: {
-        userId: { $in: Array.from(users) },  // Convert Set to Array
+        userId: { $in: Array.from(users) }, // Convert Set to Array
         sportsId: sportsIdQuery,
         cashOrCredit: { $in: ["Bet", "Commission", "loosing"] },
         createdAt: { $gte: req.query.startDate, $lte: req.query.endDate }
@@ -72,26 +70,20 @@ const getDailyPLReport = async (req, res) => {
     },
     {
       $group: {
-        _id: "$userId",  // Group by userId
-        totalAmount: { $sum: "$amount" },  // Sum amounts for each user
+        _id: "$userId", // Group by userId
+        totalAmount: { $sum: "$amount" }, // Sum amounts for each user
         name: { $first: "$userInfo.userName" },
-        role: { $first: "$userInfo.role" }  // Get the role of the user
+        role: { $first: "$userInfo.role" } // Get the role of the user
       }
     },
     {
       $group: {
-        _id: null,  // Combine all entries
+        _id: null, // Combine all entries
         results: {
           $push: {
             userId: "$_id",
             name: "$name",
-            totalAmount: {
-              $cond: {
-                if: { $eq: "$role", 1 },  // If super admin
-                then: "$totalAmount",      // Show their actual amount
-                else: "$totalAmount"       // For others (including admins), add amounts of their children in the final result
-              }
-            }
+            totalAmount: "$totalAmount" // No condition needed here for the totalAmount
           }
         }
       }
@@ -108,9 +100,21 @@ const getDailyPLReport = async (req, res) => {
                 "$$value",
                 {
                   $cond: [
-                    { $eq: { $arrayElemAt: ["$$this.role", 0] }, 2 },  // Check if role is admin
-                    [{ userId: "$$this.userId", name: "$$this.name", totalAmount: { $sum: [{ $ifNull: ["$$this.totalAmount", 0] }, { $ifNull: ["$$this.totalAmount", 0] }] } }],  // Sum up for admins
-                    [{ userId: "$$this.userId", name: "$$this.name", totalAmount: "$$this.totalAmount" }] // For super admin, keep actual amount
+                    { $eq: ["$$this.role", 2] }, // Check if the role is 2
+                    [{
+                      userId: "$$this.userId",
+                      name: "$$this.name",
+                      totalAmount: {
+                        $sum: [
+                          { $ifNull: ["$$this.totalAmount", 0] } // Ensure we handle null values correctly
+                        ]
+                      }
+                    }], // For role 2 (admins)
+                    [{
+                      userId: "$$this.userId",
+                      name: "$$this.name",
+                      totalAmount: "$$this.totalAmount" // For other roles, keep actual amount
+                    }] // For other users
                   ]
                 }
               ]
@@ -118,7 +122,7 @@ const getDailyPLReport = async (req, res) => {
           }
         }
       }
-    },
+    }    
   ]);
 
   return res.send({
@@ -128,6 +132,7 @@ const getDailyPLReport = async (req, res) => {
     total: response[0]?.results?.length || 0
   });
 };
+
 
 
 
