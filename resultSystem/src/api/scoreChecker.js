a5aa33d22b9831fad9fb2edd80a5f50548dacd6f
bhattitayab <bhattitayab747@gmail.com>
2024-10-06 18:18:08 +0400

    new function in helper.js for winning-loosing etc

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -11,8 +11,11 @@ const MarketIDs = require('../../../app/models/marketIds');
 const FancyOdds = require('../../../app/models/fancyOdds');
 
 const { API_DOMAIN } = require('../../../app/global/constants');
-const { checkActiveBettors } = require('../../../helper/bet');
+const { getAmountOfWinner } = require('../CalculateBets/helper');
 const { getSessionFancyResult, getSessionBookmakerResult } = require('../../../helper/api/sessionAPIHelper');
+
+
+
 const { handleLosingBet, handleWinningBet, handleDrawBet } = require('../CalculateBets/calculations');
 
 const horseRaceUrl = 'http://136.244.77.249:33333';
