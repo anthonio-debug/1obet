a2eeec528b49ebf629ff7f0a78b5266d83ddb1da
bhattitayab <bhattitayab747@gmail.com>
2024-10-09 22:28:31 +0400

    casinos reverted reversed exposure save in db minus sign missing

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -170,8 +170,9 @@ parent and then add new NextExposure for this parent based on highestAmount for 
       updatedExposure = -amountToBeSub;
       console.log("first entry check for minus::::::",updatedExposure);
     }else{
-      console.log("first entry check for minus for else::::::",updatedExposure);
+      
       updatedExposure = -(arr_userExp[user.userId] - amountToBeSub);
+      console.log("first entry check for minus for else::::::",updatedExposure);
     }
     console.log("parent id : ",user.userId," previous  Exposure---------------------------------------------",user.exposure);
     console.log("parent id : ",user.userId," Reversed  Exposure---------------------------------------------",arr_userExp[user.userId]);
