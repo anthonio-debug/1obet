c5d06867206406868e4495975906d9d5c56774bf
bhattitayab <bhattitayab747@gmail.com>
2024-10-11 03:50:17 +0400

    dealer exposure dealer availabel balance correction

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -121,8 +121,7 @@ function scoreChecker() {
         const bets = await Bets.find({
           marketId: betData.marketId,
           sportsId: betData.sportsId,
-          status: 1,
-          calculateExp:true,
+          status: 1
         });
 
         // const checkEventMarket = await MarketIDs.findOne({ eventId: betData.eventId, marketName: "Match Odds" }).sort({ _id: -1 });
@@ -141,8 +140,7 @@ function scoreChecker() {
 
           //console.log("result.winnerSelectionId == -1 -->", betData.marketId);
           for (const bet of bets) {
-            let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelectionId);
-            return;
+          
           
             if (typeof bet.isManuel !== 'undefined' && bet.isManuel == true && result.manuelClose == false) {
               continue;
@@ -153,10 +151,6 @@ function scoreChecker() {
         } else {
           //console.log("ELSE result.winnerSelectionId == -1 -->", betData.marketId);
           for (const bet of bets) {
-
-            let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelectionId);
-            return;
-
             if (typeof bet.isManuel !== 'undefined' && bet.isManuel == true && result.manuelClose == false) {
               continue;
             }
