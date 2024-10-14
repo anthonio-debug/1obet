a53d11442eacb6eda8b81656b36c438adb7c0918
bhattitayab <bhattitayab747@gmail.com>
2024-10-15 01:23:51 +0400

    isue in figures statement

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -66,7 +66,7 @@ function scoreChecker() {
   }
 
   async function eventsResult(betData) {
-    console.log("Result checking event for ", betData);
+    console.log("Result checking event for ", betData.marketId , "================" ,  betData.userId,"==============");
     try {
       let results;
       const manuelRecord = await MarketIDs.findOne({
