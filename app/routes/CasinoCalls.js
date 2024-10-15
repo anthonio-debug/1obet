5589aaf9e33d3795977a348af56ecfa3ba32f2d2
bhattitayab <bhattitayab747@gmail.com>
2024-10-16 01:26:45 +0400

    casino calls transactions problem

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -62,12 +62,13 @@ async function findAndProcessTransactions(user) {
   const month = (now.getMonth() + 1).toString().padStart(2, '0');
   const day = now.getDate().toString().padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
+  session.startTransaction(); 
   for (let attempt = 0; attempt < maxRetries; attempt++) {
     try {
       
 
       const limitValue = 1; // Set your desired limit here
-      session.startTransaction(); 
+      
       const groupedTransactions = await CasinoCalls.aggregate([
         {
           $match: {
@@ -98,7 +99,7 @@ async function findAndProcessTransactions(user) {
       ]).session(session);  
 
 
-
+     
 
       
       if (!groupedTransactions || groupedTransactions.length === 0) {
