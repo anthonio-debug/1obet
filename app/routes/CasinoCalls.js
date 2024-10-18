2c55079452e1b430548494deec850d11f0d27f32
bhattitayab <bhattitayab747@gmail.com>
2024-10-18 21:44:50 +0400

    insertMissingTransactions enabled

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -454,12 +454,12 @@ async function findAndProcessTransactions(user) {
     } catch (error) {
       console.error('Error processing transactions:', error);
       //await session.abortTransaction();
-      if (attempt < maxRetries - 1) {
-        // Delay before retrying
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Delay for 1 second
-      } else {
-        throw error; // Re-throw the error after max retries
-      }
+      // if (attempt < maxRetries - 1) {
+      //   // Delay before retrying
+      //   await new Promise(resolve => setTimeout(resolve, 1000)); // Delay for 1 second
+      // } else {
+      //   throw error; // Re-throw the error after max retries
+      // }
     }finally {
       // End the session
       session.endSession();
