1328aa99fcbd70c65fa06024f6c65cd6099e58c7
bhattitayab <bhattitayab747@gmail.com>
2024-10-18 22:33:49 +0400

    insertMissingTransactions enabled

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -1097,7 +1097,8 @@ function scoreChecker() {
         }else{
           correctScore = bet.score % 10;
         }
-        console.log("correctScore-----------------------------------------figures------------>>>>",correctScore);
+        console.log("correctScore---------------------",bet.betData._id,"--------------------figures------------>>>>",correctScore);
+        
         //let winningsCalculate = await getAmountOfWinnerFigures(bet.betData,correctScore);
 
 
@@ -1157,7 +1158,7 @@ function scoreChecker() {
         //let winningsCalculate = await getAmountOfWinnerFigures(bet.betData,correctScore);
 
 
-        console.log("correctScore-----------------------------------------KALLI,JOTTA------------>>>>",correctScore);
+        console.log("correctScore-----------------------",bet.betData._id,"--------------------KALLI,JOTTA------------>>>>",correctScore);
         
         // if (bet.score == -1) {
         //   await handleDrawBet(bet.betData);
@@ -1206,7 +1207,7 @@ function scoreChecker() {
         }else{
           correctScore = bet.score % 10;
         }
-        console.log("correctScore-----------------------------------------BARA/CHOTA------------>>>>",correctScore);
+        console.log("correctScore---------------------",bet.betData._id,"----------------------BARA/CHOTA------------>>>>",correctScore);
 
         //let winningsCalculate = await getAmountOfWinnerFigures(bet.betData,correctScore);
 
