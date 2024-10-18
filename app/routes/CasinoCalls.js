9a329053c6c2bed8f9c321da733b71f28160de02
bhattitayab <bhattitayab747@gmail.com>
2024-10-18 21:36:32 +0400

    insertMissingTransactions enabled

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -54,7 +54,7 @@ const checkMarketBlocked = async (user) => {
 }
 const mongoose = require('mongoose');
 async function findAndProcessTransactions(user) {
-  //await insertMissingTransactions();
+  await insertMissingTransactions();
   //console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",user);
   const session = await mongoose.startSession();
   const maxRetries = 1; // Max retries for the transaction
