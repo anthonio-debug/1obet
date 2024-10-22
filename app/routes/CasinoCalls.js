0161bc21343da6d86c7b4b490fb7045ba968616c
bhattitayab <bhattitayab747@gmail.com>
2024-10-22 23:25:41 +0400

    casino and majid code

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -59,6 +59,7 @@ async function findAndProcessTransactions(user) {
   //console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",user);
   const session = await mongoose.startSession();
   await insertMissingTransactions();
+  return
   const maxRetries = 1; // Max retries for the transaction
   const now = new Date();
   const year = now.getFullYear().toString();
