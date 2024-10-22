dec61dbcb1f0a25ff93181a2e77c3ab09b48b7f0
bhattitayab <bhattitayab747@gmail.com>
2024-10-22 08:49:23 +0400

    positive exposure blockaing

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -688,8 +688,9 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
       //console.log("arham updatedavailableBalance ",UpdatedExposure )
 
       await session.startTransaction();
-      const lastMaxWithdraw = await Cash.findOne({ userId: user.userId }).sort({ _id: -1 },{session});
+      const lastMaxWithdraw = await Cash.findOne({ userId: user.userId }).sort({ _id: -1 });
       await session.commitTransaction();
+
       if(user.exposure<=0 && user.availableBalance>=amount && lastMaxWithdraw.availableBalance >=amount && lastMaxWithdraw.availableBalance >0){
         await session.startTransaction();
        console.log("debit is successully................................................");
