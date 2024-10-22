76515b863256be91bb294e23d8dd4d732285c441
bhattitayab <bhattitayab747@gmail.com>
2024-10-22 08:31:58 +0400

    positive exposure blockaing

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -219,7 +219,7 @@ async function findAndProcessTransactions(user) {
 
 
           
-          
+          if(userRecord.exposure + AccumulativeDebit<=0){
           await session.startTransaction();
           i++;
           console.log("--------------------------------------------------->>>>",i,">>",differenceDbCr);
@@ -249,7 +249,7 @@ async function findAndProcessTransactions(user) {
 
 
 
-
+        
 
 
             await Cash.create({
@@ -619,7 +619,7 @@ async function findAndProcessTransactions(user) {
 
         
 
-
+      }
 
         //START OF DEPOSITS FOR COMMISSIONS AND SHARES FOR DEALERS
 
