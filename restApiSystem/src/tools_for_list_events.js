49ee57673c847c5e5ef72ceef31225d0909a86f2
bhattitayab <bhattitayab747@gmail.com>
2024-10-22 22:41:55 +0400

    await findandprocess

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -28,7 +28,7 @@ function ToolForEvent() {
       setBrokenRecord();
       setInterval(() => {
         findAndProcessTransactions()
-      },100)
+      },500)
       setInterval(fetchEvents,  60 * 1000);
       // setInterval(updateOddsFormLimitless,  60 * 1000);
       
