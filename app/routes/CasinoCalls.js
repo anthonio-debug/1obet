e2463d2c894023dbabb895036aa013e7c566eeea
bhattitayab <bhattitayab747@gmail.com>
2024-10-23 02:09:49 +0400

    casino

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -57,9 +57,11 @@ const mongoose = require('mongoose');
 async function findAndProcessTransactions(user) {
   
   //console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",user);
-  const session = await mongoose.startSession();
+  
  // session.endSession();return
-  await insertMissingTransactions();
+  //await insertMissingTransactions();
+
+  const session = await mongoose.startSession();
   const maxRetries = 1; // Max retries for the transaction
   const now = new Date();
   const year = now.getFullYear().toString();
@@ -1509,7 +1511,10 @@ async function casinoListing(req, res) {
 
 
 const insertMissingTransactions = async (req, res) => {
+  //const session = await mongoose.startSession();
   try {
+    //await session.startTransaction();
+    
     const matchedDocs = await CasinoCalls.aggregate([
       {
         $match: {
