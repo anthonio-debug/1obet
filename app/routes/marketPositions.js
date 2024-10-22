f543570074caa9cb28e5c41c8339d8485535b9f2
bhattitayab <bhattitayab747@gmail.com>
2024-10-22 21:36:46 +0400

    Qadir work

--- app/routes/marketPositions.js
+++ app/routes/marketPositions.js
@@ -1,58 +1,121 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 const User = require("../models/user");
+const Deposits = require("../models/deposits");
 const loginRouter = express.Router();
 
 const getMarketPositions = async (req, res) => {
   const errors = validationResult(req);
-  if (errors.errors.length !== 0) {
-    return res.status(400).send({ errors: errors.errors });
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
 
-  const userId = req.body.userId;
+  const { userId, betId } = req.body;
 
-  const users = await User.distinct("userId", { createdBy: userId });
-  const currentUser = await User.findOne({ userId: userId });
+  const currentUser = await User.findOne({ userId });
+  if (!currentUser) {
+    console.error(`Current user not found for userId: ${userId}`);
+    return res.status(404).json({ success: false, message: "Current user not found." });
+  }
 
+  let parentUserResponse = [];
   const parentUser = await User.findOne({ userId: currentUser.createdBy });
 
-  const response = await User.aggregate([
+  const childUserIds = await User.distinct("userId", { createdBy: userId });
+  const betNComission = ["Bet", "Commission", "loosing", "Settlement"];
+
+  const currentUserResponse = await Deposits.aggregate([
     {
       $match: {
-        userId: {
-          $in: users,
-        },
+        userId: currentUser.userId,
+        cashOrCredit: { $in: betNComission },
+        betId: betId,
       },
     },
     {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "userId",
+        as: "userInfo",
+      },
+    },
+    { $unwind: "$userInfo" },
+    {
       $group: {
         _id: "$userId",
-        name: { $first: "$userName" },
-        amount: { $sum: "$clientPL" },
-        role: { $first: "$role" },
+        name: { $first: "$userInfo.userName" },
+        role: { $first: "$userInfo.role" },
+        amount: { $sum: "$amount" },
       },
     },
   ]);
 
-  currentUser &&
-    response.push({
-      _id: currentUser.userId,
-      name: currentUser.userName,
-      amount: currentUser.clientPL,
-      role: currentUser.role,
-    });
+  if (parentUser) {
+    parentUserResponse = await Deposits.aggregate([
+      {
+        $match: {
+          userId: parentUser.userId,
+          cashOrCredit: { $in: betNComission },
+          betId: betId,
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "userId",
+          as: "userInfo",
+        },
+      },
+      { $unwind: "$userInfo" },
+      {
+        $group: {
+          _id: "$userId",
+          name: { $first: "$userInfo.userName" },
+          amount: { $sum: "$upLineAmount" },
+          role: { $first: "$userInfo.role" },
+        },
+      },
+    ]);
+  }
 
-  parentUser &&
-    response.push({
-      _id: parentUser.userId,
-      name: parentUser.userName,
-      amount: parentUser.clientPL,
-      role: parentUser.role,
-    });
+  const childAmount = parentUserResponse[0].amount + currentUserResponse[0].amount
 
-  return res.send({
+  const childResponse = await Deposits.aggregate([
+    {
+      $match: {
+        userId: { $in: childUserIds },
+        cashOrCredit: { $in: betNComission },
+        betId: betId,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "userId",
+        as: "userInfo",
+      },
+    },
+    { $unwind: "$userInfo" },
+    {
+      $group: {
+        _id: "$userId",
+        name: { $first: "$userInfo.userName" },
+        amount: { $sum: "$amount" },
+        role: { $first: "$userInfo.role" },
+      },
+    },
+  ]);
+  childResponse[0].amount = - childAmount
+  const response = [...childResponse, ...currentUserResponse, ...parentUserResponse];
+
+  console.log("Final response:", response);
+
+  return res.status(200).json({
     success: true,
-    message: "Market Positions Reports !",
+    message: "Market Positions Reports!",
     results: response,
   });
 };
