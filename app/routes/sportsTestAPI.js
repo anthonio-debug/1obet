d45ae4dd6ad56afe6535d8d852cf07d723afcac8
bhattitayab <bhattitayab747@gmail.com>
2024-10-23 18:20:44 +0400

    deposits count old

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4039,7 +4039,7 @@ async function deleteOdds(req, res) {
     //await expPositive.deleteMany({});
     
     const thirtyDaysAgo = new Date();
-thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 3);
+thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
  //await Cash.countDelete({ betDateTime: { $lt: thirtyDaysAgo } });
 const count = await Cash.countDocuments({ betDateTime: { $lt: thirtyDaysAgo } });
 // console.log(`Count of records older than 30 days: ${count}`);
