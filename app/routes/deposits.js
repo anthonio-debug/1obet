720cadd810ed4e46069de3d5b9df7f19f1a259b7
bhattitayab <bhattitayab747@gmail.com>
2024-10-24 20:11:25 +0400

    qadir code getledgerdetails

--- app/routes/deposits.js
+++ app/routes/deposits.js
@@ -569,7 +569,12 @@ function getLedgerDetails(req, res) {
       }];
 
       const userRole = user.role;
-      console.log("user role", user);
+
+      const cashNCreditnBet = userRole === "5" ? ['Bet', 'Casino Bet', 'Cash', 'Credit'] : ['Bet', 'Casino Bet'];
+
+      cashPipeline.push({
+        $match: { cashOrCredit: { $in: cashNCreditnBet } }
+      });
 
       if (userRole !== '5' && req.body.type) {
         cashPipeline.push({ $match: { cashOrCredit: req.body.type }, });
@@ -614,7 +619,7 @@ function getLedgerDetails(req, res) {
                 matchId: "$matchId",
                 marketId: "$marketId",
                 betSession: "$betSession",
-                roundId: "$roundId"
+                roundId: "$roundId",
               }
             }
           },
@@ -652,7 +657,6 @@ function getLedgerDetails(req, res) {
       Cash.aggregate(cashPipeline, async (err, result) => {
         if (result[0].results && result[0].results.length > 0) {
           for (let i = 0; i < result[0].results.length; i++) {
-            //console.log()
             if (result[0].results[i].betId) {
               try {
                 const betInfo = await Bet.findOne({
@@ -674,12 +678,7 @@ function getLedgerDetails(req, res) {
           }
         }
 
-        if (
-          err ||
-          !result ||
-          result.length === 0 ||
-          result[0].results.length === 0
-        ) {
+        if (err || !result || result.length === 0 || result[0].results.length === 0) {
           return res.status(200).send({ message: 'Deposit record not found' });
         }
 
@@ -691,22 +690,16 @@ function getLedgerDetails(req, res) {
             total: result[0].metadata[0] ? result[0].metadata[0].total : 0,
             limit: limit ? limit : 0,
             page: page ? page : 0,
-            pages:
-              limit && result[0].metadata[0].total
-                ? Number((result[0].metadata[0].total / limit).toFixed(0))
-                : 0,
+            pages: limit && result[0].metadata[0].total ? Number((result[0].metadata[0].total / limit).toFixed(0)) : 0,
           },
         };
 
         return res.send(responseData);
       });
-
-
     });
   } catch (err) {
     res.status(500).json({ success: false, msg: "Failed to get Ledger Detail info" })
   }
-
 }
 
 
