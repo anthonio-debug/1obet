c52516a535113154aea114db846712a0cdfc8ed5
bhattitayab <bhattitayab747@gmail.com>
2024-10-25 16:18:51 +0400

    removed some consoles

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -153,15 +153,15 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
       });
 
 
-
+      let expPositiveData;
       
-      let expPositive = await expPositive.findOne({ userId:userToUpdate.userId,betId:bet.betId,roundId:bet.marketId });
-      console.log("-------------------------------------user.............",expPositive);
+      expPositiveData = await expPositive.findOne({ userId:userToUpdate.userId,betId:bet.betId,roundId:bet.marketId });
+      console.log("-------------------------------------user.............",expPositiveData);
       console.log("-------------------------------------userToUpdate.userId.............",userToUpdate.userId);
       console.log("-------------------------------------bet.betId.............",bet.betId);
       console.log("-------------------------------------bet.marketId.............",bet.marketId);
 
-      if(expPositive){
+      if(expPositiveData){
         await expPositive.updateOne(
           {
             userId:userToUpdate.userId,betId:bet.betId,roundId:bet.marketId
@@ -289,14 +289,14 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
 
 
               
-              let expPositive = await expPositive.findOne({ userId:user.userId,betId:bet.betId,roundId:bet.marketId });
+              expPositiveData = await expPositive.findOne({ userId:user.userId,betId:bet.betId,roundId:bet.marketId });
               
-              console.log("-------------------------------------parents.............",expPositive);
+              console.log("-------------------------------------parents.............",expPositiveData);
               console.log("-------------------------------------user.userId.............",user.userId);
               console.log("-------------------------------------bet.betId.............",bet.betId);
               console.log("-------------------------------------bet.marketId.............",bet.marketId);
               
-      if(expPositive){
+      if(expPositiveData){
         await expPositive.updateOne(
           {
             userId:user.userId,betId:bet.betId,roundId:bet.marketId
