19255dd9dcf3cff5d31eb38a5097394a79465aae
bhattitayab <bhattitayab747@gmail.com>
2024-10-25 15:48:33 +0400

    removed some consoles

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -375,7 +375,7 @@ function apiRequests() {
         for(let element of marketsData) {
           
           
-          console.log(' element Mujahid ----------', element);
+          //console.log(' element Mujahid ----------', element);
           //mujahid code here start
           const time30minuts = 10*60*1000; 
           const currentTime=  Date.now();
@@ -386,7 +386,7 @@ function apiRequests() {
           if (remaingTime<time30minuts){
             await MarketIDS.findOneAndUpdate({marketId:element.marketId}, {$set:{ReadyForOdds:true}})
           }
-          console.log("======================-------------- market Name =",element.marketName)
+          //console.log("======================-------------- market Name =",element.marketName)
           if(element.marketName=='Match Odds'){
             console.log('listMarketsByCronJobs is running for marketname: ----------',element.marketName);
           }
