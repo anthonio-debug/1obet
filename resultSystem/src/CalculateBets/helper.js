a1b6461a5b93ec06c39c43eac79a5c0bbebebad0
bhattitayab <bhattitayab747@gmail.com>
2024-10-25 20:38:08 +0400

    fancy tracking

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -37,6 +37,8 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
     console.error('Error: User Not Found');
     return;
   }
+  console.log("userToUpdate----------------------------------------",userToUpdate.availableBalance);
+  console.log("userToUpdate----------------------------------------",userToUpdate);
   const lastMaxWithdraw = await Deposits.findOne({ userId: bet.userId }).sort({ _id: -1 });
     let lastWithdrawalRow_AvailableBalance = lastMaxWithdraw.availableBalance;
     let lastWithdrawalRow_balance = lastMaxWithdraw.balance;
