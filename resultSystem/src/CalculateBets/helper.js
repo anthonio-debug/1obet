7375a1dcb9e8a4b3ba503c7c584a343de2ecb6f9
bhattitayab <bhattitayab747@gmail.com>
2024-10-25 15:20:08 +0400

    expPositive changes in settlements

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -156,6 +156,7 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
 
 
       let expPositive = await expPositive.findOne({ userId:userToUpdate.userId,betId:bet.betId,roundId:bet.marketId });
+      console.log("-------------------------------------parents.............",expPositive);
       if(expPositive){
         await expPositive.updateOne(
           {
@@ -285,6 +286,7 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
 
 
               let expPositive = await expPositive.findOne({ userId:user.userId,betId:bet.betId,roundId:bet.marketId });
+              console.log("-------------------------------------parents.............",expPositive);
       if(expPositive){
         await expPositive.updateOne(
           {
