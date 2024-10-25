27f0ec447af38fe479cf4dd59eb02b3143f99000
bhattitayab <bhattitayab747@gmail.com>
2024-10-25 20:53:16 +0400

    fancy tracking

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -90,7 +90,7 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
 
 
 
-    if(bet.isFancyOrBookMaker==true && fancyData != null){
+    if(bet.isfancyOrbookmaker==true && fancyData != null){
 
       console.log("Winner  11  position for ",winner.runner,"----------------------------------------->",winner.position);
       if (winner.runner == selectionId) {
