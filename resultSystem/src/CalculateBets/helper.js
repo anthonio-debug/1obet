18eb0b97a8b6864819cc0eda91ab4f47487147dc
bhattitayab <bhattitayab747@gmail.com>
2024-10-25 21:00:37 +0400

    fancy tracking

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -66,34 +66,23 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
   var amount = 0
   var winnerRunner = '';
   runnerPosition?.forEach(winner => {
-
-
-
-
-
-
-
-        
-
-
-
+    
     if(bet.isfancyOrbookmaker==true && bet.fancyData != null){
-
-      console.log("Winner  11  position for ",winner.runner,"----------------------------------------->",winner.position);
+      console.log("Winner position for ",winner.runner,"----------------------------------------->",winner.position);
       if (winner.runner == selectionId) {
-        selectedRunnerAmount=winner.position
-        winnerRunner = winner.runner
+          selectedRunnerAmount=winner.position
+          winnerRunner = winner.runner
+      }
     }else{
-      console.log("Winner 22 amount for ",winner.runner,"----------------------------------------->",winner.amount);
+
+      console.log("Winner amount for ",winner.runner,"----------------------------------------->",winner.amount);
       if (winner.runner == selectionId) {
-        selectedRunnerAmount=winner.amount
-        winnerRunner = winner.runner
+          selectedRunnerAmount=winner.amount
+          winnerRunner = winner.runner
+      }
+      
     }
-    }
-    }
-
-
-
+        
     });
 
     console.log("NaN issue with betexposureAmount: ",betexposureAmount);
