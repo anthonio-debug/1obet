838257a6cbec0be2a417fb3bb016e511814cbf41
bhattitayab <bhattitayab747@gmail.com>
2024-10-29 02:59:35 +0400

    figures exposure issue

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4070,7 +4070,7 @@ const countCurrentPosition = await Bets.countDocuments({ betTime: { $lt: thirtyD
   
   
 
-    await expPositive.updateMany({ userId:23022,calculateExp:true }, { $set: { status:0 } });
+    await expPositive.updateMany({ userId:23022,calculateExp:true }, { $set: { status:1 } });
     await Bets.updateMany({ userId:23022,calculateExp:true }, { $set: { status:0 } });
      await User.updateOne({ userId:23020 }, {
       $set: {
