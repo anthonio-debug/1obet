00d05b1109f993dbb20a6ea8d432cdb3bf96f50a
bhattitayab <bhattitayab747@gmail.com>
2024-10-31 22:08:14 +0400

    current position

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -828,7 +828,7 @@ async function getAmountOfWinnerFigures(betId, selectionId) {
       break; // Exit loop if transaction succeeds
     } catch (error) {
       console.log(error,"==============================================================================");
-      if (error && error.hasErrorLabel("TransientTransactionError") && retries < maxRetries) {
+      if ( retries < maxRetries) {
         retries++;
         console.log(`Retrying transaction... attempt ${retries}`);
         continue; // Retry the transaction
