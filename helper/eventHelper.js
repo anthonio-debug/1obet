5a547edebfd9d1cb91b9987d88859faa56002af3
bhattitayab <bhattitayab747@gmail.com>
2024-11-01 19:07:15 +0400

    qadir

--- helper/eventHelper.js
+++ helper/eventHelper.js
@@ -16,7 +16,7 @@ const fetchMarket = async (event) => {
     console.log("eventId=-=-=-=-=-=-=-=-=-= eventId",eventId);
     const marketsData = await listMarketCatalogue(eventId);
     console.log("marketsData=-=-=-=-=-=-=-=-=-= marketsData", marketsData);
-    
+    console.log("marketsData.length....................................................",marketsData.length);
     if (!marketsData.length) return;
 
     let marketStatus = 'PENDING';
