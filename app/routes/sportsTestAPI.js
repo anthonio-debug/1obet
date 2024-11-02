fe9a5572f8f47385f7e49ae3e3724b9047ace2b7
bhattitayab <bhattitayab747@gmail.com>
2024-11-03 03:49:48 +0400

    fancies parent epoxures issu

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4084,7 +4084,7 @@ const countCurrentPosition = await Bets.countDocuments({ betTime: { $lt: thirtyD
     //23279 hierarchy9asmb
     //23281 hierarchy9asmmb
     
-    await Bets.updateMany({ userId:23278,calculateExp:true }, { $set: { status:1 } });
+    await Bets.updateMany({ userId:23281,calculateExp:true }, { $set: { status:1 } });
     await Bets.updateMany({ userId:23279,calculateExp:true }, { $set: { status:1 } });
     await Deposits.deleteMany({
       userId: 23278,
