c828a1e3db720ef07885b3e1f3fbff0608879adb
bhattitayab <bhattitayab747@gmail.com>
2024-11-02 17:00:39 +0400

    dealer balance issue

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -700,9 +700,9 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
       //await session.startTransaction();
       const lastMaxWithdraw = await Cash.findOne({ userId: user.userId }).sort({ _id: -1 });
       //await session.commitTransaction();
-      console.log("hereeeeeeeeeeeeeeeeeeeeeeee",user,"eeeeeeeeeeeeeeeeeeeeeeeeeeeee 1")
-      console.log("hereeeeeeeeeeeeeeeeeeeeeeee",lastMaxWithdraw,"eeeeeeeeeeeeeeeeeeeeeeeeeeeee 1")
-      console.log("amount::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::",amount);
+     // console.log("hereeeeeeeeeeeeeeeeeeeeeeee",user,"eeeeeeeeeeeeeeeeeeeeeeeeeeeee 1")
+     // console.log("hereeeeeeeeeeeeeeeeeeeeeeee",lastMaxWithdraw,"eeeeeeeeeeeeeeeeeeeeeeeeeeeee 1")
+    //  console.log("amount::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::",amount);
       if(user.exposure<=0 && user.availableBalance>=amount && lastMaxWithdraw.availableBalance >=amount && lastMaxWithdraw.availableBalance >0){
         //await session.startTransaction();
        console.log("debit is successully................................................");
@@ -719,7 +719,7 @@ const WinLoseTransManagement = async (balance, payload, users123, action, res, s
         );
         //await session.commitTransaction();
         let parentUsersIds = await getParents(user.userId);
-      console.log("parentUsersIds----------------------------------------------",parentUsersIds);
+      //console.log("parentUsersIds----------------------------------------------",parentUsersIds);
       const parentUser = await User.find({
         userId: {
           $in: [...parentUsersIds]
@@ -1618,7 +1618,7 @@ let newCasinoCall;
       const balance = user.availableBalance / casinoMultiples;
 
 
-      console.log("++===================== going to save data in casinocalls", matchedPayload);
+      
       await WinLoseTransManagement(balance, matchedPayload, user, 0, res);
       // const newCasinoCall = await new CasinoCalls({
       //   transaction_id: matchedPayload.transaction_id,
