95f6176d120f2af6b36ade724b94314c23e6de98
bhattitayab <bhattitayab747@gmail.com>
2024-11-03 04:39:03 +0400

    fancies parent epoxures issu

--- resultSystem/src/CalculateBets/calculations.js
+++ resultSystem/src/CalculateBets/calculations.js
@@ -1496,7 +1496,7 @@ async function handleLosingBetX(bet) {
               }],{session});
 			  
 
-              upMovingAmount = Number((upMovingAmount - (user.commission / 100) * totalRemainingAmount).toFixed(3));
+              //upMovingAmount = Number((upMovingAmount - (user.commission / 100) * totalRemainingAmount).toFixed(3));
 
               if (!config.commissionLessSubMarkets.includes(bet.type) && bet.subMarketId != config.Fancy && bet.subMarketId != config.BookMaker && TotalWin > TotalLose) {
                 const lastMaxWithdraw = await Deposits.findOne({ userId: user.userId }).sort({ _id: -1 });
