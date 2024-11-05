a27014acceaec10e3a6ba81c27b7363e82806626
bhattitayab <bhattitayab747@gmail.com>
2024-11-05 22:55:09 +0400

    console

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4131,9 +4131,9 @@ const { ObjectId } = require('mongodb'); // Make sure to import ObjectId
 // });
 
 
-await Bets.updateMany({ userId:23331 }, { $set: { status:1 ,resultData:'.',resultId:null} });
-await Bets.updateMany({ userId:23332 }, { $set: { status:1 ,resultData:'.',resultId:null} });
-await Bets.updateMany({ userId:23350 }, { $set: { status:1 ,resultData:'.',resultId:null} });
+await Bets.updateMany({ userId:23331 }, { $set: { status:0 } });
+await Bets.updateMany({ userId:23332 }, { $set: { status:0 } });
+await Bets.updateMany({ userId:23350 }, { $set: { status:0 } });
 
 
 
