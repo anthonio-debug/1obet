ad6dfee5fb92fa79954926bc656a2278d9a291b5
bhattitayab <bhattitayab747@gmail.com>
2024-11-06 20:45:45 +0400

    helper

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -524,7 +524,7 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
         await session.commitTransaction();
         break; // Exit loop if transaction succeeds
       } catch (error) {
-        if (error instanceof MongoError && error.hasErrorLabel && error.hasErrorLabel("TransientTransactionError") && retries < maxRetries) {
+        if (error instanceof mongoose && error.hasErrorLabel && error.hasErrorLabel("TransientTransactionError") && retries < maxRetries) {
           retries++;
           console.log(`Retrying transaction... attempt ${retries}`);
           continue; // Retry the transaction
