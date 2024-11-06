4a49ee7faae6f7971d24ba14bf18cd6a687e7712
bhattitayab <bhattitayab747@gmail.com>
2024-11-07 01:24:08 +0400

    parents statement with 2 bettors same market

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -27,7 +27,6 @@ function ToolForResults() {
         {
           $match: {
             sportsId: { $in: targetArray },
-            userId: { $in: [23331,23332,23350] },
             marketId: { $ne: null },
             isfancyOrbookmaker: false,
             status: 1,
@@ -67,12 +66,10 @@ function ToolForResults() {
         if (!result.betDocument) continue;
 
         if (result.betDocument.sportsId === '1' || result.betDocument.sportsId === '2' || result.betDocument.sportsId === '4') {
-          console.log("sports result.betDocument----------------------------------------------",result.betDocument);
+          console.log("result.betDocument----------------------------------------------",result.betDocument);
           await scoreChecker.eventsResult(result.betDocument);
         
         } else if (result.betDocument.sportsId === '7' || result.betDocument.sportsId === '4339') {
-          console.log("races result.betDocument----------------------------------------------",result.betDocument);
-          
           await scoreChecker.racingResult(result.betDocument);
         } else {
           //console.log("Undefined sports type ", result.betDocument);
