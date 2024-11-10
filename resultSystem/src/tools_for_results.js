80040bff32f647c932e0778f418ef529c7f9b38c
bhattitayab <bhattitayab747@gmail.com>
2024-11-10 14:23:54 +0400

    bookmaker not settling issue

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -114,6 +114,7 @@ function ToolForResults() {
         if (betData.fancyData) {
           await scoreChecker.fancyResult(betData, betData.fancyData);
         } else {
+          console.log("book maker..........................",betData);
           await scoreChecker.bookMakerResult(betData);
         }
       }
