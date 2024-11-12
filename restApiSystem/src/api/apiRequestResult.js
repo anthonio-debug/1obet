1eb5e682f643ee322581c8f37016855b07c81fcb
bhattitayab <bhattitayab747@gmail.com>
2024-11-13 02:36:45 +0400

    market status CLOSED on settlement from active bets

--- restApiSystem/src/api/apiRequestResult.js
+++ restApiSystem/src/api/apiRequestResult.js
@@ -92,9 +92,9 @@ function apiRequestResult() {
             await MarketIDs.findOneAndUpdate({_id: market._id}, {$set: {winnerInfo,status:'CLOSED'}});
 
             if (market.marketName === 'Match Odds') {
-              await Events.findOneAndUpdate({Id: market.eventId}, {$set: {winner: winnerInfo, isResultSaved: true,status:'CLOSED'}});
+              await Events.findOneAndUpdate({Id: market.eventId}, {$set: {winner: winnerInfo, isResultSaved: true,inplay:false}});
             } else {
-              await Events.findOneAndUpdate({Id: market.eventId}, {$set: {isResultSaved: true,status:'CLOSED'}});
+              await Events.findOneAndUpdate({Id: market.eventId}, {$set: {isResultSaved: true,inplay:false}});
             }
           }
         }
