5fc9f7d17720ced2c749b538d76a25a8d0723208
bhattitayab <bhattitayab747@gmail.com>
2024-11-13 02:08:56 +0400

    market status CLOSED on settlement from active bets

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2204,6 +2204,7 @@ const saveMarketIDSWinnerRunner = async (req, res) => {
           $set: {
             winnerInfo: req.body.runnerId,
             manuelClose: true,
+            status: 'CLOSED',
             winnerRunnerData: req.body.runnerId
           }
         }
