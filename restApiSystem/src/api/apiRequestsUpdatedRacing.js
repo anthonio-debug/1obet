38b412b63bf7de350185ca6a9e5dc30185ec9034
bhattitayab <bhattitayab747@gmail.com>
2024-11-13 00:46:40 +0400

    CLOSED markets still queried

--- restApiSystem/src/api/apiRequestsUpdatedRacing.js
+++ restApiSystem/src/api/apiRequestsUpdatedRacing.js
@@ -141,7 +141,7 @@ const getRaceMarketIds = async (sportsId) => {
     {
       $match: {
         sportID: Number(sportsId),
-        status: {$in: ['INACTIVE', 'CLOSED', 'OPEN', 'SUSPENDED']},
+        status: {$in: ['INACTIVE', 'OPEN', 'SUSPENDED']},
         openDate: {
           $gte: startTime,
           $lte: endTime
