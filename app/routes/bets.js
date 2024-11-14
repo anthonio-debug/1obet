eea55eb3dbd1fe50cb0531f0882f9a7a9a596a69
bhattitayab <bhattitayab747@gmail.com>
2024-11-14 21:39:29 +0400

    casino calls

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -4124,7 +4124,7 @@ async function getMatchedBets(req, res) {
 
       marketId = req.query.marketId;
       matchId = req.query.id;
-      console.log("market id within body..............::::", marketId);
+      //console.log("market id within body..............::::", marketId);
     }
 
 
@@ -4150,7 +4150,7 @@ async function getMatchedBets(req, res) {
     let marketOpendate;
     if (marketId !== '') {
       const market = await MarketIDS.findOne({ marketId: marketId })
-       console.log("-----------------", market);
+       //console.log("-----------------", market);
       if(market)
       marketOpendate = market.openDate;
     if(market && market.eventId!='')
