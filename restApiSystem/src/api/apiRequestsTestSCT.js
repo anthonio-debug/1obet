7b9fd167f6cf12fe58c0943d9293e5c54397a594
bhattitayab <bhattitayab747@gmail.com>
2024-11-14 19:28:06 +0400

    soccer all markets were saving

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -536,6 +536,11 @@ function apiRequests() {
         });
         //sorting end      
         console.log("I am sorted:::::::::::::::::::::::::::::::", marketIds.length);
+        console.log("I am sorted:::::::::::::::::::::::::::::::", marketIds.length);
+        console.log("I am sorted:::::::::::::::::::::::::::::::", marketIds.length);
+        console.log("I am sorted:::::::::::::::::::::::::::::::", marketIds.length);
+        console.log("I am sorted:::::::::::::::::::::::::::::::", marketIds.length);
+        console.log("I am sorted:::::::::::::::::::::::::::::::", marketIds.length);
         console.log("I am saving these markets...:::::::::::::::::::::::::::::::", marketIds);
 
         sortedarrMarketIds.indexOf("Apple");
