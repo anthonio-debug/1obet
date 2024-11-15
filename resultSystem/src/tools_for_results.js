ae807b32cc06774a0be94a986f05c86f28dcaa89
bhattitayab <bhattitayab747@gmail.com>
2024-11-16 00:50:46 +0400

    delaer statement tracking for figures

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -204,8 +204,7 @@ function ToolForResults() {
         },
         {
           $sort: {
-            'betData.subMarketId': 1,
-            'betData.userId': 1  // Sort by betData.subMarketId in ascending order (use -1 for descending)
+            'betData.subMarketId': 1,  // Sort by betData.subMarketId in ascending order (use -1 for descending)
           }
         }
       ]).exec();
