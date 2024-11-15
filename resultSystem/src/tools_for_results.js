51b33691fcfabe2c95a32e6d552b2994abce3385
bhattitayab <bhattitayab747@gmail.com>
2024-11-15 04:29:12 +0400

    figures and racing settlement

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -23,54 +23,55 @@ function ToolForResults() {
     //console.log("-----------------------------------------------------------");
     const currentTime = new Date().getTime();
     try {
-      const results = await Bets.aggregate(
-        
-        [
-          {
-            $match: {
-              status: 1,
-              calculateExp: true,
-              type: { $in: [2, 3, 4] },
-              betSession: { $ne: null }
-            }
-          },
-          {
-            $lookup: {
-              from: 'sessions',
-              let: { matchId: '$matchId', betSession: '$betSession' },
-              pipeline: [
-                {
-                  $match: {
-                    $expr: {
-                      $and: [{ $eq: ['$Id', '$$matchId'] }, { $eq: ['$sessionNo', '$$betSession'] }]
-                    }
+      const results = await Bets.aggregate([
+        {
+          $match: {
+            status: 1,
+            calculateExp: true,
+            type: { $in: [2, 3, 4] },
+            betSession: { $ne: null }
+          }
+        },
+        {
+          $lookup: {
+            from: 'sessions',
+            let: { matchId: '$matchId', betSession: '$betSession' },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $and: [
+                      { $eq: ['$Id', '$$matchId'] },
+                      { $eq: ['$sessionNo', '$$betSession'] }
+                    ]
                   }
                 }
-              ],
-              as: 'sessionDetails'
-            }
-          },
-          {
-            $unwind: '$sessionDetails'
-          },
-          {
-            $match: {
-              'sessionDetails.score': { $ne: 0 },
-              'sessionDetails.manuelSave': true
-            }
-          },
-          {
-            $project: {
-              betData: '$$ROOT',
-              score: '$sessionDetails.score'
-            }
-          },
-          {
-            $sort:{subMarketId:1}
+              }
+            ],
+            as: 'sessionDetails'
           }
-        ]
-
-      ).exec();
+        },
+        {
+          $unwind: '$sessionDetails'
+        },
+        {
+          $match: {
+            'sessionDetails.score': { $ne: 0 },
+            'sessionDetails.manuelSave': true
+          }
+        },
+        {
+          $project: {
+            betData: '$$ROOT',
+            score: '$sessionDetails.score'
+          }
+        },
+        {
+          $sort: {
+            'betData.subMarketId': 1  // Sort by betData.subMarketId in ascending order (use -1 for descending)
+          }
+        }
+      ]).exec();
       console.log("results.length_{{{{{{{{{{{-------------------------------",results);
       console.log("results.length_{{{{{{{{{{{-------------------------------",results);
       console.log("results.length_{{{{{{{{{{{-------------------------------",results);
