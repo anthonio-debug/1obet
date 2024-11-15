8b5c633ade9d08292d0a41d1e01df0d457c4bf22
bhattitayab <bhattitayab747@gmail.com>
2024-11-16 02:43:55 +0400

    races events issue for USA

--- restApiSystem/src/api/apiRequestsUpdatedRacing.js
+++ restApiSystem/src/api/apiRequestsUpdatedRacing.js
@@ -273,7 +273,7 @@ function apiRequests() {
       return new Date(d).toString() !== "Invalid Date";
     }
 
-    const now = moment();
+    const now = moment()+60*1000*1000;
     const startTime = now.format('YYYY-MM-DDTHH:mm:ss[Z]');
     const endTime = moment(now).add(24, 'hours').format('YYYY-MM-DDTHH:mm:ss[Z]');
 
