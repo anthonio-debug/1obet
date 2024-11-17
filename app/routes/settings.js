66920d9574e52d58f2a52785f7df21fc4c85c666
bhattitayab <bhattitayab747@gmail.com>
2024-11-17 14:29:06 +0400

    home page of bettor

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1361,12 +1361,16 @@ async function bettorDashboardGames2(req, res) {
     console.log("cricketSalt--------------------------------------------------------------------------",cricketSalt);
     const cricket = await Promise.all(
       cricketSalt.map(async (event) => {
+        
         if (event.marketIds && event.marketIds.length > 0) {
-          const marketId = event.marketIds[0].id;
-          console.log("looking for marketid.....................",marketId);
-          console.log("looking for marketid.....................",marketId);
-          console.log("looking for marketid.....................",marketId);
-          console.log("looking for marketid.....................",marketId);
+          //const marketId = event.marketIds[0].id;
+          const matchOddsMarket = await MarketIDS.findOne({ eventId:event.Id,marketName:'Match Odds' }).sort({
+            createdAt: -1
+          });
+          
+          const marketId = matchOddsMarket.marketId;
+
+          console.log("looking for marketid.....................",matchOddsMarket.marketId);
           console.log("looking for event.Id.....................",event.Id);
 
           const oddsData = await Odds.findOne({ marketId: marketId,status:'OPEN' }).sort({
