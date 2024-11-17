10d9dc5d3049056499c9411474bb2af70f425bc8
bhattitayab <bhattitayab747@gmail.com>
2024-11-17 22:36:43 +0400

    increment user id

--- app/routes/user.js
+++ app/routes/user.js
@@ -97,7 +97,7 @@ async function registerUser(req, res) {
       // Update their isDeleted field to true using updateMany()
       await User.updateMany({ userName: userNameLowerNext }, { isDeleted: true });
 
-      var lastUserID = data.userId + 17000;
+      var lastUserID = data.userId + 1;
       console.log("lastUserID--------------------------------",lastUserID);
       if (lastUserID < 1000) {
         lastUserID = 1000;
