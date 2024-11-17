5b2d971ef8d0fe59b78609bf85981d591e5c5a3e
bhattitayab <bhattitayab747@gmail.com>
2024-11-17 12:57:25 +0400

    home page dashboard events

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -792,7 +792,7 @@ function apiRequests() {
                     });
 
                     if (ix !== -1 && tempArray[ix].indexID === 0) {
-                      if(eventId=='33785371'){
+                      if(eventId=='33779987'){
                         console.log("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",el);
                       console.log("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
                       console.log("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
@@ -856,7 +856,7 @@ function apiRequests() {
                       console.log("11111111111111111111111111111111111111111111111111111111111111111111111");
                       console.log("11111111111111111111111111111111111111111111111111111111111111111111111");
                       console.log("11111111111111111111111111111111111111111111111111111111111111111111111");
-                      console.log("11111111111111111111111111111111111111111111111111111111111111111111111");
+                      console.log("11111111111111111111111111111111111111111111111111111111111111111111111",eventId);
                       
                     }
                     io.to('#' + eventId).emit('odds', {
