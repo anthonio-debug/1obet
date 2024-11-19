fac4a88c46c2dcea3f1237354e69e6f6a98ca803
bhattitayab <bhattitayab747@gmail.com>
2024-11-19 21:56:11 +0400

    getexp positives

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -279,7 +279,7 @@ async function findAndProcessTransactions() {
         
 
 
-            await Cash.create([{
+            await Cash.create({
               userId: userRecord.userId,
               description: `Casino (${tran.game_id})`,
               date: new Date().getTime(),
@@ -298,7 +298,7 @@ async function findAndProcessTransactions() {
               roundId: tran._id,
               marketId: tran._id,
               matchId: Cgame_id
-            }], { session });
+            }, { session });
 
 
 
