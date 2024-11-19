090449eb253441b1e0ef8a54d73fb65864c6736c
bhattitayab <bhattitayab747@gmail.com>
2024-11-20 02:02:51 +0400

    winner not highlighting on backend

--- restApiSystem/src/tools_for_result.js
+++ restApiSystem/src/tools_for_result.js
@@ -24,7 +24,7 @@ function ToolForResult() {
     try {
       const eventMarkets = await MarketIDs.find({
         readyForScore: true,
-        status: 'SUSPENDED123',
+        status: 'SUSPENDED',
 
         sportID: { $in: [1, 2, 4] },
         winnerInfo: null
