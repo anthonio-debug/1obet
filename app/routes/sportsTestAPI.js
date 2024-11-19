c55be159dda62ac36be0b0c40cfd462792b98123
bhattitayab <bhattitayab747@gmail.com>
2024-11-19 15:00:47 +0400

    current position manual deletion

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4226,6 +4226,12 @@ const { ObjectId } = require('mongodb'); // Make sure to import ObjectId
     //   description: { $regex: "Event", $options: "i" } // Case-insensitive search for "Casino"
     // })
 
+    
+
+    await CurrentPosition.deleteMany(
+      {matchsId:{$ne:'673a98f1382b84468f1b8219'}}
+    )
+return
     await Deposits.deleteMany({
       userId: 45098,
       description: { $regex: "Event", $options: "i" } // Case-insensitive search for "Casino"
