edf0e9ee42a936100e308a334149b5517fdf55ec
bhattitayab <bhattitayab747@gmail.com>
2024-11-19 07:15:28 +0400

    user exp

--- restApiSystem/src/api/apiRequestsUpdatedRacing.js
+++ restApiSystem/src/api/apiRequestsUpdatedRacing.js
@@ -795,7 +795,7 @@ async function raceOddsJob(marketIds) {
       const requestData = {
         "marketIds": marketIds
       }
-      console.log("market ids for getting race odds...----------------------------",marketIds);
+      //console.log("market ids for getting race odds...----------------------------",marketIds);
       const url = `${config.newThirdURL}/listMarketBook`;
       const response = await axios.post(url, requestData, header);
       const oddsData = response.data.result;
