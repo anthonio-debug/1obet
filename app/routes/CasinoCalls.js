412fe5b776718bd9973dad294fed3b6f8949ef62
bhattitayab <bhattitayab747@gmail.com>
2024-11-19 04:21:49 +0400

    casino issues

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -104,7 +104,7 @@ async function findAndProcessTransactions() {
 
      
       
-      //console.log("groupedTransactions================",groupedTransactions.length,"=============================",groupedTransactions);
+      console.log("groupedTransactions================",groupedTransactions.length,"=============================",groupedTransactions);
       
       if (!groupedTransactions || groupedTransactions.length === 0) {
        // console.log('No transactions found for the given round_id and username.');
