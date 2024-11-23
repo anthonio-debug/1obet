f6e022a5ce7961314fa39e77e355dadb4025f68d
bhattitayab <bhattitayab747@gmail.com>
2024-11-23 22:24:50 +0400

    status of markets closed to be deleted

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4174,11 +4174,14 @@ let userIdcas = 45141;
       $or: [
         { status: 'CLOSED', sportID: 4339 },
         { status: 'CLOSED', sportID: 7 },
+        { status: 'CLOSED', sportID: 1 },
+        { status: 'CLOSED', sportID: 2 },
+        { status: 'CLOSED', sportID: 4 }
 
      ]
     });
-    await MarketIDS.updateMany({ sportID: 4339 }, { $set: { status: 'CLOSED' } });
-    await MarketIDS.updateMany({ sportID: 7 }, { $set: { status: 'CLOSED' } });
+    // await MarketIDS.updateMany({ sportID: 4339 }, { $set: { status: 'CLOSED' } });
+    // await MarketIDS.updateMany({ sportID: 7 }, { $set: { status: 'CLOSED' } });
 
 
     const count = 0;
