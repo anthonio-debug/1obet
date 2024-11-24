e11de0550bc7a5169f1fdc5c9f3eb85d23146137
bhattitayab <bhattitayab747@gmail.com>
2024-11-25 03:58:15 +0400

    market open date issue

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -667,13 +667,14 @@ function apiRequests() {
 
         if (oddsData.length > 0) {
           //console.log(oddsData.length);
-          console.log("API returned================================>>",element.marketId,">>>>>>>>>",oddsData.length);
+          console.log("API returned================================>>>>>>>>>>>",oddsData.length);
         
           let counter = 0;
           try {
             for (let index = 0; index < oddsData.length; index++) {
               counter = counter + 1;
               const element = oddsData[index];
+              console.log("element.marketId=================================================",element.marketId);
               if( element.marketId=='1.235859242'){
                 console.log("oddsdata..............................",element);
                 console.log("element.runners-----------------",element.runners);
