12b2bcdde35d462efb709c269b52793c9bf1630e
bhattitayab <bhattitayab747@gmail.com>
2024-11-24 20:44:45 +0400

    socket issue

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -57,14 +57,14 @@ const checkMarketBlocked = async (user) => {
 }
 
 async function removeClosedMkts() { 
-  console.log("---------------------------------");
+  //console.log("---------------------------------");
   const twoMinutesAgo = Date.now() - 2 * 60 * 1000;
     //const ghclosedMkts = await MarketIDS.find({ sportID:{$in:[7,4339]},status: 'CLOSED', openDate:{$lt:twoMinutesAgo} })
     const ghclosedMkts = await MarketIDS.find({status: 'CLOSED', openDate:{$lt:twoMinutesAgo} })
 
      ghclosedMkts &&
      ( ghclosedMkts.forEach(async (market) => {
-      console.log("market.marketId........................---------------------------",market.marketId);
+      //console.log("market.marketId........................---------------------------",market.marketId);
       let ghcountghbetsCount = await Bets.countDocuments({ marketId:market.marketId,status:1 })
       
       if(!ghcountghbetsCount){
