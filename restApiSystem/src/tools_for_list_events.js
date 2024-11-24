6b8b162d50f6e461ac8c314f76e7c2472a111f67
bhattitayab <bhattitayab747@gmail.com>
2024-11-24 11:51:50 +0400

    status of markets closed to be deleted

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -322,7 +322,7 @@ async function updateOddsFormLimitless() {
             ReadyForOdds:true,
             status: { $in: ['INACTIVE', 'OPEN', 'SUSPENDED'] },
             marketName: { $ne: 'Bookmaker' },
-            // openDate: {$gte: startTime, $lte: endTime},
+             openDate: {$gte: startTime, $lte: endTime},
             $or: [{ sportID: 1 }, { sportID: 2 }, { sportID: 4 }]
           }
         },
