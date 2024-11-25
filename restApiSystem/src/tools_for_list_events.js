76898c33744e733a7e9f9866d57721da5c0988f1
bhattitayab <bhattitayab747@gmail.com>
2024-11-25 16:35:56 +0400

    cron job issue

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -323,7 +323,7 @@ async function updateOddsFormLimitless() {
                 ReadyForOdds: true,
                 status: { $in: ['INACTIVE', 'OPEN', 'SUSPENDED'] },
                 marketName: { $ne: 'Bookmaker' },
-                $or: [{ sportID: 1 }]
+                $or: [{ sportID: 4 }]
             }
         },
         {
