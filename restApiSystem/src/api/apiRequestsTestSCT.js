1e0002dceb94e8965bdaddce910470130c4746c3
bhattitayab <bhattitayab747@gmail.com>
2024-11-25 08:08:55 +0400

    odds stuck problem

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -918,7 +918,7 @@ function apiRequests() {
                     const ix = _.findIndex(tempArray, function (o) {
                       return o.market == marketId;
                     });
-                    clearInterval(intervalId);
+                    //clearInterval(intervalId);
                     if (ix !== -1 && tempArray[ix].indexID === 0) {
                       
                       //if(marketData.eventId=='33771961' && element.marketId=='1.235859242'){
