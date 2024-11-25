34ad014c38496a5cf970b1ffabb8358d1e94d332
bhattitayab <bhattitayab747@gmail.com>
2024-11-25 16:27:51 +0400

    axous timeout

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -660,7 +660,7 @@ function apiRequests() {
     console.log("1-----------",requestData);
     const url = `${config.newThirdURL}/listMarketBook`;
 
-    axios.post(url, {timeout: 2000},requestData, header).then(
+    axios.post(url, {timeout: 2500},requestData, header).then(
       async (response) => {
 
         if (!response?.data?.result) return;
