585c2688de8de851f3d6e65f3e47fadaa4a84043
bhattitayab <bhattitayab747@gmail.com>
2024-11-25 17:39:12 +0400

    cron

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -895,7 +895,7 @@ function apiRequests() {
                       const numericDateTime = `${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}${now.getSeconds().toString().padStart(2, '0')}`;
                      
                         try{
-                        await MarketIDS.updateOne({ marketId: marketId, runners: null }, { $set: { updatedAt:numericDateTime,runners: runners } },{ session });
+                        await MarketIDS.updateOne({ marketId: marketId, runners: null }, { $set: { updatedAt:numericDateTime,runners: runners } });
                         }catch (error) {
                           console.error('Error emitting odds data:', error);
                       }
