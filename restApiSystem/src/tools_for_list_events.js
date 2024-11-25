fc4a188dfda1525544aa1a30ce360be6d7dc1e48
bhattitayab <bhattitayab747@gmail.com>
2024-11-25 23:38:49 +0400

    tmieout

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -55,7 +55,7 @@ function ToolForEvent() {
 
       const intervalId = setInterval(() => {
         fetchOdds(true, intervalId);
-      }, 12000);
+      }, 8000);
 
       setInterval(() => {
         setBrokenRecord();
