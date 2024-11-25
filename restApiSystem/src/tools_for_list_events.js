675cbd17ebda542e5fe99ee63ae6faa1bb012ebd
bhattitayab <bhattitayab747@gmail.com>
2024-11-25 06:53:45 +0400

    odds stuck problem

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -55,7 +55,7 @@ function ToolForEvent() {
 
       const intervalId = setInterval(() => {
         fetchOdds(true, intervalId);
-      }, 1500);
+      }, 2000);
 
       setInterval(() => {
         setBrokenRecord();
