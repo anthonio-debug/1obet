96d79f48c369d2f4e3cf93d2cc21a7da94ec9339
bhattitayab <bhattitayab747@gmail.com>
2024-11-26 04:17:48 +0400

    cron

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -675,7 +675,7 @@ function apiRequests() {
 
         if (!response?.data?.result) return;
         const oddsData = response.data.result;
-        console.log("response.data.result------------------------------->>",oddsData);
+        //console.log("response.data.result------------------------------->>",oddsData);
         let checkedMarkets = [];
 
         if (oddsData.length > 0) {
@@ -693,10 +693,10 @@ function apiRequests() {
               const element = oddsData[index];
               console.log("element.marketId=================================================",element.marketId);
               //if( element.marketId=='1.235859242'){
-                console.log("oddsdata..............................",element);
-                console.log("element.runners-----------------",element.runners);
+               // console.log("oddsdata..............................",element);
+                //console.log("element.runners-----------------",element.runners);
               //}
-              console.log("element..............................",element);
+              //console.log("element..............................",element);
 
 
 
