21e3ca6b4700db90f3aba45aef847acc0509d381
bhattitayab <bhattitayab747@gmail.com>
2024-11-26 04:19:54 +0400

    cron

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -1005,7 +1005,7 @@ function apiRequests() {
               
             }
             console.log("-------I reached outside.....................");
-            console.log("item.market--=====================>>>>",item.market);
+            console.log("item.market--=====================>>>>",marketId);
             const filteredArray = tempArray.filter((item) => !checkedMarkets.includes(item.market));
             console.log("-------I reached outside after.....................");
             console.log("filteredArray...........",filteredArray);
