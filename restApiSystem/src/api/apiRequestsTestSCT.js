20b34b1cf2949ec131eeb1e243977d855992af4c
bhattitayab <bhattitayab747@gmail.com>
2024-11-26 09:47:13 +0400

    cron

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -646,12 +646,12 @@ function apiRequests() {
       console.error('Error getting settings:', error);
      }
       
-     //console.log(Settings1);
-    if(Settings1 && Settings1.settingValue=='1'){
-      return
-    }
+     console.log(Settings1);
+    // if(Settings1 && Settings1.settingValue=='1'){
+    //   return
+    // }
 
-    await Settings.findOneAndUpdate({settingKey: 'IsJobRunning'}, {$set:{settingValue:'1'}})
+    // await Settings.findOneAndUpdate({settingKey: 'IsJobRunning'}, {$set:{settingValue:'1'}})
 
     //if(iterate==0) {}else{return}
     let tempArray = [];
