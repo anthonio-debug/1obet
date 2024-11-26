0f949b33a050b94dab900d6529f551351ffcc4d5
bhattitayab <bhattitayab747@gmail.com>
2024-11-26 21:03:16 +0400

    cron removed consoles and set clearinterval

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -384,7 +384,7 @@ async function updateOddsFormLimitless() {
         
 
         const session = await mongoose.startSession();
-
+        session.endSession();
         try{
           Settings1 = await Settings.findOne({ settingKey: 'IsJobRunning',settingValue:'1' })
         }catch (error) {
@@ -424,7 +424,7 @@ async function updateOddsFormLimitless() {
           } else {
             console.error('Transaction Error getodds:', error);
             await session.abortTransaction();
-            session.endSession();
+            
             break; // Exit loop if error is not transient
           }
         } finally {
