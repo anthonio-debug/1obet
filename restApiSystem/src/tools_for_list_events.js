a67c1f3e56c57ce06cda95a48161cb7ca0e11996
bhattitayab <bhattitayab747@gmail.com>
2024-11-26 08:13:12 +0400

    cron

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -315,7 +315,7 @@ async function updateOddsFormLimitless() {
       const now = moment().utc(); // Get the current time in UTC
       const startTime = moment(now).subtract(8000, 'minutes').valueOf(); // Get the timestamp in minutes
       const endTime = moment(now).add(1000, 'minutes').valueOf(); // Add 5 hours and get the timestamp in minutes
-
+      console.log("fffffffffffffffffffffffffff");
       //openDate: {$gte: startTime, $lte: endTime},
       const documents = await MarketIDs.aggregate([ 
         {
