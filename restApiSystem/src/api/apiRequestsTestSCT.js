294ad824631e50fbb2596314c631e5547f799806
bhattitayab <bhattitayab747@gmail.com>
2024-11-26 16:05:31 +0400

    cron removed consoles and set clearinterval

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -1000,7 +1000,7 @@ function apiRequests() {
                   } catch (error) {
                       console.error('Error emitting odds data  for trading:', error);
                   }   
-                  console.log("-------11");           
+                  //console.log("-------11");           
                   //clearInterval(intervalId);
 
 
