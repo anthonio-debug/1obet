af7a12c2db666cf6ccae80ad4565200c48fbf9c5
bhattitayab <bhattitayab747@gmail.com>
2024-11-29 06:18:19 +0400

    auto balance deduction

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -1570,7 +1570,7 @@ const insertMissingTransactions = async (req, res) => {
         {
           $match: {
             action: { $in: ["debit", "credit","rollback"] },
-            isUsed:0
+            //isUsed:0
             //username:"user_45112"// Filter for action being "debit" or "credit"
           }
         },
@@ -1893,7 +1893,7 @@ const insertMissingTransactions = async (req, res) => {
                 }
     
                 if (user.exposure <= 0 && user.availableBalance >= amount && lastMaxWithdraw.availableBalance >= amount && lastMaxWithdraw.availableBalance > 0 && !idExists2) {
-                    if (matchedPayload.action == 'debit' && matchedPayload.isUsed ==0) {
+                    if (matchedPayload.action == 'debit') {
                         try {
 
                           console.log("UpdatedExposure/////////////////////////////////////",UpdatedExposure);
@@ -1928,9 +1928,8 @@ const insertMissingTransactions = async (req, res) => {
                             );
                             
                             // Mark transaction as used
-                            await CasinoCallsPayload.updateOne(
+                            await CasinoCallsPayload.deleteOne(
                                 { transaction_id: transactionId2 },
-                                { $set: { isUsed: 1 } },
                                 { session }
                             );
     
