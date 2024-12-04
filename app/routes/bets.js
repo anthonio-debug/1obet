93c396005d20594086b65513f3993fe53e711b2e
bhattitayab <bhattitayab747@gmail.com>
2024-12-04 04:17:14 +0400

    cricket match odds bet place change

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -536,13 +536,11 @@ function checkMultiResponseCricketOdds(odds, rates,selectedBetRate,type) {
     let ratesOppositeshuffle = []
     
     if (type === 0) {
-      ratesOppositeshuffle = rates.filter(rate => rate > selectedBetRate);
       rates = rates.filter(rate => rate <= selectedBetRate);
-      
+      ratesOppositeshuffle = rates.filter(rate => rate > selectedBetRate);
     } else if (type === 1) {
-      ratesOppositeshuffle = rates.filter(rate => rate < selectedBetRate);
       rates = rates.filter(rate => rate >= selectedBetRate);
-      
+      ratesOppositeshuffle = rates.filter(rate => rate < selectedBetRate);
     }
     console.log("ratesOppositeshuffle------------------>>>>",ratesOppositeshuffle);
     let lastValOfRates = 0
