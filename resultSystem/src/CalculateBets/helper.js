4296f2a0b79cfd3c65df564e1e4feb9e7f177db6
bhattitayab <bhattitayab747@gmail.com>
2024-12-13 06:06:11 +0400

    settlign races issue

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -30,7 +30,7 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
     const mongoose = require('mongoose');
     
       const session = await mongoose.startSession();
-      session.endSession();
+      
       const maxRetries = 3; // Max retries for the transaction
       let retries = 0;
       while (retries < maxRetries) {
