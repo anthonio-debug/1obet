845b545dcca1c92c25a363ae6fc3da37643b7fd2
bhattitayab <bhattitayab747@gmail.com>
2024-12-15 21:38:23 +0400

    current position values with bettor check

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -843,7 +843,7 @@ async function insertOrUpdateCurrentPosition(data) {
           event: data.event
         },
         $set: {
-          runnersPosition: data.runnersPosition // Always update this field if found
+          amount: data.amount,runnersPosition: data.runnersPosition // Always update this field if found
         }
       },
       // Upsert option to insert if no matching document
