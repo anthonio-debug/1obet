2fd80c3e60060bf7f1f9f65583272b62ea74b980
bhattitayab <bhattitayab747@gmail.com>
2024-12-15 04:37:21 +0400

    current position new

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -547,7 +547,7 @@ async function saveCurrentPosition(userId,finalShareAmountInLoss,bet,userCommiss
                  }
                 }
                 const marketsData =  MarketIDS.find({ marketId:marketId,"runners.SelectionId": position.runner });
-                console.log("marketsData----------------------------",marketsData);
+                console.log("marketsData----------------------------",marketsData.runners);
                 console.log("marketsData.runners[0].runnerName----------------------------",marketsData.runners[0].runnerName);
                 runnersPosition.push({
                       runner:position.runner,
