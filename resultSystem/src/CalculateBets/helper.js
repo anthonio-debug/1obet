474128138da490959b339513cb631c258b8fe267
bhattitayab <bhattitayab747@gmail.com>
2024-12-16 17:27:47 +0400

    exclude settlements for specific traders

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -20,7 +20,7 @@ async function getAmountOfWinnerTemp(betId, selectionId) {
    //selectionId = selectionId.replace(/\s/g, '');
 
    if(betId.marketId=='1.236890568'){
-   console.log("|||||||||||||||||",betId.userId,"||||||||||||||||||||||||||||||||||||||||||",selectionId);
+   console.log("|||||||||||||||||",betId.userId,"|||||||||||||||||||||||",betId.marketId,"|||||||||||||||||||",selectionId);
     selectionId = '5728225';
     console.log("|||||||||||||||",betId.userId,"||||||||||||AFTER||||||||||||||||||||||||||||||||",selectionId);
    }
