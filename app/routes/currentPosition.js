8ace546be3c60801c08c135bd8e00477f9e6c2dd
keepsimple7 <keepsimple95@outlook.com>
2024-12-19 19:33:16 +0800

    fix: compare value

--- app/routes/currentPosition.js
+++ app/routes/currentPosition.js
@@ -362,10 +362,10 @@ const getCurrentPosition2 = async (req, res) => {
         };
         res.send(response);
       } else {
-        for (const item of currentPositionData) {
+        for (const [index, item] of currentPositionData.entries()) {
           for (const data2 of currentPositionData2) {
             if (data2?.matchsId === item.matchId && data2?.marketId === item.marketId && data2?.subMarketId === item.subMarketId) {
-              item.position2Amount = data2.amount
+              currentPositionData[index].position2Amount = data2.amount
               continue
             }
           }
