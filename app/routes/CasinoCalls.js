5ce1e5c1164d46c2672624f10847debdbe452ca4
bhattitayab <bhattitayab747@gmail.com>
2024-12-27 01:35:46 +0400

    casino calls duplicate debits

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -1017,20 +1017,19 @@ async function  casino (req, res) {
     return res.send({ status: '400', msg: 'Invalid Request' });
   }
   const payload1 = req.query
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
   
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
   
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
-  console.log("payload1.transaction_id-----------------------",payload1.transaction_id);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
+  console.log("payload1-----------------------",payload1);
   const c = await new CasinoCallsPayload(payload1)
   
   //console.log("c.........................",c);
