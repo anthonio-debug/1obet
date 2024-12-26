3c714d2905079720f06b2041a723c85c147927a6
bhattitayab <bhattitayab747@gmail.com>
2024-12-26 16:14:19 +0400

    allowed from local host

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4166,7 +4166,7 @@ async function deleteOdds(req, res) {
   const bodyArray = Object.entries(req.body).map(([key, value]) => ({ [key]: value }));
 
      await MarketIDS.deleteMany({status:'ABANDONED'});
-     await MarketIDS.deleteMany({status:'CLOSED'});
+     //await MarketIDS.deleteMany({status:'CLOSED'});
      await MarketIDS.deleteMany({status:'PASSED-THROUGH'});
      await InPlayEvents.deleteMany({status:'CLOSED-EVENTLIST'});
 
