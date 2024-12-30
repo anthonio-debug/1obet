2dbfddb1d273e7ae769d48062e5b32bd2fba1b37
bhattitayab <bhattitayab747@gmail.com>
2024-12-31 03:56:04 +0400

    current position remove previous runnersposition

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -611,7 +611,7 @@ async function saveCurrentPosition(userId, finalShareAmountInLoss, bet, userComm
     const summarizedResults = await RunnerWiselossShares.aggregate([
       {
         $group: {
-          _id: { dealerId: "$dealerId", marketId: "$marketId",subMarketId: "$subMarketId",betSession: "$betSession", runner: "$runner" },
+          _id: { dealerId: "$dealerId", marketId: "$marketId",subMarketId: bet.subMarketId,betSession: bet.betSession, runner: "$runner" },
           totalAmount: { $sum: "$amount" }
         }
       }
