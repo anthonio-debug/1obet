eec547cbd33e55684717845acb3e3f85c68e1f26
bhattitayab <bhattitayab747@gmail.com>
2024-12-30 05:22:49 +0400

    current position savecurrentposition function changes

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -476,7 +476,7 @@ async function saveCurrentPosition(userId, finalShareAmountInLoss, bet, userComm
       subMarketId:bet.subMarketId,
       betSession:bet.betSession}).sort({ _id: -1 }).limit(1);
     console.log("prevCurrentPosition2----",prevCurrentPosition2);
-    if(prevCurrentPosition2 ){
+    if(prevCurrentPosition2.length >0 ){
       const prevBet = await Bets.find({
         marketId: bet.marketId,
         userId: bet.userId,
