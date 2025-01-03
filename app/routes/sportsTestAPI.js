7ec868bea960d978e9a893f5ae2cff1baf857b94
bhattitayab <bhattitayab747@gmail.com>
2025-01-04 01:59:49 +0400

    remove inplay closed list

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4162,32 +4162,32 @@ async function saveCurrentPosition(req, res) {
 async function deleteOdds(req, res) {
   const eventId = req.params.eventId;
   
-   const url = 'https://stageapi.worldcasinoonline.com/api/games';
+  //  const url = 'https://stageapi.worldcasinoonline.com/api/games';
   
-  const data = {
-      partnerKey: "uGT24/SXjsKcwBLu9iFoC43mX102ggFcH+KNWM9FITuSXHMEO44AkWBuJ+paSRCLz9W1sIxdHiQ=",
-      providerCode: null  // Use null instead of "SN"
-  };
+  // const data = {
+  //     partnerKey: "uGT24/SXjsKcwBLu9iFoC43mX102ggFcH+KNWM9FITuSXHMEO44AkWBuJ+paSRCLz9W1sIxdHiQ=",
+  //     providerCode: null  // Use null instead of "SN"
+  // };
   
-  const headers = {
-      'Content-Type': 'application/json'
-  };
+  // const headers = {
+  //     'Content-Type': 'application/json'
+  // };
   
-  const timeoutDuration = 30000;  // 30 seconds timeout
+  // const timeoutDuration = 30000;  // 30 seconds timeout
   
-  axios.post(url, data, { headers, timeout: timeoutDuration })
-      .then(response => {
-          console.log('Products available Response:', response.data);
-      })
-      .catch(error => {
-          if (error.response) {
-              console.error('Server responded with error:', error.response.data);
-          } else if (error.request) {
-              console.error('No response received:', error.request);
-          } else {
-              console.error('Error setting up the request:', error.message);
-          }
-      });
+  // axios.post(url, data, { headers, timeout: timeoutDuration })
+  //     .then(response => {
+  //         console.log('Products available Response:', response.data);
+  //     })
+  //     .catch(error => {
+  //         if (error.response) {
+  //             console.error('Server responded with error:', error.response.data);
+  //         } else if (error.request) {
+  //             console.error('No response received:', error.request);
+  //         } else {
+  //             console.error('Error setting up the request:', error.message);
+  //         }
+  //     });
 
 
   
