66af82c1b70d09ee1c1f56916faafcc91c1ea95f
bhattitayab <bhattitayab747@gmail.com>
2025-01-03 05:46:13 +0400

    casino calls few changes

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -1125,32 +1125,19 @@ async function  debitO (req, res) {
 
 
 async function  getGamesByProviderName (req, res) {
-  const url = 'https://stageapi.worldcasinoonline.com/api/games';
+  const url = 'https://stageapi.worldcasinoonline.com/api/games?partnerKey=uGT24/SXjsKcwBLu9iFoC43mX102ggFcH+KNWM9FITuSXHMEO44AkWBuJ+paSRCLz9W1sIxdHiQ=&providerCode=null';
 
-const data = {
-    partnerKey: "uGT24/SXjsKcwBLu9iFoC43mX102ggFcH+KNWM9FITuSXHMEO44AkWBuJ+paSRCLz9W1sIxdHiQ=",
-    providerCode: null  // Use null instead of "SN"
-};
 
-const headers = {
-    'Content-Type': 'application/json'
-};
 
-const timeoutDuration = 30000;  // 30 seconds timeout
+const response = await axios.get(url);
 
-axios.post(url, data, { headers, timeout: timeoutDuration })
-    .then(response => {
-        console.log('Products available Response:', response.data);
-    })
-    .catch(error => {
-        if (error.response) {
-            console.error('Server responded with error:', error.response.data);
-        } else if (error.request) {
-            console.error('No response received:', error.request);
-        } else {
-            console.error('Error setting up the request:', error.message);
-        }
-    });
+return res.json({
+  success: true,
+  message: 'getting Data Found successfully',
+  results: response
+});
+
+
 
     
 }
