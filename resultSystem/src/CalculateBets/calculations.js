1d174a602d1711ffe835a7f2cadb2a3dd6a4bbb2
bhattitayab <bhattitayab747@gmail.com>
2025-01-05 07:47:42 +0400

    tempexposure for fancies plus issue

--- resultSystem/src/CalculateBets/calculations.js
+++ resultSystem/src/CalculateBets/calculations.js
@@ -1126,7 +1126,7 @@ async function handleWinningBetX(bet, winner) {
             for (const user of parentUser) {
               let expPositiveDataP;
               expPositiveDataP = await expPositive.findOne({ userId:user.userId,betId:bet._id.toString() ,calculateExp:true }).sort({ _id: -1 });
-              if(expPositiveDataP && expPositiveDataP.calculateExp==true && expPositiveDataP.isUsed===0){
+              if(expPositiveDataP && expPositiveDataP.calculateExp===true && expPositiveDataP.isUsed===0){
               const totalExpoisure = Number((user.exposure + Number(((user.commission / 100) * totalRemainingAmount))));
               const totalBalance = Number((user.balance - Number(((user.commission / 100) * remainingAmount))));
               const totalavailableBalance = Number((user.availableBalance + Number(((user.commission / 100) * commissionAmount))));
@@ -1171,7 +1171,7 @@ async function handleWinningBetX(bet, winner) {
                 let UpdatedExposureAmount2 = user.exposure + winningsShareAmount2;
                 
                 
-              if(expPositiveDataP.calculateExp==true && expPositiveDataP.isUsed===0){
+              if(expPositiveDataP.calculateExp===true && expPositiveDataP.isUsed===0){
                 
                 let updatetempExposure = user.tempExposure + Math.abs(expPositiveDataP.expCaptured)
                 console.log("expPositiveDataP._id::::",expPositiveDataP._id,"isUsed:::::",expPositiveDataP.isUsed);
@@ -1598,7 +1598,7 @@ async function handleLosingBetX(bet) {
               let expPositiveDataP;
               expPositiveDataP = await expPositive.findOne({ userId:user.userId,betId:bet._id.toString() ,calculateExp:true }).sort({ _id: -1 });
             
-              if(expPositiveDataP && expPositiveDataP.calculateExp==true  && expPositiveDataP.isUsed===0){
+              if(expPositiveDataP && expPositiveDataP.calculateExp===true  && expPositiveDataP.isUsed===0){
 
               const totalExpoisure = Number((user.exposure + Number(((user.commission / 100) * remainingAmount))));
               const totalavailableBalance = Number((user.availableBalance + Number(((user.commission / 100) * remainingAmount + (user.commission / 100) * TotalLoosingAmount))));
@@ -1650,7 +1650,7 @@ async function handleLosingBetX(bet) {
               }
               
               
-              if(expPositiveDataP.calculateExp==true && expPositiveDataP.isUsed===0){
+              if(expPositiveDataP.calculateExp===true && expPositiveDataP.isUsed===0){
                 
                 let updatetempExposure = user.tempExposure + Math.abs(expPositiveDataP.expCaptured)
                 console.log("expPositiveDataP._id::::",expPositiveDataP._id,"isUsed:::::",expPositiveDataP.isUsed);
