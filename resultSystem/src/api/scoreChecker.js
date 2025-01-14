4ab7bcbc0e3eb141d69e210dc438f4609ebc24d7
bhattitayab <bhattitayab747@gmail.com>
2025-01-15 03:09:45 +0400

    fancies new setup created by

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -671,6 +671,7 @@ function scoreChecker() {
             if(newBetUser.createdBy==46205 || newBetUser.createdBy==46206){
 
               await handleWinningBetXX(bet);
+              
 
             }else{
               if (bet.type == 0) {
