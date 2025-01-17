5005dd2de44269249b059f08d61ee0c6fc76e1ba
bhattitayab <bhattitayab747@gmail.com>
2025-01-17 07:05:01 +0400

    allowed users

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -669,7 +669,7 @@ function scoreChecker() {
               { userId: bet.userId }
             );
             console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-            if(newBetUser.createdBy==46241 || newBetUser.createdBy==46243 || newBetUser.createdBy==46230 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
+            if(newBetUser.createdBy==46249 || newBetUser.createdBy==46250 || newBetUser.createdBy==46230 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
 
               await handleWinningBetXX(bet);
 
