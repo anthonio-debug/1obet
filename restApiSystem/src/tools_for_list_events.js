fb061f17f9b6642e9cbe378879a51b639f630204
bhattitayab <bhattitayab747@gmail.com>
2025-01-17 04:12:36 +0400

    speed of score api

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -34,9 +34,9 @@ function ToolForEvent() {
       setInterval(() => {
         findAndProcessTransactions()
       },1000)
-      setInterval(() => {
-        callBothApis()
-      },1000)
+      // setInterval(() => {
+      //   callBothApis()
+      // },1000)
       setInterval(() => {
         removeClosedMkts()
       },50000)
