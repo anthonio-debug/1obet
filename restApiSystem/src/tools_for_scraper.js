a4d46b3b93c758de077761dc61fc5cd86e2dcfdf
bhattitayab <bhattitayab747@gmail.com>
2025-01-17 04:23:40 +0400

    speed of score api

--- restApiSystem/src/tools_for_scraper.js
+++ restApiSystem/src/tools_for_scraper.js
@@ -28,7 +28,8 @@ async function callBothApis() {
   await scraper.fetchCricketScoreFromScoreApi(); // Call the second function
 }
 function ToolForScraper() {
-  return { init , fetchCricketScoreFromApi,fetchCricketScoreFromScoreApi};
+  //return { init , fetchCricketScoreFromApi,fetchCricketScoreFromScoreApi};
+  return { init};
 
   async function init(_io, express) {
     io = _io;
