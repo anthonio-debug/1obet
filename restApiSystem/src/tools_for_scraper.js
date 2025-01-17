f94750f50030be68dcf0410f4248bbe66668a281
bhattitayab <bhattitayab747@gmail.com>
2025-01-17 05:14:29 +0400

    speed of score api

--- restApiSystem/src/tools_for_scraper.js
+++ restApiSystem/src/tools_for_scraper.js
@@ -28,8 +28,8 @@ async function callBothApis() {
   await scraper.fetchCricketScoreFromScoreApi(); // Call the second function
 }
 function ToolForScraper() {
- // return { init , fetchCricketScoreFromApi,fetchCricketScoreFromScoreApi};
-  return { init};
+  return { init , fetchCricketScoreFromApi,fetchCricketScoreFromScoreApi};
+  //return { init};
 
   async function init(_io, express) {
     io = _io;
@@ -178,7 +178,7 @@ function ToolForScraper() {
           }else{
             console.log("!isObjectEqual(activeCrickets.get(eventId), apiCricketScore)")
           }
-          if (!activeCrickets.has(eventId) || !isObjectEqual(activeCrickets.get(eventId), apiCricketScore)) {
+          //if (!activeCrickets.has(eventId) || !isObjectEqual(activeCrickets.get(eventId), apiCricketScore)) {
             console.log("1111111111111111111111111111111111111");
             activeCrickets.set(eventId, apiCricketScore);
             const cricketScore = await Crickets.findOneAndUpdate({ eventId: apiCricketScore.eventId }, apiCricketScore, { upsert: true, new: true, setDefaultsOnInsert: true });
@@ -257,7 +257,7 @@ function ToolForScraper() {
 
               io.emit('cricket_score_api', {...frontScore, figureCurrentPositionData2, cbCurrentPositionData2, jkCurrentPositionData2, sessionNo});
             }
-          }
+          //}
         }
       }
     } catch (error) {
@@ -270,4 +270,4 @@ function ToolForScraper() {
 }
 
 // module.exports = {callBothApis,ToolForScraper};
-module.exports = ToolForScraper;
+module.exports = callBothApis;
