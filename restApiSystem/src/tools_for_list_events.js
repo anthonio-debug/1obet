16458af77ece53dbc2508dfbf606236cfb23a318
bhattitayab <bhattitayab747@gmail.com>
2025-01-20 03:58:46 +0400

    score api speed

--- restApiSystem/src/tools_for_list_events.js
+++ restApiSystem/src/tools_for_list_events.js
@@ -36,7 +36,7 @@ function ToolForEvent() {
       },1000)
       setInterval(() => {
         callBothApis()
-      },1000)
+      },2000)
       setInterval(() => {
         removeClosedMkts()
       },50000)
