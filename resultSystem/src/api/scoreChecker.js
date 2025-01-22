72eaab4a48a79ecca04b33977d30b4f407310806
bhattitayab <bhattitayab747@gmail.com>
2025-01-22 16:29:39 +0400

    Aura casino authentication

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -1319,7 +1319,7 @@ function scoreChecker() {
 
 
         const newBetUser = await User.findOne(
-          { userId: bet.userId }
+          { userId: bet.betData.userId }
         );
         console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
         if(newBetUser.createdBy==46279 || newBetUser.createdBy==46278 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
@@ -1369,7 +1369,7 @@ function scoreChecker() {
         
         
         const newBetUser = await User.findOne(
-          { userId: bet.userId }
+          { userId: bet.betData.userId }
         );
         console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
         if(newBetUser.createdBy==46279 || newBetUser.createdBy==46278 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
