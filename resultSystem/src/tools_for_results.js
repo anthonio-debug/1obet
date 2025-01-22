2a6fd6981513d11d2e489063c0ff047b7433af12
bhattitayab <bhattitayab747@gmail.com>
2025-01-22 04:13:09 +0400

    parent settlment function

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -115,7 +115,7 @@ function ToolForResults() {
         ).catch((e) => console.error(e));
        // console.log("------------------------------------------------------------------------",betData);
         if (betData.fancyData) {
-          console.log("betData going to fancyResult----",betData);
+         // console.log("betData going to fancyResult----",betData);
           await scoreChecker.fancyResult(betData, betData.fancyData);
         } else {
           //console.log("betData going to bookMakerResult----",betData);
