250d07facd233508143ccb99ce057a1bf5b5b33f
bhattitayab <bhattitayab747@gmail.com>
2025-01-30 13:05:19 +0400

    cancel bet working

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2818,7 +2818,7 @@ const cancelSingleBet = async (req, res) => {
       });
     }
 
-    await handleWinningBetXX(bet,1);
+    //await handleWinningBetXX(bet,1);
    // await SettleParents(user,bet,winningAmount,session,formattedDate,1)
 
     if ([2, 3, 4].includes(bet.type)) {
