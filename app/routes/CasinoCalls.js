84fb875de67f37064f2e00ffcbcd682cbbc1b01e
bhattitayab <bhattitayab747@gmail.com>
2025-01-30 13:28:48 +0400

    poker auth

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -2115,7 +2115,7 @@ if (!transactionId2) {
   
       
   
-        return res.status(201).json({    responseData });
+        return res.status(201).json(    responseData );
       
     } catch (error) {
       console.error("Error handling casino:", error);
