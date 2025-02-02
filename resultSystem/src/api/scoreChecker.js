86fb2259047f43a81c7744eadf4b635e197b05c3
bhattitayab <bhattitayab747@gmail.com>
2025-02-02 04:11:53 +0400

    fancies new setup XX enabled and calling single

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -729,12 +729,12 @@ function scoreChecker() {
               { userId: bet.userId }
             );
             console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-           /// if(newBetUser.createdBy==46384 ||  newBetUser.createdBy==46387 || newBetUser.createdBy==46278 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
+            if(newBetUser.createdBy==46384 ||  newBetUser.createdBy==46387 || newBetUser.createdBy==46278 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
 
               await handleWinningBetXX(bet,0);
 
-            //}else{
-             // await handleWinningBetXX(bet,0);
+            }else{
+              await handleWinningBetXX(bet,0);
               // if (bet.type == 0) {
               //   console.log("bet.marketId::::::::::",bet.marketId);
               //   console.log(bet.type,"=====",parseInt(bet.TargetScore),"--parseInt(result.result)----->>>>",parseInt(result.result));
@@ -752,10 +752,10 @@ function scoreChecker() {
               // } else {
               //   await handleDrawBetX(bet);
               // }
-           // }
+            }
             
           
-          //}
+          }
         }
       }
     } catch (error) {
