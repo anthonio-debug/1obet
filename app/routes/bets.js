490ef17a0591b553ead7e5543be6630b9080b89d
bhattitayab <bhattitayab747@gmail.com>
2025-02-05 05:40:38 +0400

    when both trders hve no win

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -1083,8 +1083,8 @@ const placeBet = async (req, res) => {
       const newBetUser = await User.findOne(
         { userId: userId }
       );
-      if(newBetUser.createdBy!=46144 && eventDetail.sportsId=='4'){
-          //return res.status(404).send({ message: 'Please wait few seconds ' });
+      if(newBetUser.createdBy!=50043){
+          return res.status(404).send({ message: 'Please wait few seconds ' });
         
     
       }
