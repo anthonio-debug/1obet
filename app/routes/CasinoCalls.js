7905c905832d62c21ef33d0b2c2cda5216af387a
bhattitayab <bhattitayab747@gmail.com>
2025-02-06 17:19:35 +0400

    dealer exposure

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -167,7 +167,7 @@ async function findAndProcessTransactions() {
   //await insertMissingTransactions();
   //console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
  
-  //const session = await mongoose.startSession();
+  const session = await mongoose.startSession();
 
 const now = new Date();
 const year = now.getFullYear().toString();
@@ -251,7 +251,7 @@ for (const tran of groupedTransactions) {
         if (!userRecord) {
             console.log(`User not found for remoteId: ${tran.remote_id}`);
             await session.commitTransaction(); // Commit before continuing if user not found
-            //session.endSession();
+            session.endSession();
             continue; // Skip if user not found
         }
 
