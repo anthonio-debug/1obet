4c5b79b9d84b4cda2545e646fbdd3ee70c0a99b5
bhattitayab <bhattitayab747@gmail.com>
2025-02-08 10:13:06 +0400

    winnerrunner data error

--- resultSystem/src/api/scoreChecker.js
+++ resultSystem/src/api/scoreChecker.js
@@ -224,13 +224,10 @@ function scoreChecker() {
               { userId: bet.userId }
             );
             console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-            if(newBetUser.createdBy==50024 || newBetUser.createdBy==50025 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
-              let winningsCalculate = await getAmountOfWinnerTempUpdated(bet,result.winnerSelectionId);
             
-            }else{
               let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelectionId);
              
-            }
+            
             
             
             return;
@@ -249,13 +246,10 @@ function scoreChecker() {
               { userId: bet.userId }
             );
             console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-            if(newBetUser.createdBy==50024 || newBetUser.createdBy==50025 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
-           
-            let winningsCalculate = await getAmountOfWinnerTempUpdated(bet,result.winnerSelectionId);
-            }else{
+            
               let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelectionId);
               
-            }
+            
 
             return;
            
@@ -334,11 +328,9 @@ function scoreChecker() {
           { userId: bet.userId }
         );
         console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-        if(newBetUser.createdBy==50024 || newBetUser.createdBy==50025 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
-          await getAmountOfWinnerTempUpdated(bet,bet.resultData);
-        }else{
+        
           await getAmountOfWinnerTemp(bet,bet.resultData);
-        }
+        
     
       }
       
@@ -402,13 +394,10 @@ function scoreChecker() {
                 { userId: bet.userId }
               );
               console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-              if(newBetUser.createdBy==50024 || newBetUser.createdBy==50025 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
-               
-              let winningsCalculate = await getAmountOfWinnerTempUpdated(bet,result.winnerSelectionId);
-              }else{
+              
                 let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelectionId);
              
-              } 
+              
             }
             //console.log("handle bet draw");
             //await handleDrawBet(bet);
@@ -428,12 +417,9 @@ function scoreChecker() {
             { userId: bet.userId }
           );
           console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-          if(newBetUser.createdBy==50024 || newBetUser.createdBy==50025 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
-           
-            let winningsCalculate = await getAmountOfWinnerTempUpdated(bet,result.winnerSelectionId);
-           }else{
+          
             let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelectionId);
-           } return;
+            return;
             
           }
         }
@@ -543,13 +529,12 @@ function scoreChecker() {
               { userId: bet.userId }
             );
             console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-            if(newBetUser.createdBy==50024 || newBetUser.createdBy==50025 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
-             
-            let winningsCalculate = await getAmountOfWinnerTempUpdated(bet,result.winnerSelId);
-            }else{
+           
+            
               let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelId);
               
-            }
+            
+
             return;
           }
         } else {
@@ -562,13 +547,10 @@ function scoreChecker() {
               { userId: bet.userId }
             );
             console.log("newBetUser.createdBy>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",newBetUser.createdBy);
-            if(newBetUser.createdBy==50024 || newBetUser.createdBy==50025 || newBetUser.createdBy==46277 || newBetUser.createdBy==46276 || newBetUser.createdBy==11001 || newBetUser.createdBy==46265 || newBetUser.createdBy==46266 || newBetUser.createdBy==46231 || newBetUser.createdBy==46234 || newBetUser.createdBy==46235){
             
-            let winningsCalculate = await getAmountOfWinnerTempUpdated(bet,result.winnerSelId);
-            }else{
               let winningsCalculate = await getAmountOfWinnerTemp(bet,result.winnerSelId);
               
-            }
+            
             return;
 
             
