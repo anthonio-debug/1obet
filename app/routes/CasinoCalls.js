eb879ade7315a404367b309961eee18db3126ad7
bhattitayab <bhattitayab747@gmail.com>
2025-02-10 07:15:26 +0400

    auracasino commisiosn fixing

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -3047,8 +3047,8 @@ async function pokererresults(req, res) {
       console.log("----------------------9--------------------------");
       console.log("user.exposure------",user.exposure);
       console.log("existingCall.calculateExposure----------------->>>>>",existingCall.calculateExposure);
-      usersUpdatedExposure = user.exposure - ( existingCall.calculateExposure * auracasinoMultiples )
-      usersUpdatedavailableBalance = user.availableBalance - (existingCall.calculateExposure * auracasinoMultiples )
+      usersUpdatedExposure = user.exposure - ( existingCall.calculateExposure  )
+      usersUpdatedavailableBalance = user.availableBalance - (existingCall.calculateExposure )
     
     
       profitLoss = Math.abs(profitLoss) 
