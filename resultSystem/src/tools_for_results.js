b71bcf33812a325c6de1e337c0b2762e49ea71c0
bhattitayab <bhattitayab747@gmail.com>
2025-02-16 13:56:56 +0400

    some bets were not settling

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -218,6 +218,7 @@ function ToolForResults() {
      
      
       if (results.length > 0) {
+        console.log("results------befor mnauel----",results);
         await scoreChecker.manuel(results);
       }
     } catch (error) {
