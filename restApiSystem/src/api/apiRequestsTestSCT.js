d5730ab133cd5d150edf8ddf75719fefa5eaf053
bhattitayab <bhattitayab747@gmail.com>
2025-02-20 22:25:18 +0400

    corps reversed

--- restApiSystem/src/api/apiRequestsTestSCT.js
+++ restApiSystem/src/api/apiRequestsTestSCT.js
@@ -698,7 +698,8 @@ function apiRequests() {
 
         if (!response?.data?.result) return;
         const oddsData = response.data.result;
-        
+        console.log("oddsData------------------",oddsData);
+        console.log("oddsData length---------------------",oddsData.length);
         
         iterate++;
         let checkedMarkets = [];
