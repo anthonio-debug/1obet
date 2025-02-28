b8c5fc3d6560fae28c6db608334ad778b0d6bd3a
bhattitayab <bhattitayab747@gmail.com>
2025-02-28 13:22:12 +0400

    races saving

--- app/models/marketIds.js
+++ app/models/marketIds.js
@@ -5,7 +5,7 @@ const MarketIDsSchema = new mongoose.Schema({
   marketId: { type: String,index: true },
   marketName:{ type: String},
   marketType:{ type: String, require: false},
-  betSession: { type: Number, default: 0 },
+  betSession: { type: Number, default: null },
   hasbetfairFancy: { type: Boolean, default: false },
   inPlay: { type: Boolean, default: false },
   iscancelled: { type: Boolean, default: false },
