935a1a9f35d6966cfbd4a8d5b30e77ae9e014d13
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-02-28 05:32:06 -0500

    deleteEventAndMarketId permission

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2191,13 +2191,15 @@ async function setLoginHistories(req, res) {
 }
 
 async function deleteEventAndMarketId(req, res) {
-  if (req.decoded.role != '0') {
+  if (req.decoded?.role != '0') {
     return res.status(404).send({ message: 'only company can ... ' });
   }
 
-  if (!req.query.eventId) {
+  if (!req.query?.eventId) {
     return res.status(404).send({ message: 'Id required ... ' });
   }
+
+  console.log(req.query);
 
   const currentEv = await Events.findOne({ Id: req.query.eventId });
 
@@ -3657,7 +3659,7 @@ loginRouter.post('/updateDefaultBetSizes', settingsValidation.validate('updateDe
 
 loginRouter.get('/GetExchangeRates', GetExchangeRates);
 loginRouter.get('/setCloseEventWithCancelBet', setCloseEventWithCancelBet);
-loginRouter.get('/deleteEventAndMarketId', deleteEventAndMarketId);
+router.get('/deleteEventAndMarketId', deleteEventAndMarketId);
 
 loginRouter.get('/setMatchShow', setMatchShow);
 loginRouter.get('/setLoginHistories', setLoginHistories);
