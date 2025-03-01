434041695e8b96fb9936e029d5cf0e9966169693
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-01 17:42:28 -0500

    a

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -217,7 +217,8 @@ function ToolForResults() {
           console.log("betData is array: => ", Array.isArray(betData));
 
           if (Array.isArray(betData) == true)
-            for (const [index, bet] of betData) {
+            for (const bet of betData) {
+              console.log(index, bet);
               console.log("calling getAmountOfWinnerTemp => ");
               console.log(bet._id);
               if (config.FigureEvenOddSmallBig.includes(Number(bet.subMarketId))) {
@@ -238,24 +239,24 @@ function ToolForResults() {
                 throw new Error("Error occured while settling the bet");
               }
 
-              if (index == betData.length - 1) {
-                await MarketIDS.updateOne( // update the marketid state as settled
-                  {
-                    _id: fancyMarketId._id
-                  },
-                  {
-                    $set: {
-                      isSettled: true
-                    }
-                  }
-                )
-              }
+
             }
 
 
         }
 
-
+        if (index == betData.length - 1) {
+          await MarketIDS.updateOne( // update the marketid state as settled
+            {
+              _id: fancyMarketId._id
+            },
+            {
+              $set: {
+                isSettled: true
+              }
+            }
+          )
+        }
 
         await Settings.findOneAndUpdate({ settingKey: 'IsTempJobRunning' }, { $set: { settingValue: '0' } });
       }
