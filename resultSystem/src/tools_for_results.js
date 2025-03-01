cc01311439b6cab94a735d261da2879f23bfdd2b
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-01 17:43:48 -0500

    aaa

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -244,18 +244,16 @@ function ToolForResults() {
 
         }
 
-        if (index == betData.length - 1) {
-          await MarketIDS.updateOne( // update the marketid state as settled
-            {
-              _id: fancyMarketId._id
-            },
-            {
-              $set: {
-                isSettled: true
-              }
+        await MarketIDS.updateOne( // update the marketid state as settled
+          {
+            _id: fancyMarketId._id
+          },
+          {
+            $set: {
+              isSettled: true
             }
-          )
-        }
+          }
+        )
 
         await Settings.findOneAndUpdate({ settingKey: 'IsTempJobRunning' }, { $set: { settingValue: '0' } });
       }
