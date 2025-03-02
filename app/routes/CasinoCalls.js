185264fe090d90a5f93f147f67b199f4be0cf63e
bhattitayab <bhattitayab747@gmail.com>
2025-03-03 02:48:20 +0400

    fetch data

--- app/routes/CasinoCalls.js
+++ app/routes/CasinoCalls.js
@@ -3293,7 +3293,7 @@ async function fetchResultsByMarketId(req, res) {
             console.log("downpl:", downplValue);
         }
     }
-      return res.status(200).json({ data: results });
+      return res.status(200).json({ data: response.data });
     }
 
   } catch (error) {
