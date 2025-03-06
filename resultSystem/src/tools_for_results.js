e0031c60675e560d5ca74e1d9c502b41aecdd355
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-06 13:50:16 -0500

    aa

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -217,8 +217,8 @@ function ToolForResults() {
           console.log("betData is array: => ", Array.isArray(betData));
 
           if (Array.isArray(betData) == true)
-            for (const [index, bet] of betData.entries()) {
-              console.log(index, bet);
+            for (const bet of betData) {
+              console.log(bet);
               console.log("calling getAmountOfWinnerTemp => ");
               console.log(bet._id);
               if (config.FigureEvenOddSmallBig.includes(Number(bet.subMarketId))) {
@@ -239,19 +239,20 @@ function ToolForResults() {
                 throw new Error("Error occured while settling the bet");
               }
 
-              if (index == betData.length - 1) {
-                await MarketIDS.updateOne( // update the marketid state as settled
-                  {
-                    _id: fancyMarketId._id
-                  },
-                  {
-                    $set: {
-                      isSettled: true
-                    }
-                  }
-                )
+              // if (index == betData.length - 1) {
+
+              // }
+            }
+          await MarketIDS.updateOne( // update the marketid state as settled
+            {
+              _id: fancyMarketId._id
+            },
+            {
+              $set: {
+                isSettled: true
               }
             }
+          )
 
 
         }
