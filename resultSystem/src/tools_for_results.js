b7b90e0754b87f598b2154722943d0805a750b5f
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-07 04:35:59 -0500

    Revert "aa"
    This reverts commit e0031c60675e560d5ca74e1d9c502b41aecdd355.

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -218,8 +218,8 @@ function ToolForResults() {
           console.log(betData);
 
           if (Array.isArray(betData) == true)
-            for (const bet of betData) {
-              console.log(bet);
+            for (const [index, bet] of betData.entries()) {
+              console.log(index, bet);
               console.log("calling getAmountOfWinnerTemp => ");
               console.log(bet._id);
               if (config.FigureEvenOddSmallBig.includes(Number(bet.subMarketId))) {
@@ -240,20 +240,19 @@ function ToolForResults() {
                 throw new Error("Error occured while settling the bet");
               }
 
-              // if (index == betData.length - 1) {
-
-              // }
-            }
-          await MarketIDS.updateOne( // update the marketid state as settled
-            {
-              _id: fancyMarketId._id
-            },
-            {
-              $set: {
-                isSettled: true
+              if (index == betData.length - 1) {
+                await MarketIDS.updateOne( // update the marketid state as settled
+                  {
+                    _id: fancyMarketId._id
+                  },
+                  {
+                    $set: {
+                      isSettled: true
+                    }
+                  }
+                )
               }
             }
-          )
 
 
         }
