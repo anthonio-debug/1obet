a742977b5be604ea62294f0ec51e35b299a5b45f
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-11 01:28:39 -0400

    expire

--- app/middlewares/loginMiddleware.js
+++ app/middlewares/loginMiddleware.js
@@ -80,10 +80,10 @@ function check(req, res, next, token) {
             .send({ message: 'Invalid or expired authorization token' }); //when the testing, comment
         var dateNow = new Date();
         console.log("**************************");
-        console.log(new Date(decoded.expr));
+        console.log(new Date(decoded.exp));
         console.log(dateNow);
         console.log("**************************");
-        if (decoded.expr < dateNow.getTime()) {
+        if (decoded.exp < dateNow.getTime()) {
           return res
             .status(404)
             .send({ message: 'Invalid or expired authorization token' });
