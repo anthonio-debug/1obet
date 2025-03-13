f0814a95bcafa8f3f4d7fd36ea880b80a67959d6
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-13 11:44:26 -0700

    err fix

--- app/routes/admin/bets.js
+++ app/routes/admin/bets.js
@@ -167,7 +167,7 @@ const GetAllBets = async (req, res) => {
           console.log(documents);
           marketData = [...documents];
         } else {
-          let _marketId = _bet.marketId.slice(0, _bet.marketId);
+          let _marketId = _bet.marketId;
           let documents = await MarketIDS.find({
             $or: [
               { marketId: _marketId },
