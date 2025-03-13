80ec6c48f34766931519c1642f349e68fa6d768a
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-13 12:28:04 -0700

    asdfas

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -3152,6 +3152,7 @@ const setFancyScore = async (req, res) => {
   let now = new Date();
   const numericDateTime = `${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}${now.getSeconds().toString().padStart(2, '0')}`;
 
+  console.log*("####################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
   console.log({
     eventId: eventId,
     marketId: String(fancyData).toUpperCase(),
