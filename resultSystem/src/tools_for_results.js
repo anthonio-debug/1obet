5574c6cfc904f9a2c8068ebc2a96ff3b7af8ddae
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-14 09:20:54 -0700

    =

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -204,6 +204,28 @@ function ToolForResults() {
             }
           },
           {
+            $addFields: {
+              cleanedMarketId: {
+                $replaceAll: {
+                  input: { $ifNull: [{ $toString: "$cleanedMarketId" }, ""] },  // Ensure cleanedMarketId is treated as a string
+                  find: "(",  // Replace dashes with empty string
+                  replacement: ""
+                }
+              }
+            }
+          },
+          {
+            $addFields: {
+              cleanedMarketId: {
+                $replaceAll: {
+                  input: { $ifNull: [{ $toString: "$cleanedMarketId" }, ""] },  // Ensure cleanedMarketId is treated as a string
+                  find: ")",  // Replace dashes with empty string
+                  replacement: ""
+                }
+              }
+            }
+          },
+          {
             $match: {
               cleanedMarketId: { $in: cleanedInput == cleanedInput_ballrun ? [cleanedInput] : [cleanedInput, cleanedInput_ballrun] }
 
