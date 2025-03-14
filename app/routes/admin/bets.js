9bb2819c6f267f9f0c7d6f3bc6e14afe40829a8b
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-14 08:21:29 -0700

    =

--- app/routes/admin/bets.js
+++ app/routes/admin/bets.js
@@ -169,7 +169,7 @@ const GetAllBets = async (req, res) => {
         } else {
           let _marketId = _bet.marketId;
 
-          _marketId = _marketId.slice(0, _marketId.indexOf('('));
+          // _marketId = _marketId.slice(0, _marketId.indexOf('('));
 
           let cleanedInput = _marketId.replace(/[^A-Za-z0-9]/g, '').toUpperCase();  // Clean and uppercase the input
           let cleanedInput_ballrun = _marketId.replace(/over/gi, 'ball').replace(/[^A-Za-z0-9]/g, '').toUpperCase();  // Clean and uppercase the input
