6bd05e4f9afa7f85cf0c0495117c38e355e3e787
bhattitayab <bhattitayab747@gmail.com>
2025-03-15 13:25:53 +0400

    score api response

--- helper/api/scoreApiHelper.js
+++ helper/api/scoreApiHelper.js
@@ -82,27 +82,27 @@ async function getCricketScoreAPI(eventId) {
     const response = await axios.get(url)
     
       
-    //   console.log("cricket response...........................",response);
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
+      console.log("cricket response...........................",response);
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
 
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
 
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
-    // console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
+    console.log("cricket response..........................." );
 
 
       return  response.data
