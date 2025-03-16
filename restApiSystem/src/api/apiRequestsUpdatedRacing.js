82db650fcf9a6aea68607aaa8bde9078061ad569
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-16 09:07:07 -0700

    aa

--- restApiSystem/src/api/apiRequestsUpdatedRacing.js
+++ restApiSystem/src/api/apiRequestsUpdatedRacing.js
@@ -1057,7 +1057,7 @@ function apiRequests() {
               else await MarketIDS.updateOne({ marketId: odds.marketId }, { $set: { updatedAt: numericDateTime, status: odds.status } });
             }
 
-            if (!RacingOddsMap.has(marketId) || !isObjectEqual(RacingOddsMap.get(marketId), frontOdds)) {
+            // if (!RacingOddsMap.has(marketId) || !isObjectEqual(RacingOddsMap.get(marketId), frontOdds)) {
               RacingOddsMap.set(marketId, frontOdds);
               if (typeof odds.status === 'undefined' || odds.status !== 'OPEN') {
                 //console.log(odds.marketId, " this market has no odds.....");
@@ -1137,7 +1137,7 @@ function apiRequests() {
                   raceCurrentPosition2,
                 });
               }
-            }
+            // }
 
             responsedMarketIDs.push(odds.marketId);
           } else {
