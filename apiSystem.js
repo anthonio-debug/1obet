37a2917b91338c55c8415b9280632afbd1f22e69
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-16 06:25:26 -0700

    odds delete compare

--- apiSystem.js
+++ apiSystem.js
@@ -210,7 +210,7 @@ async function deleteOdds() {
     let fancyoddsDocuments = await fancyOdds.aggregate([
       {
         $match: {
-          created: { $lt: new Date(thirtyMinutesAgo).toISOString() }
+          created: { $lt: new Date(thirtyMinutesAgo) }
         }
       },
       {
