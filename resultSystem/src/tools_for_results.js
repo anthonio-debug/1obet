4f7fa72a7311bb96352acc89c93e5abd8f0f60cf
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-17 04:16:41 -0700

    a

--- resultSystem/src/tools_for_results.js
+++ resultSystem/src/tools_for_results.js
@@ -432,13 +432,17 @@ function ToolForResults() {
           );
 
           let cloneitem = await MarketIDS.findById(fancyMarketId._id);
+          cloneitem = cloneitem.toObject();
 
-          delete cloneitem._id;
-          delete fancyMarketId._id;
+          delete cloneitem._id; // Remove the _id from cloneitem to prevent duplicates
+          delete fancyMarketId._id; // Remove the _id from fancyMarketId for the same reason
+          
           // clone marketid
-          let cloneresult = await cloneMarketIDS.collection.insertOne({...cloneitem});
-          let cloneresult1 = await cloneMarketIDS.collection.insertOne({...fancyMarketId, isSettled: true,
-            lastCheck: new Date().getTime()});
+          let cloneresult = await cloneMarketIDS.collection.insertOne({ ...cloneitem });
+          let cloneresult1 = await cloneMarketIDS.collection.insertOne({
+            ...fancyMarketId, isSettled: true,
+            lastCheck: new Date().getTime()
+          });
           console.log("cloneresult");
           console.log(cloneresult, cloneresult1);
 
