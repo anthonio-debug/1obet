3f5314385528badad62cd1d4779e332d33565536
bhattitayab <bhattitayab747@gmail.com>
2025-03-19 14:19:18 +0400

    statement balance issue

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -443,7 +443,7 @@ async function SettleParents(user, bet, winningAmount, session, formattedDate, c
 
 
     updatedtotalavailableBalance = Number((reversedavailableBalance - FinalShareAmount));
-    totalBalance = Number((user.balance + winningAmount)-FinalShareAmount);
+    totalBalance = Number(user.balance +FinalShareAmount);
     totalClientPLAmount = user.downLineShare != 100 ? Number((((100 - user.downLineShare) / 100) * winningAmount)) : 0;
     totalClientPL = Number((user.clientPL + totalClientPLAmount));
 
