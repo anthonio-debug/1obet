6bb3862ad4e4e1925d4524c3a4204c0e0a23726d
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-22 19:18:50 -0700

    a

--- resultSystem/src/CalculateBets/helper.js
+++ resultSystem/src/CalculateBets/helper.js
@@ -307,7 +307,7 @@ async function getAmountOfWinnerTemp(betId, selectionId, cancelled) {
       await cloneBets.insertMany(documents, { session });
       console.log("checkcheckcheckcheck");
       console.log([...documents.map(doc => doc._id)]);
-      await Bets.deleteMany({ _id: { $in: [...documents.map(doc => doc._id)] } }, { session });
+      // await Bets.deleteMany({ _id: { $in: [...documents.map(doc => doc._id)] } }, { session });
       /* end */
 
       await deleteObsolete(bet.marketId, bet.betSession, bet.subMarketId, bet.eventId, bet, session, bet)
