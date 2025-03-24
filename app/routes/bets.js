0be269735b41334b473436a1b7971d9b347c311e
bhattitayab <bhattitayab747@gmail.com>
2025-03-24 12:53:52 +0400

    btting enabled

--- app/routes/bets.js
+++ app/routes/bets.js
@@ -963,7 +963,7 @@ const placeBet = async (req, res) => {
       (Array.isArray(item.subMarketId) ? item.subMarketId.includes(userSubMarketId) : item.subMarketId === userSubMarketId)
     );
 
-    if ((marketIds.includes(marketId) || subMarketId.includes(subMarketDetail.Id) || blockedSubMarketsByParentBet) && !user.betLockStatus) {
+    if (marketIds.includes(marketId) || subMarketId.includes(subMarketDetail.Id) || blockedSubMarketsByParentBet || user.betLockStatus) {
       activeBettors.delete(userId);
       return res.status(404).send({ message: 'Betting disabled' });
     }
