21b9baf4cbd7717fd9cc2762ae105a5ad9043aaf
bhattitayab <bhattitayab747@gmail.com>
2025-03-24 04:06:58 +0400

    adjusting exposures by passing userId in URL

--- app/routes/sportsTestAPI.js
+++ app/routes/sportsTestAPI.js
@@ -4425,13 +4425,14 @@ async function deleteOdds(req, res) {
   //    await MarketIDS.deleteMany({status:'ABANDONED'});
   //await MarketIDS.deleteMany({status:'CLOSED'});
   const dealerId = eventId
-  const users = await User.find({
-    $or: [
-      { createdBy: dealerId },
-      { userId: dealerId }
-    ]
-  });
-  //const users = await User.find({'userId':46329});
+
+  // const users = await User.find({
+  //   $or: [
+  //     { createdBy: dealerId },
+  //     { userId: dealerId }
+  //   ]
+  // });
+  const users = await User.find({'userId':dealerId,'role':'5'});
         console.log("users::",users);
   for (const user of users) {
       const userId = user.userId;
