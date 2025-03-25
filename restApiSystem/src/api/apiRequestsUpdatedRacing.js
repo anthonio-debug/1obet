c0f4b832e776f25842b6c9dd0e0785002579a5f3
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-25 04:44:16 -0700

    a

--- restApiSystem/src/api/apiRequestsUpdatedRacing.js
+++ restApiSystem/src/api/apiRequestsUpdatedRacing.js
@@ -201,7 +201,7 @@ const getRaceMarketIds = async (sportsId) => {
   ]).exec();
 
   let marketIds = [];
-  //console.log("documents.length======================================>>>>>",documents.length);
+  console.log("documents.length======================================>>>>>",documents.length);
   if (documents.length > 0) {
     documents.forEach(element => {
       marketIds.push(element.marketId);
