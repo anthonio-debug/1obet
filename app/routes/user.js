0769e508efdf39d8933c3ac33bb5ce0b5758abb0
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-28 06:15:08 -0700

    =

--- app/routes/user.js
+++ app/routes/user.js
@@ -73,20 +73,29 @@ async function registerUser(req, res) {
 
       const user = new User(req.body);
       const userNameLower = user.userName.toLowerCase()
-      user.userName = userNameLower;
-      console.log("userinfo");
-      console.log(user);
+      user.userName = userNameLower
 
       // if (req.body.role !== '5') {
       try {
         // const salt = await bcrypt.genSalt(10);
-        await user.hashPass();
-        // user.password = await bcrypt.hash(req.body.password, salt);
+        // user.hashPass(function (err) {
+        //   if (err) return res.status(404).send({ message: 'NEW_PASS_HASH_FAIL' });
+        //   // user.save((err, results) => {
+        //   //   if (err) {
+        //   //     return res.status(404).send({ message: 'USER_NOT_FOUND' });
+        //   //   }
+        //   //   // return res.send({
+        //   //   //   success: true,
+        //   //   //   message: 'USER_PASSWORD_UPDATED',
+        //   //   //   results: results
+        //   //   // });
+        //   // });
+        // });
+        user.password = await bcrypt.hash(req.body.password, 10);
       } catch (err) {
         return res.status(500).send({ message: 'Error encrypting password', err });
       }
       // }
-      console.log(user);
 
       // Check if the user's role is 5, and if so, set downLineShare to null Ignore downLineShare field if role is 5
       if (req.body.role == '5') {
