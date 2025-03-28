3967788a3556b94abdbddc246732e4400c8c2e57
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-03-27 21:48:26 -0700

    =

--- app/routes/user.js
+++ app/routes/user.js
@@ -689,7 +689,7 @@ function searchUsers(req, res) {
 async function getCurrentUser(req, res) {
   const errors = validationResult(req);
   console.log(errors);
-  console.log(req);
+  console.log(req.body);
   if (errors.errors.length !== 0) {
     return res.status(400).send({ errors: errors.errors });
   }
